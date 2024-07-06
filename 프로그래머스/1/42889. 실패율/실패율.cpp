0db5642345419cb12c372aae099dbbe792f9e398
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2024-07-07 00:36:18 +0900

    [level 1] Title: 실패율, Time: 0.01 ms, Memory: 4.13 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.cpp
+++ 프로그래머스/1/42889. 실패율/실패율.cpp
@@ -1,37 +1,38 @@
 #include <bits/stdc++.h>
-
 using namespace std;
+//실패율 = 도전자수/넘긴사람수+도전자수
+//result = 실패율높은 스테이지순 내림차순
+//실패율이 같은 스테이지가 있다면 작은번호의스테이지가 먼저오며 스테이지에도달한유저가없다면 실패율은0
 const int MaxN = 505;
-int stage_clear[MaxN];
-int stage_amount[MaxN];
-
-vector<int> solution(int N, vector<int> stages) {
-    vector<int> answer;
-
-    // stage_clear 배열과 stage_amount 배열을 0으로 초기화
-    memset(stage_clear, 0, sizeof(stage_clear));
-    memset(stage_amount, 0, sizeof(stage_amount));
-
-    // 각 스테이지에 도달한 플레이어 수를 계산하고, 이전 스테이지까지의 플레이어 수를 누적하여 계산
-    for (const int& num : stages) {
-        for (int i = 1; i <= num; i++)
-            stage_clear[i]++;
-        stage_amount[num]++;
+int player_challenge[MaxN];
+int player_clear[MaxN];
+bool compare(const std::pair<double, int>& a, const std::pair<double, int>& b)
+{
+    return std::abs(a.first - b.first) == 0.0f ? a.second < b.second : a.first > b.first;
+}
+vector<int> solution(int N, vector<int> stages) 
+{
+    for(int i = 0; i < stages.size(); i++)
+    {
+        for(int j = 1; j < stages[i]; j++)
+            player_clear[j]++;
+        player_challenge[stages[i]]++;
     }
-
-    // 스테이지 번호를 담은 배열을 초기화
-    for (int i = 1; i <= N; i++)
-        answer.push_back(i);
-
-    // 실패율을 기준으로 정렬
-    sort(answer.begin(), answer.end(),
-        [&](const int& a, const int& b) {
-            double fail1 = stage_clear[a] ? (double)stage_amount[a] / (double)stage_clear[a] : 0;
-            double fail2 = stage_clear[b] ? (double)stage_amount[b] / (double)stage_clear[b] : 0;
-            if (abs(fail1 - fail2) < std::numeric_limits<double>::epsilon()) // 실패율이 같은 경우
-                return a < b; // 스테이지 번호를 오름차순으로 정렬
-            return fail1 > fail2; // 실패율을 내림차순으로 정렬
-        });
-
+   
+    std::vector<std::pair<double, int>> result;
+    for(int i = 1; i <= N; i++)
+    {
+        std::pair<double, int> p;
+        p.second = i;
+        if(!player_challenge[i] || !(player_challenge[i] + player_clear[i]))
+            p.first = 0.0f;
+        else
+            p.first = player_challenge[i] / (double)(player_challenge[i] + player_clear[i]);
+        result.push_back(p);
+    }
+    std::sort(result.begin(), result.end(), compare);
+    std::vector<int> answer;
+    for(const auto& iter : result)
+        answer.push_back(iter.second);
     return answer;
 }
