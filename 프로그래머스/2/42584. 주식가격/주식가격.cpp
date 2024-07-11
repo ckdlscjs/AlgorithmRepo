293f3874b4ee62d697d965385502018921f5e69a
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2024-07-11 19:45:50 +0900

    [level 2] Title: 주식가격, Time: 14.51 ms, Memory: 16.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42584. 주식가격/주식가격.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+vector<int> solution(vector<int> prices) 
+{   
+    std::vector<int> answer;
+    answer.resize(prices.size());
+    std::stack<int> st;
+    for(int i = 0; i < prices.size(); i++)
+    {
+        while(st.size() && prices[st.top()] > prices[i])
+        {
+            answer[st.top()] = i - st.top();
+            st.pop();
+        }
+        st.push(i);
+    }
+    while(st.size())
+    {
+        answer[st.top()] = prices.size() - 1 - st.top();
+        st.pop();
+    }
+    return answer;
+}
