ccc673727a56fc745877975e369b5ac0bd466fec
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2024-07-17 05:50:44 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 110.31 ms, Memory: 105 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.cpp
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.cpp
@@ -1,16 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
-std::unordered_set<int> sets;
+
 vector<int> solution(vector<int> arr) 
 {
-    vector<int> answer;
-    answer.push_back(arr[0]);
-    for(const int& num : arr)
+    std::vector<int> answer;
+    std::stack<int> st;
+    for(int i = 0; i < arr.size(); i++)
     {
-        if(answer[answer.size()-1] == num)
-            continue;
-        answer.push_back(num);
+        if(st.empty())
+        {
+            st.push(arr[i]);
+        }
+        else
+        {
+            if(st.top() == arr[i])
+            {
+                 continue;
+            }
+            else
+            {
+                int num = st.top();
+                st.pop();
+                st.push(arr[i]);
+                answer.push_back(num);
+            }
+        }
     }
-
+    answer.push_back(st.top());
     return answer;
 }
