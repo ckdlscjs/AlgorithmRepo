96f3bdaef2a89c898415f8334a5a506be8da621b
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2024-08-13 16:21:32 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 0.31 ms, Memory: 4.11 MB -BaekjoonHub

---
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+const int dy[] = {0, 0, 1, -1};
+const int dx[] = {1, -1, 0, 0};
+int dist[105][105];
+int solution(vector<vector<int>> maps)
+{
+    int n = maps.size();
+    int m = maps[0].size();
+    std::queue<std::pair<int, int>> q;
+    q.push({0, 0});
+    dist[0][0] = 1;
+    if(!maps[0][0])
+        return -1;
+    while(q.size())
+    {
+        int y = q.front().first;
+        int x = q.front().second;
+        q.pop();
+        for(int dir = 0; dir < 4; dir++)
+        {
+            int ny = y + dy[dir];
+            int nx = x + dx[dir];
+            if(ny < 0 || nx < 0 || ny >= n || nx >= m || !maps[ny][nx] || dist[ny][nx])
+                continue;
+            dist[ny][nx] = dist[y][x] + 1;
+            q.push({ny, nx});
+        }
+    }
+    return dist[n-1][m-1] ? dist[n-1][m-1] : -1;
+}
