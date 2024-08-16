4980bcfb4d78dbed33cac5b8240513aa7d89a8b4
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2024-08-17 04:38:05 +0900

    [level 3] Title: 합승 택시 요금, Time: 9.89 ms, Memory: 5.49 MB -BaekjoonHub

--- 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.cpp
+++ 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.cpp
@@ -1,8 +1,7 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 const int MaxN = 205;
-const int INF = 100000000;
+const int INF = 987654321;
 std::vector<std::pair<int, int>> graph[MaxN];
 void dijkstra(int* costs, int start)
 {
@@ -29,8 +28,32 @@ void dijkstra(int* costs, int start)
         }
     }
 }
+void floydwarshall(const vector<vector<int>>& fares, int n, int (*costs)[MaxN])
+{
+    std::fill(&costs[0][0], &costs[n][n+1], INF);
+    for(int i = 1; i <= n; i++)
+        costs[i][i] = 0;
+    for(const auto& iter : fares)
+    {
+        costs[iter[0]][iter[1]] = iter[2];
+        costs[iter[1]][iter[0]] = iter[2];
+    }
+    for(int mid = 1; mid <= n; mid++)
+    {
+        for(int i = 1; i <= n; i++)
+        {
+            for(int j = 1; j <= n; j++)
+            {
+                if(costs[i][mid] == INF || costs[mid][j] == INF)
+                    continue;
+                costs[i][j] = std::min(costs[i][j], costs[i][mid] + costs[mid][j]);
+            }
+        }
+    }
+}
 int solution(int n, int s, int a, int b, vector<vector<int>> fares) 
 {
+    /*
     for(const auto& iter : fares)
     {
         graph[iter[0]].push_back({iter[1],iter[2]});
@@ -45,5 +68,12 @@ int solution(int n, int s, int a, int b, vector<vector<int>> fares)
     long long int sum = 987654321;
     for(int i = 1; i <= n; i++)
         sum = std::min((long long int)(cost_s[i] + cost_a[i] + cost_b[i]), sum);
+        */
+    int costs[MaxN][MaxN];
+    floydwarshall(fares, n, costs);
+    long long int sum = INF;
+    for(int i = 1; i <= n; i++)
+        sum = std::min(sum, (long long int)(costs[s][i] + costs[i][a] + costs[i][b]));
+    
     return sum;
 }
