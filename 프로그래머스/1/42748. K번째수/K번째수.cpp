3cb0b475ccd3c15ef11102daa02267bf08295d57
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2024-08-28 14:06:22 +0900

    [level 1] Title: K번째수, Time: 0.02 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> solution(vector<int> array, vector<vector<int>> commands) 
+{
+    vector<int> answer;
+    for(const auto& iter : commands)
+    {
+        std::vector<int> temps(array.begin()+iter[0]-1, array.begin()+iter[1]);
+        std::sort(temps.begin(), temps.end(), std::less<>());
+        answer.push_back(temps[iter[2]-1]);
+    }
+    return answer;
+}
