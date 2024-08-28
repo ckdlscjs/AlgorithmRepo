7c05bf946ba17e0f4ec80eed4c4c6372e3c294b3
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2024-08-28 23:55:00 +0900

    [level 2] Title: H-Index, Time: 0.05 ms, Memory: 4.17 MB -BaekjoonHub

---
+++ 프로그래머스/2/42747. H－Index/H－Index.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> citations) {
+    sort(citations.begin(), citations.end());
+    int answer = 0;
+    int n = citations.size();
+    
+    for (int i=0; i<n; i++) {
+        int hIndex = n-i;
+        
+        if (citations[i] >= hIndex) {
+            answer = hIndex;
+            break;
+        }
+    }
+    
+    return answer;
+}
