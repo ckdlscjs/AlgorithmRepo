db74e094486df9f2e758d4de8c596ebcdb409f3a
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2024-08-31 07:10:56 +0900

    [Silver I] Title: 효율적인 해킹, Time: 2020 ms, Memory: 3508 KB -BaekjoonHub

---
+++ 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.cc
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n, m;
+const int MaxN = 10005;
+std::vector<int> tree[MaxN];
+int cnt[MaxN];
+int result = 0;
+int main() 
+{
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(0);
+  std::cout.tie(0);
+  std::cin >> n >> m;
+  for(int i = 0; i < m; i++)
+  {
+    int a, b;
+    std::cin >> a >> b;
+    tree[a].push_back(b);
+  }
+  for(int i = 1; i <= n; i++)
+  {
+    bool visited[MaxN] = {false, };
+    visited[i] = true;
+    std::queue<int> q;
+    q.push(i);
+    while(q.size())
+    {
+      int cur = q.front();
+      q.pop();
+      cnt[cur]++;
+      result = std::max(result, cnt[cur]);
+      for(const auto& iter : tree[cur])
+      {
+        if(visited[iter])
+          continue;
+        visited[iter] = true;
+        q.push(iter);
+      }
+    }
+  }
+    
+  std::priority_queue<int, std::vector<int>, std::greater<int>> pq;
+  for(int i = 1; i <= n; i++)
+    if(result == cnt[i])
+      pq.push(i);
+  while(pq.size())
+  {
+    std::cout << pq.top() << ' ';
+    pq.pop();
+  }
+  return 0;
+}
