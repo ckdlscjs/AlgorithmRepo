610130df17dbb4a45d97fe2f5320f8907a284fc8
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2024-09-02 12:10:26 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.02 ms, Memory: 3.68 MB -BaekjoonHub

--- 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.cpp
+++ 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.cpp
@@ -1,23 +1,21 @@
 #include <bits/stdc++.h>
-const int dy[] = {1, -1, 0 ,0};
-const int dx[] = {0, 0, -1, 1};
-std::map<std::string, int> dir;
 using namespace std;
-
-vector<int> solution(vector<string> keyinput, vector<int> board) {
-    dir["up"] = 0;
-    dir["down"] = 1;
-    dir["left"] = 2;
-    dir["right"] = 3;
-    std::vector<int> answer{0, 0};
-    for(const auto& str : keyinput)
+vector<int> solution(vector<string> keyinput, vector<int> board) 
+{
+    std::unordered_map<std::string, std::pair<int, int>> inputs;
+    inputs["up"] = {1, 0};
+    inputs["down"] = {-1, 0};
+    inputs["left"] = {0, -1};
+    inputs["right"] = {0, 1};
+    int y = 0, x = 0;
+    for(const auto& iter : keyinput)
     {
-        int ny = answer[1] + dy[dir[str]];
-        int nx = answer[0] + dx[dir[str]];
-        if(ny < -board[1]/2 || ny > board[1]/2 || nx < -board[0] / 2 || nx > board[0]/2)
+        int ny = y + inputs[iter].first;
+        int nx = x + inputs[iter].second;
+        if(ny <  -board[1] / 2 || nx < -board[0] / 2 || ny > board[1]/2 || nx > board[0]/2)
             continue;
-        answer[0] = nx;
-        answer[1] = ny;
+        y = ny;
+        x = nx;
     }
-    return answer;
+    return {x, y};
 }
