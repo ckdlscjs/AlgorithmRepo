b4495305997ef29ce222c9c3f018068d2a2ac5f9
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2024-09-09 19:12:14 +0900

    [Gold IV] Title: 뮤탈리스크, Time: 0 ms, Memory: 3092 KB -BaekjoonHub

---
+++ 백준/Gold/12869. 뮤탈리스크/뮤탈리스크.cc
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n;
+int result = 60;
+int dp[65][65][65];
+void check(int a, int b, int c, int cnt)
+{
+  if(dp[a][b][c] && dp[a][b][c] <= cnt)
+    return;
+  dp[a][b][c] = cnt;
+  if(a <= 0 && b<= 0 && c<= 0)
+  {
+    result = std::min(result, cnt);
+  }
+  else
+  {
+    check(a-9 > 0 ? a-9 : 0, b-3 > 0 ? b-3:0, c-1 > 0 ? c-1 : 0, cnt+1);
+    check(a-9 > 0 ? a-9 : 0, b-1 > 0 ? b-1:0, c-3 > 0 ? c-3 : 0, cnt+1);
+    
+    check(a-3 > 0 ? a-3 : 0, b-9 > 0 ? b-9:0, c-1 > 0 ? c-1 : 0, cnt+1);
+    check(a-3 > 0 ? a-3 : 0, b-1 > 0 ? b-1:0, c-9 > 0 ? c-9 : 0, cnt+1);
+    
+    check(a-1 > 0 ? a-1 : 0, b-9 > 0 ? b-9:0, c-3 > 0 ? c-3 : 0, cnt+1);
+    check(a-1 > 0 ? a-1 : 0, b-3 > 0 ? b-3:0, c-9 > 0 ? c-9 : 0, cnt+1);
+  }
+}
+int main() 
+{
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(0);
+  std::cout.tie(0);
+  std::cin >> n;
+  std::vector<int> scvs(3, 0);
+  for(int i = 0; i < n; i++)
+    std::cin >> scvs[i];
+  check(scvs[0], scvs[1], scvs[2], 0);
+  std::cout << result;
+  return 0;
+}
