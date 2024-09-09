feff12a0ad75c40b7ff80718b2680604d3ec8f88
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2024-09-09 19:14:03 +0900

    [Gold IV] Title: 뮤탈리스크, Time: 0 ms, Memory: 3092 KB -BaekjoonHub

--- 백준/Gold/12869. 뮤탈리스크/뮤탈리스크.cc
+++ 백준/Gold/12869. 뮤탈리스크/뮤탈리스크.cc
@@ -1,28 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 int n;
-int result = 60;
 int dp[65][65][65];
 void check(int a, int b, int c, int cnt)
 {
   if(dp[a][b][c] && dp[a][b][c] <= cnt)
     return;
   dp[a][b][c] = cnt;
-  if(a <= 0 && b<= 0 && c<= 0)
-  {
-    result = std::min(result, cnt);
-  }
-  else
-  {
-    check(a-9 > 0 ? a-9 : 0, b-3 > 0 ? b-3:0, c-1 > 0 ? c-1 : 0, cnt+1);
-    check(a-9 > 0 ? a-9 : 0, b-1 > 0 ? b-1:0, c-3 > 0 ? c-3 : 0, cnt+1);
+  if(a <= 0 && b <= 0 && c<= 0)
+    return;
+  check(a-9 > 0 ? a-9 : 0, b-3 > 0 ? b-3:0, c-1 > 0 ? c-1 : 0, cnt+1); //9 3 1
+  check(a-9 > 0 ? a-9 : 0, b-1 > 0 ? b-1:0, c-3 > 0 ? c-3 : 0, cnt+1); //9 1 3
     
-    check(a-3 > 0 ? a-3 : 0, b-9 > 0 ? b-9:0, c-1 > 0 ? c-1 : 0, cnt+1);
-    check(a-3 > 0 ? a-3 : 0, b-1 > 0 ? b-1:0, c-9 > 0 ? c-9 : 0, cnt+1);
+  check(a-3 > 0 ? a-3 : 0, b-9 > 0 ? b-9:0, c-1 > 0 ? c-1 : 0, cnt+1); //3 9 1
+  check(a-3 > 0 ? a-3 : 0, b-1 > 0 ? b-1:0, c-9 > 0 ? c-9 : 0, cnt+1); //3 1 9
     
-    check(a-1 > 0 ? a-1 : 0, b-9 > 0 ? b-9:0, c-3 > 0 ? c-3 : 0, cnt+1);
-    check(a-1 > 0 ? a-1 : 0, b-3 > 0 ? b-3:0, c-9 > 0 ? c-9 : 0, cnt+1);
-  }
+  check(a-1 > 0 ? a-1 : 0, b-9 > 0 ? b-9:0, c-3 > 0 ? c-3 : 0, cnt+1); //1 9 3
+  check(a-1 > 0 ? a-1 : 0, b-3 > 0 ? b-3:0, c-9 > 0 ? c-9 : 0, cnt+1); //1 3 9
 }
 int main() 
 {
@@ -34,6 +28,6 @@ int main()
   for(int i = 0; i < n; i++)
     std::cin >> scvs[i];
   check(scvs[0], scvs[1], scvs[2], 0);
-  std::cout << result;
+  std::cout << dp[0][0][0];
   return 0;
 }
