caf78b82b2c44b83a319f9785e2a224ed76a7b17
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2024-09-10 09:28:05 +0900

    [Gold IV] Title: 숨바꼭질 2, Time: 0 ms, Memory: 2932 KB -BaekjoonHub

--- 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.cc
+++ 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.cc
@@ -2,7 +2,7 @@
 using namespace std;
 int n, k;
 int dp[100005];
-int cnt = 0;
+int cnt[100005];
 int main() 
 {
   std::ios::sync_with_stdio(false);
@@ -10,28 +10,28 @@ int main()
   std::cout.tie(0);
   std::cin >> n >> k;
   dp[n] = 1;
+  cnt[n] = 1;
   std::queue<int> q;
   q.push(n);
   while(q.size())
   {
     int cur = q.front();
     q.pop();
-    if(cur == k)
-    {
-      cnt++;
-      continue;
-    }
     for(const auto& iter : {cur-1, cur+1, cur*2})
     {
       if(iter < 0 || iter > 100000)
         continue;
       if(!dp[iter])
-        dp[iter] = dp[cur] + 1;
-      if(dp[iter] == dp[cur] + 1)
-        q.push(iter);
+      {
+          dp[iter] = dp[cur] + 1;
+          q.push(iter);
+          cnt[iter] += cnt[cur];
+      }
+      else if(dp[iter] == dp[cur] + 1)
+          cnt[iter] += cnt[cur];
     }
   }
   std::cout << dp[k] - 1 <<'\n';
-  std::cout << cnt;
+  std::cout << cnt[k];
   return 0;
 }
