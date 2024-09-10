0c960fdc0da6cd07892aab95fe4f0bf665e75493
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2024-09-11 04:14:05 +0900

    [Platinum V] Title: 백조의 호수, Time: 96 ms, Memory: 14592 KB -BaekjoonHub

--- 백준/Platinum/3197. 백조의 호수/백조의 호수.cc
+++ 백준/Platinum/3197. 백조의 호수/백조의 호수.cc
@@ -1,120 +1,94 @@
-#pragma once
 #include <bits/stdc++.h>
-const int dy[] = { 0, 0, 1, -1 };
-const int dx[] = { 1, -1, 0, 0 };
-int r, c;
-const int max = 1501;
-char arr[max][max];
-bool visited_swan[max][max];
-struct Pos
+using namespace std;
+const int dy[] = {0, 0, 1, -1};
+const int dx[] = {1, -1, 0, 0};
+const int MaxN = 1505;
+char arr[MaxN][MaxN];
+bool visit_swan[MaxN][MaxN];
+bool visit_water[MaxN][MaxN];
+int r, c, LIdx;
+std::pair<int, int> L[2];
+std::queue<std::pair<int, int>> move_swan, next_swan;
+std::queue<std::pair<int, int>> expand_water, next_water;
+int main() 
 {
-	int y;
-	int x;
-	Pos(int _y, int _x)
-	{
-		y = _y;
-		x = _x;
-	}
-};
-std::queue<Pos> q_swan;
-std::queue<Pos> q_swan_next;
-std::queue<Pos> q_ices;
-std::queue<Pos> q_ices_next;
-
-bool Bfs_swan()
-{
-	while (q_swan.size())
-	{
-		Pos pos = q_swan.front();
-		q_swan.pop();
-		for (int dir = 0; dir < 4; dir++)
-		{
-			int ny = pos.y + dy[dir];
-			int nx = pos.x + dx[dir];
-			if (ny < 0 || nx < 0 || ny >= r || nx >= c || visited_swan[ny][nx])
-				continue;
-			visited_swan[ny][nx] = true;
-			if (arr[ny][nx] == 'L')
-				return true;
-			if (arr[ny][nx] == 'X')
-			{
-				q_swan_next.push(Pos(ny, nx));
-				continue;
-			}
-			q_swan.push(Pos(ny, nx));
-		}
-	}
-	return false;
-}
-
-void Bfs_ice()
-{
-	while (q_ices.size())
-	{
-		Pos pos = q_ices.front();
-		q_ices.pop();
-		for (int dir = 0; dir < 4; dir++)
-		{
-			int ny = pos.y + dy[dir];
-			int nx = pos.x + dx[dir];
-			if (ny < 0 || nx < 0 || ny >= r || nx >= c)
-				continue;
-			if (arr[ny][nx] == 'X')
-			{
-				arr[ny][nx] = '.';
-				q_ices_next.push(Pos(ny, nx));
-				continue;
-			}
-		}
-	}
-}
-
-int main(void)
-{
-	std::ios::sync_with_stdio(false);
-	std::cin.tie(0);
-	std::cout.tie(0);
-	std::cin >> r >> c;
-	Pos swan(0, 0);
-	for (int i = 0; i < r; i++)
-	{
-		std::string str;
-		std::cin >> str;
-		for (int j = 0; j < c; j++)
-		{
-			arr[i][j] = str[j];
-			if (arr[i][j] == 'L')
-			{
-				swan.y = i;
-				swan.x = j;
-			}
-			if (arr[i][j] != 'X')
-			{
-				q_ices.push(Pos(i, j));
-			}
-		}
-	}
-	q_swan.push(Pos(swan));
-	visited_swan[swan.y][swan.x] = true;
-
-	int count = 0;
-	while (!Bfs_swan())
-	{
-		count++;	
-		Bfs_ice();
-		while (q_swan_next.size())
-		{
-			q_swan.push(q_swan_next.front());
-			q_swan_next.pop();
-		}
-			
-		while (q_ices_next.size())
-		{
-			q_ices.push(q_ices_next.front());
-			q_ices_next.pop();
-		}
-			
-	}
-	std::cout << count;
-	return 0;
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(0);
+  std::cout.tie(0);
+  std::cin >> r >> c;
+  for(int i = 0; i < r; i++)
+  {
+    std::string str;
+    std::cin >> str;
+    for(int j = 0; j < c; j++)
+    {
+      arr[i][j] = str[j];
+      if(arr[i][j] == 'L')
+      {
+        L[LIdx++] = std::make_pair(i, j);
+      }
+      if(arr[i][j] == '.' || arr[i][j] == 'L') //백조가있는공간도 Water
+      {
+        expand_water.push({i, j});
+        visit_water[i][j] = true;
+      }
+    }
+  }
+  move_swan.push(L[0]);
+  visit_swan[L[0].first][L[0].second] = true;
+  int cnt = 0;
+  while(!visit_swan[L[1].first][L[1].second])
+  {
+    while(move_swan.size())
+    {
+      int y = move_swan.front().first;
+      int x = move_swan.front().second;
+      move_swan.pop();
+      for(int dir = 0; dir < 4; dir++)
+      {
+        int ny = y + dy[dir];
+        int nx = x + dx[dir];
+        if(ny < 0 || nx < 0 || ny >= r || nx >= c || visit_swan[ny][nx])
+          continue;
+        visit_swan[ny][nx] = true;
+        if(arr[ny][nx] == '.')
+          move_swan.push({ny, nx});
+        else
+          next_swan.push({ny, nx});
+      }
+    }
+    if(visit_swan[L[1].first][L[1].second])
+      continue;
+    
+    while(expand_water.size())
+    {
+      int y = expand_water.front().first;
+      int x = expand_water.front().second;
+      expand_water.pop();
+      for(int dir = 0; dir < 4; dir++)
+      {
+        int ny = y + dy[dir];
+        int nx = x + dx[dir];
+        if(ny < 0 || nx < 0 || ny >= r || nx >= c || visit_water[ny][nx] || arr[ny][nx] != 'X')
+          continue;
+        visit_water[ny][nx] = true;
+        next_water.push({ny, nx});
+        arr[ny][nx] = '.';
+      }
+    }
+    while(next_swan.size())
+    {
+      move_swan.push(next_swan.front());
+      next_swan.pop();
+    }
+    while(next_water.size())
+    {
+      expand_water.push(next_water.front());
+      next_water.pop();
+    }
+    cnt++;
+  }
+  
+  std::cout << cnt;
+  return 0;
 }
