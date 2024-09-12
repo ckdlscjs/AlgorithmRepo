eddb05ef87edc4ed5848b9996893db8d30676b22
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2024-09-13 06:39:09 +0900

    [Gold I] Title: 동전 뒤집기, Time: 536 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Gold/1285. 동전 뒤집기/동전 뒤집기.cc
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n;
+std::string arrs[21][2];
+int main() 
+{
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(0);
+  std::cout.tie(0);
+  std::cin >> n;
+  for(int i = 0; i < n; i++)
+  {
+    std::string str;
+    std::cin>>str;
+    std::string rev_str;
+    for(int j = 0; j < str.size(); j++)
+      rev_str += str[j] == 'T' ? 'H' : 'T';
+    arrs[i][0] = str;
+    arrs[i][1] = rev_str;
+  }
+  int res = 987654321;
+  for(int hori = 0; hori < (1 << n); hori++)
+  {
+    int cnt_t = 0;
+    for(int j = 0; j < n; j++)
+    {
+      int temp_t = 0;
+      for(int i = 0; i < n; i++)
+      {
+        int rev = hori & (1 << i) ? 1 : 0;
+        temp_t += arrs[i][rev][j] == 'T' ?  1 : 0;
+      }
+      cnt_t += temp_t > n/2 ? n-temp_t : temp_t;
+    }
+    res = std::min(res, cnt_t);
+  }
+  std::cout << res;
+  return 0;
+}
