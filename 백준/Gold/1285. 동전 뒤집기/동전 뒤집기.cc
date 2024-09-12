91a714bd4cd02cab969207c91e4fb7064b073375
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2024-09-13 07:32:53 +0900

    [Gold I] Title: 동전 뒤집기, Time: 460 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/1285. 동전 뒤집기/동전 뒤집기.cc
+++ 백준/Gold/1285. 동전 뒤집기/동전 뒤집기.cc
@@ -2,7 +2,7 @@
 using namespace std;
 
 int n;
-std::string arrs[21][2];
+int arrs[21];
 int main() 
 {
   std::ios::sync_with_stdio(false);
@@ -12,27 +12,23 @@ int main()
   for(int i = 0; i < n; i++)
   {
     std::string str;
-    std::cin>>str;
-    std::string rev_str;
+    std::cin >> str;
     for(int j = 0; j < str.size(); j++)
-      rev_str += str[j] == 'T' ? 'H' : 'T';
-    arrs[i][0] = str;
-    arrs[i][1] = rev_str;
+      if(str[j] == 'T') arrs[i] |= 1 << j;
   }
+    
   int res = 987654321;
   for(int hori = 0; hori < (1 << n); hori++)
   {
     int cnt_t = 0;
-    for(int j = 0; j < n; j++)
+    for(int j = 1; j < (1 << n); j *= 2)
     {
       int temp_t = 0;
       for(int i = 0; i < n; i++)
       {
-        int rev = hori & (1 << i) ? 1 : 0;
-        temp_t += arrs[i][rev][j] == 'T' ?  1 : 0;
+        temp_t += (hori & (1 << i) ? ~arrs[i] : arrs[i]) & j ? 1 : 0;
       }
       cnt_t += temp_t > n/2 ? n-temp_t : temp_t;
-      if(cnt_t > res) continue;
     }
     res = std::min(res, cnt_t);
   }
