92d160bd4a181a89990aa79344ce9c2c4dd95553
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2024-09-20 06:44:04 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 4.12 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.cpp
+++ 프로그래머스/1/42862. 체육복/체육복.cpp
@@ -1,45 +1,30 @@
 #include <bits/stdc++.h>
-/*
-n 명 
-2~ 30명
-1 2 3 4 5 6 7 
-x o x x x o x
-*/
+
 using namespace std;
-const int MaxN = 32;
-int check[MaxN];
+
+int sportswear[32];
 int solution(int n, vector<int> lost, vector<int> reserve) 
 {
-    for(int i = 1; i <= n; i++)
-        check[i]++;
-    for(const int& idx : reserve)
-        check[idx]++;
-    for(const int& idx : lost)
-        check[idx]--;
-    
-    for(int idx = 1; idx <= n; idx++)
-    {
-        if(check[idx])
-            continue;
-        if(idx-1 >= 1 && check[idx-1] > 1)
-        {
-            check[idx] = 1;
-            check[idx-1] = 1;
-        }
-        else if(idx+1 <= n && check[idx+1] > 1)
-        {
-            check[idx] = 1;
-            check[idx+1] = 1;
-        }
-    }
+    for(const auto& iter : lost)
+        sportswear[iter]--;
+    for(const auto& iter : reserve)
+        sportswear[iter]++;
     int answer = 0;
     for(int i = 1; i <= n; i++)
     {
-        if(check[i] >= 1)
+        if(sportswear[i] >= 1 && sportswear[i-1] < 0)
         {
-            std::cout << i << ' ';
-            answer++;
+            sportswear[i-1] += 1;
+            sportswear[i] -= 1;
+        }
+        if(sportswear[i] >= 1 && sportswear[i+1] < 0)
+        {
+            sportswear[i+1] += 1;
+            sportswear[i] -= 1;
         }
     }
+    for(int i = 1; i <= n; i++)
+        if(sportswear[i] >= 0)
+            answer++;
     return answer;
 }
