c95c7f968bfe2467885658f591d27d2b8825273b
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2024-09-25 05:38:54 +0900

    [Silver II] Title: 베르트랑 공준, Time: 0 ms, Memory: 1404 KB -BaekjoonHub

--- 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.cc
+++ 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.cc
@@ -1,29 +1,31 @@
-#include <bits/stdc++.h>
-int n;
-bool notPrime[300000];
-int primeCount[300000];
+#include <cstdio>
+#include <math.h>
+using namespace std;
+const int MAX = 300000;
+bool arr[MAX];
+
 int main(void)
 {
-    std::ios::sync_with_stdio(false);
-    std::cin.tie(0);
-    std::cout.tie(0);
-    notPrime[0] = notPrime[1] = true;
-    for(int i = 2; i <= std::sqrt(300000); i++)
+  arr[0] = true;
+  arr[1] = true;
+  for(int i = 2; i <= sqrt(MAX); i++)
+  {
+    if(arr[i]) continue;
+    int temp = i + i;
+    while(temp < MAX)
     {
-      if(notPrime[i])
-        continue;
-      for(int j = i*2; j <= 300000; j += i)
-        notPrime[j] = true;
+      arr[temp] = true;
+      temp += i;
     }
-    
-    for(int i = 1; i <= 300000; i++)
-    {
-      primeCount[i] = notPrime[i] ? primeCount[i-1] : primeCount[i-1] + 1;
-    }
-    while(std::cin >> n && n)
-    {
-      std::cout << primeCount[n*2] - primeCount[n] <<'\n';
-    }
+  }
+  int n = 0;
+  scanf("%d", &n);
+  while(n != 0)
+  {
+    int count = 0;
+    for(int i = n+1; i <= 2*n; i++) if(!arr[i]) count++;
+    printf("%d\n",count);
+    scanf("%d", &n);
+  }
   
-    return 0;
 }
