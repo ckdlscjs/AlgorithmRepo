3ef99eee6b76376e58b2bb363578ca8a639bca7e
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2024-09-27 03:11:43 +0900

    [Bronze II] Title: 수학은 비대면강의입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.cc
+++ 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.cc
@@ -6,17 +6,6 @@ int main(void)
   std::cin.tie(0);
   std::cout.tie(0);
   std::cin >> a >> b >> c >> d >> e >> f;
-  int x, y;
-  for(x = -999; x <= 999; x++)
-  {
-    for(y = -999; y <= 999; y++)
-    {
-      if(a*x + b*y == c && d*x + e*y == f)
-      {
-        std::cout << x << ' ' << y;
-        return 0;
-      }
-    }
-  }
+  std::cout << (c*e-b*f)/(a*e - b*d) << ' ' <<(d*c-a*f)/(b*d-a*e);
   return 0;
 }
