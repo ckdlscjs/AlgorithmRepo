b546cb34b43e519576bacef68984516fef3ef2c9
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2024-09-26 13:44:24 +0900

    [Silver III] Title: 서로 다른 부분 문자열의 개수, Time: 460 ms, Memory: 209864 KB -BaekjoonHub

--- 백준/Silver/11478. 서로 다른 부분 문자열의 개수/서로 다른 부분 문자열의 개수.cc
+++ 백준/Silver/11478. 서로 다른 부분 문자열의 개수/서로 다른 부분 문자열의 개수.cc
@@ -1,14 +1,6 @@
 #include <bits/stdc++.h>
 std::string str;
 std::unordered_set<std::string> chk;
-void check(std::string st, int idx)
-{
-  if(st.size())
-    chk.insert(st);
-  if(st.size() >= str.size() || idx >= str.size())
-    return;
-  check(st + str[idx], idx+1);
-}
 int main(void)
 {
   std::ios::sync_with_stdio(false);
@@ -16,7 +8,14 @@ int main(void)
   std::cout.tie(0);
   std::cin >> str;
   for(int i = 0; i < str.size(); i++)
-    check({str[i]}, i+1);
+  {
+    std::string temp = "";
+    for(int j = i; j < str.size(); j++)
+    {
+      temp += str[j];
+      chk.insert(temp);
+    }
+  }
   std::cout << chk.size();
   return 0;
 }
