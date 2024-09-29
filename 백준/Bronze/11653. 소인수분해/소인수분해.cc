04e7ac4d75696c45d0364813a06a68c8d44c187f
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2024-09-30 05:41:16 +0900

    [Bronze I] Title: 소인수분해, Time: 0 ms, Memory: 2036 KB -BaekjoonHub

--- 백준/Bronze/11653. 소인수분해/소인수분해.cc
+++ 백준/Bronze/11653. 소인수분해/소인수분해.cc
@@ -1,21 +1,31 @@
 #include <bits/stdc++.h>
+int n;
 int main(void)
 {
     std::ios::sync_with_stdio(false);
     std::cin.tie(0);
     std::cout.tie(0);
-    int n;
     std::cin >> n;
-    int divisor = 2;
+    std::set<int> divisors;
+    for(int i = 2; i <= std::sqrt(n); i++)
+    {
+      if(n%i == 0)
+      {
+        divisors.insert(i);
+        divisors.insert(n/i);
+      }
+    }
+    divisors.insert(n);
+    std::set<int>::iterator iter = divisors.begin();
     while(n != 1)
     {
-      if(n % divisor)
+      if(n % *iter)
       {
-        divisor++;
+        iter++;
         continue;
       }
-      std::cout << divisor <<'\n';
-      n /= divisor;
+      std::cout <<*iter <<'\n';
+      n /= *iter;
     }
     return 0;
 }
