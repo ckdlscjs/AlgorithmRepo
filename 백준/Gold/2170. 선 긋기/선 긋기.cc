e880c473d3686cf2bbfadd9bd3a65a956f1b4a4c
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2024-09-30 15:59:28 +0900

    [Gold V] Title: 선 긋기, Time: 328 ms, Memory: 17644 KB -BaekjoonHub

--- 백준/Gold/2170. 선 긋기/선 긋기.cc
+++ 백준/Gold/2170. 선 긋기/선 긋기.cc
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 const int MaxN = 1'000'005;
-long long int n, l, r, sum;
+long long int n, l, sum;
 std::pair<long long int, long long int> arr[MaxN]; 
 int main()
 {
@@ -12,20 +12,15 @@ int main()
         std::cin >> arr[i].first >> arr[i].second;
     std::sort(arr, arr+n, std::less<>());
     l = arr[0].first;
-    r = arr[0].second;
-    for(int i = 1; i < n; i++)
+    for(int i = 0; i < n; i++)
     {
-        if(arr[i].first > r)
-        {
-            sum += r-l;
-            l = arr[i].first;
-            r = arr[i].second;
+        if(arr[i].second < l)
             continue;
-        }
-        if(arr[i].second > r)
-            r = arr[i].second;
+        if(arr[i].first > l)
+            l = arr[i].first;
+        sum += arr[i].second - l;
+        l = arr[i].second;
     }
-    sum += r-l;
     std::cout << sum;
     return 0;
 }
