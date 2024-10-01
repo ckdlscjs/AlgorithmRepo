3e5ded7cf2caaaba161b0495ac805ac396c23da5
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2024-10-02 04:27:57 +0900

    [level 1] Title: [PCCE 기출문제] 10번 / 데이터 분석, Time: 0.74 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.cpp
+++ 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.cpp
@@ -1,24 +1,22 @@
 #include <bits/stdc++.h>
-
 using namespace std;
-
-
+std::unordered_map<std::string, int> idxs;
 vector<vector<int>> solution(vector<vector<int>> data, string ext, int val_ext, string sort_by) 
 {
-    std::vector<std::vector<int>> answer;
-    int idx_ext = ext == "code" ? 0 : ext == "date" ? 1 : ext == "maximum" ? 2 : 3;
-    int idx_sort = sort_by == "code" ? 0 : sort_by == "date" ? 1 : sort_by == "maximum" ? 2 : 3;
-    for(const auto& iter : data)
+    idxs["code"] = 0;
+    idxs["date"] = 1;
+    idxs["maximum"] = 2;
+    idxs["remain"] = 3;
+    for(auto iter = data.begin(); iter != data.end();)
     {
-        if(iter[idx_ext] > val_ext)
-            continue;
-        answer.push_back(iter);
+        if((*iter)[idxs[ext]] > val_ext)
+            iter = data.erase(iter);
+        else
+            iter++;
     }
-    std::sort(answer.begin(), answer.end(), 
-              [&](const std::vector<int>& a, const std::vector<int>& b)
-              {
-                  return a[idx_sort] < b[idx_sort];
-              }
-             );
-    return answer;
+    std::sort(data.begin(), data.end(), [=](const std::vector<int>& a, const std::vector<int>& b){
+        return a[idxs[sort_by]] < b[idxs[sort_by]];
+    });
+    
+    return data;
 }
