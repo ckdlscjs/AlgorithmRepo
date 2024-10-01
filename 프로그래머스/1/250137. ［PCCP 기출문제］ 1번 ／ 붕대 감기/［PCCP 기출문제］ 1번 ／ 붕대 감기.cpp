8d785cb0a440081155ce2804fb5d0435b7fc466c
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2024-10-01 09:00:20 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 붕대 감기, Time: 0.02 ms, Memory: 4.19 MB -BaekjoonHub

--- 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.cpp
+++ 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.cpp
@@ -1,51 +1,28 @@
+//https://school.programmers.co.kr/learn/courses/30/lessons/250137
 #include <bits/stdc++.h>
-
 using namespace std;
-/*
-bandage -> t, x, y 1초마다 x회복, t초성공시 y추가회복
-t도중 피격시 취소됨, t가초기화됨
-
-피격시 피해량만큼체력이감소됨, 0이하는 사망
-
-health는 최대체력
-
-attacks는 공격시간, 피해량
-
-모든공격이끝나고 남은체력을 return, 사망시 -1
-*/
-int at[1010];
+int attack[1005];
 int solution(vector<int> bandage, int health, vector<vector<int>> attacks) 
 {
-    int t_last = 0;
     for(const auto& iter : attacks)
+        attack[iter[0]] = iter[1];
+    int cur = health;
+    int cont_cnt = 0;
+    for(int t = 1; t <= attacks[attacks.size()-1][0]; t++)
     {
-        at[iter[0]] = iter[1];
-        t_last = iter[0];
-    }
-        
-    int cur_health = health;
-    int t = 0;
-    int t_ing = 0;
-    while(t <= t_last && cur_health > 0)
-    {
-        if(at[t])
+        if(attack[t])
         {
-            cur_health -= at[t];
-            t_ing = 0;
-        }   
-        else
-        {   if(t_ing)
-                cur_health = std::min(cur_health + bandage[1], health);
-            if(t_ing >= bandage[0])
-            {
-                t_ing = 0;
-                cur_health = std::min(cur_health + bandage[2], health);
-            }
-            
+            cont_cnt = 0;
+            cur -= attack[t];
+            if(cur <= 0)
+                break;
+            continue;
         }
-        std::cout << t << ' ' << cur_health << ' ' << t_ing << ' '<< (at[t] ? 'O' : 'X') << '\n';
-        t_ing++;
-        t++;
+        cur = std::min(cur + bandage[1], health);
+        cont_cnt++;
+        cur = std::min(cur + (cont_cnt / bandage[0] ? bandage[2] : 0), health);
+        cont_cnt %= bandage[0];
+        //std::cout << t << ' ' << cur << ' ' <<cont_cnt << ' ' <<attack[t] <<'\n';
     }
-    return cur_health > 0 ? cur_health : -1;
+    return cur > 0? cur : -1;
 }
