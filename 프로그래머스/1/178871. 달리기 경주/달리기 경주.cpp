5db11506ba9389f269958ffc377fce8f53ade2b9
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2024-10-02 05:22:50 +0900

    [level 1] Title: 달리기 경주, Time: 347.38 ms, Memory: 101 MB -BaekjoonHub

---
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+std::unordered_map<std::string, int> idxs;
+vector<string> solution(vector<string> players, vector<string> callings) 
+{
+    for(int i = 0; i < players.size(); i++)
+        idxs[players[i]] = i;
+    for(const auto& iter : callings)
+    {
+        int temp = idxs[iter];
+        idxs[players[temp-1]]++;
+        idxs[iter]--;
+        players[temp] = players[temp-1];
+        players[temp-1] = iter;
+    }
+    return players;
+}
