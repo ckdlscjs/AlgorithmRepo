0ca8ee78cff1b1bf50afa871c6f1a61a3afcacab
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2024-10-02 18:23:39 +0900

    [Silver IV] Title: 소가 길을 건너간 이유 3, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/14469. 소가 길을 건너간 이유 3/소가 길을 건너간 이유 3.cc
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+int n, sum;
+std::pair<int, int> arr[105];
+int main()
+{
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(0);
+    std::cout.tie(0);
+    std::cin >> n;
+    for(int i = 0; i < n; i++)
+      std::cin >> arr[i].first >> arr[i].second;
+    std::sort(arr, arr+n, std::less<>());
+    int r = arr[0].first + arr[0].second;
+    for(int i = 1; i < n; i++)
+    {
+      if(arr[i].first <= r)
+      {
+        r += arr[i].second;
+      }
+      else
+      {
+        r = arr[i].first + arr[i].second;
+      }
+    }
+    std::cout << r;
+    return 0;
+}
