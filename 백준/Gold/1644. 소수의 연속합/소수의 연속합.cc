4c7611036a530a3eea4a4f5d58a831079e3a2286
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2024-10-03 05:17:43 +0900

    [Gold III] Title: 소수의 연속합, Time: 32 ms, Memory: 9128 KB -BaekjoonHub

---
+++ 백준/Gold/1644. 소수의 연속합/소수의 연속합.cc
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+long long int n, start, end, sum, cnt;
+const int MaxN = 4'000'005;
+bool NotPrime[MaxN];
+std::vector<int> primes;
+int main()
+{
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(0);
+    std::cout.tie(0);
+    std::cin >> n;
+    NotPrime[0] = NotPrime[1] = true;
+    for(int i = 2; i <= MaxN; i++)
+    {
+      if(NotPrime[i])
+        continue;
+      primes.push_back(i);
+      for(int j = 2; i*j <= MaxN; j++)
+        NotPrime[i*j] = true;
+    }
+    while(end <= primes.size())
+    {
+      if(sum < n) sum += primes[end++];
+      else sum -= primes[start++];
+      if(sum == n) cnt++;
+    }
+    std::cout << cnt;
+    return 0;
+}
