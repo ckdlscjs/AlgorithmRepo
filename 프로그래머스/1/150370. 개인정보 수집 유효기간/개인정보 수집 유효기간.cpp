b7d68ab2b61e2db11d6d0a7f16d1e7250b0992d1
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2024-10-04 10:14:07 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 0.05 ms, Memory: 4.19 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.cpp
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.cpp
@@ -6,32 +6,17 @@ vector<int> solution(string today, vector<string> terms, vector<string> privacie
     for(const auto& iter : terms)
         t[iter[0]] = std::stoi(iter.substr(2));
     vector<int> answer;
-    //std::cout << iter.substr(0,4) << iter.substr(5,2) << iter.substr(8,2) <<'\n';
     for(int i = 0; i < privacies.size(); i++)
     {
-        std::string chk = privacies[i].substr(0,10);
-        int year = std::stoi(chk.substr(0,4));
-        int month = std::stoi(chk.substr(5,2));
-        int day = std::stoi(chk.substr(8,2));
-        for(int j = 0; j < t[privacies[i][11]]; j++)
-        {
-            day += j == 0 ? 27 : 28;
-            if(day > 28)
-            {
-                day -= 28;
-                month += 1;
-                if(month > 12)
-                {
-                    month -= 12;
-                    year += 1;
-                }
-            }
-        }
         int t_year = std::stoi(today.substr(0,4));
         int t_month = std::stoi(today.substr(5,2));
         int t_day = std::stoi(today.substr(8,2));
-        int sum_chk = year*12*28 + month*28 + day;
-        int sum_today = t_year*12*28 + t_month*28 + t_day;
+        int year = std::stoi(privacies[i].substr(0,4));
+        int month = std::stoi(privacies[i].substr(5,2));
+        int day = std::stoi(privacies[i].substr(8,2));
+        int sum_today = t_year * 28 * 12 + t_month * 28 + t_day;
+        int sum_chk = year * 28 * 12 + month * 28 + day;
+        sum_chk += t[privacies[i][11]] * 28 - 1;
         if(sum_chk < sum_today)
             answer.push_back(i+1);
     }
