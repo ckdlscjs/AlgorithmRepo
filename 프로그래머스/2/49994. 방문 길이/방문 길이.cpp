4b37f99a019d2f6bca5a987d5b65a59c6b0c9350
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2024-10-04 14:15:32 +0900

    [level 2] Title: 방문 길이, Time: 0.18 ms, Memory: 4.23 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.cpp
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.cpp
@@ -2,43 +2,24 @@
 using namespace std;
 const int dy[] = {-1, 1, 0, 0};
 const int dx[] = {0, 0, 1, -1};
-std::map<char, int> dir;
-bool arr[15][15][4];
+std::set<std::pair<std::pair<int, int>, std::pair<int, int>>> checks; 
+std::unordered_map<char, int> dir;
+int y, x, answer;
 int solution(string dirs) 
-{   
-    dir['U'] = 0;
-    dir['D'] = 1;
-    dir['R'] = 2;
-    dir['L'] = 3;
-    int y = 5, x = 5;
-    int count = 0;
-    for(int i = 0; i < dirs.size(); i++)
+{
+    dir['U'] = 0;dir['D'] = 1;dir['R'] = 2;dir['L'] = 3;
+    for(const auto& iter : dirs)
     {
-        int direction = dir[dirs[i]];
-        int cy = y, cx = x;
-        int ny = y + dy[dir[dirs[i]]];
-        int nx = x + dx[dir[dirs[i]]];
-        std::cout << ny << ' ' << nx << ", ";
-        if(ny < 0 || nx < 0 || ny > 10 || nx > 10)
+        int ny = y + dy[dir[iter]];
+        int nx = x + dx[dir[iter]];
+        if(ny < -5 || nx < -5 || ny > 5 || nx > 5)
             continue;
+        if(checks.find({{y, x}, {ny, nx}}) == checks.end())
+            answer++;
+        checks.insert({{y, x}, {ny, nx}});
+        checks.insert({{ny, nx}, {y, x}});
         y = ny;
         x = nx;
-        if(direction < 2)
-        {
-            if(arr[cy][cx][direction] || arr[ny][nx][std::abs(direction-1)])
-                continue;
-            arr[cy][cx][direction] = true;
-            arr[ny][nx][std::abs(direction-1)] = true;
-        }
-        else
-        {
-            if(arr[cy][cx][direction] || arr[ny][nx][std::abs(direction-3) + 2])
-                continue;
-            arr[cy][cx][direction] = true;
-            arr[ny][nx][std::abs(direction-3) + 2] = true;
-        }
-        count++;
-        std::cout << cy << ' ' <<cx << "->" << ny << ' ' << nx <<'\n';
     }
-    return count;
+    return answer;
 }
