e5543386681174f8d52b0d4755d9c94ded12763a
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2024-10-04 14:37:43 +0900

    [level 1] Title: 모의고사, Time: 0.27 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.cpp
+++ 프로그래머스/1/42840. 모의고사/모의고사.cpp
@@ -1,35 +1,22 @@
 #include <bits/stdc++.h>
+std::vector<int> an[3] = {{1, 2, 3, 4, 5}, {2, 1, 2, 3, 2, 4, 2, 5}, {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}};
+int ans[3], maxcnt;
 using namespace std;
+
 vector<int> solution(vector<int> answers) 
 {
-    std::vector<std::vector<int>> player;
-    std::vector<int> p1 = {1,2,3,4,5};
-    std::vector<int> p2 = {2,1,2,3,2,4,2,5};
-    std::vector<int> p3 = {3,3,1,1,2,2,4,4,5,5};
-    player.push_back(p1);
-    player.push_back(p2);
-    player.push_back(p3);
-    vector<int> score;
-    score.push_back(0);
-    score.push_back(0);
-    score.push_back(0);
-    int maximum = 0;
     for(int i = 0; i < answers.size(); i++)
     {
-        if(player[0][i % player[0].size()] == answers[i])
-            score[0]++;
-        maximum = std::max(maximum, score[0]);
-        if(player[1][i % player[1].size()] == answers[i])
-            score[1]++;
-        maximum = std::max(maximum, score[1]);
-        if(player[2][i % player[2].size()] == answers[i])
-            score[2]++; 
-        maximum = std::max(maximum, score[2]);
-        
+        ans[0] += answers[i] == an[0][i%an[0].size()];
+        ans[1] += answers[i] == an[1][i%an[1].size()];
+        ans[2] += answers[i] == an[2][i%an[2].size()];
     }
+    maxcnt = std::max({ans[0], ans[1], ans[2]});
     std::vector<int> answer;
-    for(int i = 0; i < score.size(); i++)
-        if(score[i] == maximum)
+    for(int i = 0; i < 3; i++)
+    {
+        if(maxcnt == ans[i])
             answer.push_back(i+1);
+    }
     return answer;
 }
