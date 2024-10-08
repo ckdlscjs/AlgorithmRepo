bd36721ca2a2b9d41e221cef0b8172326a0a0aa9
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2024-10-08 21:17:59 +0900

    [level 3] Title: 표 편집, Time: 61.89 ms, Memory: 52.1 MB -BaekjoonHub

--- 프로그래머스/3/81303. 표 편집/표 편집.cpp
+++ 프로그래머스/3/81303. 표 편집/표 편집.cpp
@@ -1,87 +1,90 @@
-//https://school.programmers.co.kr/learn/courses/30/lessons/81303
 #include <bits/stdc++.h>
+/*
+"U X": 현재 선택된 행에서 X칸 위에 있는 행을 선택합니다.
+"D X": 현재 선택된 행에서 X칸 아래에 있는 행을 선택합니다.
+"C" : 현재 선택된 행을 삭제한 후, 바로 아래 행을 선택합니다. 단, 삭제된 행이 가장 마지막 행인 경우 바로 윗 행을 선택합니다.
+"Z" : 가장 최근에 삭제된 행을 원래대로 복구합니다. 단, 현재 선택된 행은 바뀌지 않습니다.
+n : 행 개수
+k : 처음 선택된 행의 위치
+cmd : 명령어들이 담긴 문자열배열
+*/
 using namespace std;
 struct Node
 {
-    int idx;
+    int idx = 0;
     Node* prev = nullptr;
     Node* next = nullptr;
     Node(int _idx) : idx(_idx), prev(nullptr), next(nullptr) {}
-
 };
-Node* linkedList = nullptr;
-Node* cursor = nullptr;
+Node* cursor;
+std::stack<Node*> st;
+bool check[1000005];
+void Order(const std::string& order)
+{
+    int X = order.size() <= 1 ? 0 : std::stoi(order.substr(2));
+    if(order[0] == 'U')
+    {
+        for(int i = 0; i < X; i++)
+            cursor = cursor->prev;
+    }
+    if(order[0] == 'D')
+    {
+        for(int i = 0; i < X; i++)
+            cursor = cursor->next;
+    }
+    if(order[0] == 'C')
+    {   
+        check[cursor->idx] = false;
+        st.push(cursor);
+        Node* prevNode = cursor->prev;
+        Node* nextNode = cursor->next;
+        if(prevNode)
+            prevNode->next = nextNode;
+        if(nextNode)
+            nextNode->prev = prevNode; 
+        cursor = (nextNode) ? nextNode : prevNode;
+    }
+    if(order[0] == 'Z')
+    {
+        Node* delNode = st.top();
+        check[delNode->idx] = true;
+        st.pop();
+        if(delNode->prev)
+            delNode->prev->next = delNode;
+        if(delNode->next)
+            delNode->next->prev = delNode;
+    }
+}
+
 string solution(int n, int k, vector<string> cmd) 
 {
     string answer = "";
-    for(int i = 0; i < n; i++)
+    
+    std::memset(check, true, sizeof(check));
+    Node* head = new Node(0);
+    Node* temp = head;
+    cursor = head;
+    for(int i = 1; i < n; i++)
     {
-        answer += "X";
         Node* newNode = new Node(i);
-        if(!linkedList)
-        {
-            linkedList = newNode;
-        }
-        else
-        {
-            linkedList->next = newNode;
-            newNode->prev = linkedList;
-            linkedList = newNode;
-        }
+        newNode->prev = temp;
+        temp->next = newNode;
+        temp = newNode;
         if(i == k)
-            cursor = linkedList;
+            cursor = newNode;
+        
     }
-    std::stack<Node*> st;
-    
-    for(const auto& iter : cmd)
+    for(int i = 0; i < cmd.size(); i++)
+        Order(cmd[i]);
+   
+    while(head)
     {
-        if(iter[0] == 'U')
-        {
-            for(int i = 0; i < std::stoi(iter.substr(2)); i++)
-                cursor = cursor->prev;
-        }
-        else if(iter[0] == 'D')
-        {
-            for(int i = 0; i < std::stoi(iter.substr(2)); i++)
-                cursor = cursor->next;
-        }
-        else if(iter[0] == 'C')
-        {
-            st.push(cursor);
-            Node* temp = cursor;
-            if(temp->next == nullptr)
-            {
-                cursor = temp->prev;
-                cursor->next = nullptr;
-            }
-            else
-            {
-                cursor = temp->next;
-                if(cursor->prev)
-                    cursor->prev = temp->prev;
-                if(cursor->prev)
-                    cursor->prev->next = cursor;
-            }
-        }
-        else
-        {
-            Node* insert = st.top();
-            st.pop();
-            if(insert->prev)
-                insert->prev->next = insert;
-            if(insert->next)
-                insert->next->prev = insert;
-        }
+        Node* nextNode = head->next;
+        delete head;
+        head = nextNode;
     }
-    
-    while(cursor->prev)
-        cursor = cursor->prev;
-    while(cursor)
-    {
-        answer[cursor->idx] = 'O';
-        cursor = cursor->next;
-    }
-    
-    
+    for(int i = 0; i < n; i++)
+        answer += check[i] ? "O" : "X";
+  
     return answer;
 }
