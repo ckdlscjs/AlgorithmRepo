076bd19161dae4da936fa1c5dce2a600ea5203f6
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2024-10-08 19:13:26 +0900

    [Gold V] Title: 흙길 보수하기, Time: 0 ms, Memory: 2100 KB -BaekjoonHub

--- 백준/Gold/1911. 흙길 보수하기/흙길 보수하기.cc
+++ 백준/Gold/1911. 흙길 보수하기/흙길 보수하기.cc
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
-int n, l;
+int n, l, LL, RR, res, cnt;
 std::pair<int, int> water[10005];
 int main()
 {
@@ -14,18 +14,14 @@ int main()
   {
     return a.second < b.second;
   });
-  int left = water[0].first;
-  int right = (water[0].second - water[0].first);
-  int res = (right / l + (right%l? 1 : 0));
-  right = res * l + left;
-  for(int i = 1; i < n; i++)
+  for(int i = 0; i < n; i++)
   {
-    if(right < water[i].second)
+    if(RR < water[i].second)
     {
-      left = std::max(water[i].first, right);
-      right = water[i].second - left;
-      int cnt = (right / l + (right%l? 1 : 0));
-      right = cnt * l + left;
+      LL = std::max(water[i].first, RR);
+      RR = water[i].second - LL;
+      cnt = (RR / l + (RR%l? 1 : 0));
+      RR = cnt * l + LL;
       res += cnt;
     }
   }
