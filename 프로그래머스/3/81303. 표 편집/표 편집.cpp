954c99e03387c2eea4161a9b9a2e4e0fa67f06b8
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2024-10-08 21:12:40 +0900

    [level 3] Title: 표 편집, Time: 79.41 ms, Memory: 52.2 MB -BaekjoonHub

--- 프로그래머스/3/81303. 표 편집/표 편집.cpp
+++ 프로그래머스/3/81303. 표 편집/표 편집.cpp
@@ -1,90 +1,87 @@
+//https://school.programmers.co.kr/learn/courses/30/lessons/81303
 #include <bits/stdc++.h>
 using namespace std;
-
-struct Node {
+struct Node
+{
     int idx;
-    Node* prev;
-    Node* next;
+    Node* prev = nullptr;
+    Node* next = nullptr;
     Node(int _idx) : idx(_idx), prev(nullptr), next(nullptr) {}
+
 };
-
-Node* cursor;
-std::stack<Node*> st;
-bool check[1000005];
-
-void Order(const std::string& order) {
-    int X = order.size() <= 1 ? 0 : std::stoi(order.substr(2));
-    if (order[0] == 'U') {
-        for (int i = 0; i < X; i++) {
-            cursor = cursor->prev;
-        }
-    } else if (order[0] == 'D') {
-        for (int i = 0; i < X; i++) {
-            cursor = cursor->next;
-        }
-    } else if (order[0] == 'C') {
-        st.push(cursor);
-        Node* prevNode = cursor->prev;
-        Node* nextNode = cursor->next;
-        if (prevNode) {
-            prevNode->next = nextNode;
-        }
-        if (nextNode) {
-            nextNode->prev = prevNode;
-        }
-        check[cursor->idx] = false;
-        cursor = (nextNode) ? nextNode : prevNode;
-    } else if (order[0] == 'Z') {
-        Node* delNode = st.top();
-        st.pop();
-        if (delNode->prev) {
-            delNode->prev->next = delNode;
-        }
-        if (delNode->next) {
-            delNode->next->prev = delNode;
-        }
-        check[delNode->idx] = true;
-    }
-}
-
-string solution(int n, int k, vector<string> cmd) {
-    string answer(n, 'X');
-    
-    Node* head = new Node(0);
-    Node* temp = head;
-    cursor = head;
-
-    for (int i = 1; i < n; i++) {
+Node* linkedList = nullptr;
+Node* cursor = nullptr;
+string solution(int n, int k, vector<string> cmd) 
+{
+    string answer = "";
+    for(int i = 0; i < n; i++)
+    {
+        answer += "X";
         Node* newNode = new Node(i);
-        newNode->prev = temp;
-        temp->next = newNode;
-        temp = newNode;
-        if (i == k) {
-            cursor = newNode;
+        if(!linkedList)
+        {
+            linkedList = newNode;
+        }
+        else
+        {
+            linkedList->next = newNode;
+            newNode->prev = linkedList;
+            linkedList = newNode;
+        }
+        if(i == k)
+            cursor = linkedList;
+    }
+    std::stack<Node*> st;
+    
+    for(const auto& iter : cmd)
+    {
+        if(iter[0] == 'U')
+        {
+            for(int i = 0; i < std::stoi(iter.substr(2)); i++)
+                cursor = cursor->prev;
+        }
+        else if(iter[0] == 'D')
+        {
+            for(int i = 0; i < std::stoi(iter.substr(2)); i++)
+                cursor = cursor->next;
+        }
+        else if(iter[0] == 'C')
+        {
+            st.push(cursor);
+            Node* temp = cursor;
+            if(temp->next == nullptr)
+            {
+                cursor = temp->prev;
+                cursor->next = nullptr;
+            }
+            else
+            {
+                cursor = temp->next;
+                if(cursor->prev)
+                    cursor->prev = temp->prev;
+                if(cursor->prev)
+                    cursor->prev->next = cursor;
+            }
+        }
+        else
+        {
+            Node* insert = st.top();
+            st.pop();
+            if(insert->prev)
+                insert->prev->next = insert;
+            if(insert->next)
+                insert->next->prev = insert;
         }
     }
-
-    for (int i = 0; i < n; i++) {
-        check[i] = true;
+    
+    while(cursor->prev)
+        cursor = cursor->prev;
+    while(cursor)
+    {
+        answer[cursor->idx] = 'O';
+        cursor = cursor->next;
     }
-
-    for (const auto& c : cmd) {
-        Order(c);
-    }
-
-    for (int i = 0; i < n; i++) {
-        if (check[i]) {
-            answer[i] = 'O';
-        }
-    }
-
-    // 메모리 해제
-    temp = head;
-    while (temp != nullptr) {
-        Node* nextNode = temp->next;
-        delete temp;
-        temp = nextNode;
-    }
-
+    
+    
     return answer;
 }
