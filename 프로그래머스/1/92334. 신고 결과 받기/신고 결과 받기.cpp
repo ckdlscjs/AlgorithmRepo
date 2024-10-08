62594d3c18d6e06216565f9d0f080b8f16730c1c
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2024-10-09 05:50:23 +0900

    [level 1] Title: 신고 결과 받기, Time: 91.52 ms, Memory: 48.8 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.cpp
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.cpp
@@ -1,29 +1,31 @@
+//https://school.programmers.co.kr/learn/courses/30/lessons/92334
 #include <bits/stdc++.h>
 using namespace std;
-
+std::unordered_set<std::string> reports;
+std::unordered_map<std::string, int> cnts;
+std::unordered_map<std::string, std::vector<std::string>> reportlist;
 vector<int> solution(vector<string> id_list, vector<string> report, int k) 
-{  
-    std::unordered_map<std::string, int> report_results;
-    for(const auto& iter : id_list)
-        report_results[iter] = 0;
-        
-    std::unordered_map<std::string, std::unordered_set<std::string>> reports_count;
-    for(auto iter : report)
+{
+    for(const auto& iter : report)
     {
-        std::string id = iter.substr(0, iter.find(' '));
-        iter.erase(0, iter.find(' ') + 1);
-        std::string rep = iter;
-        reports_count[rep].insert(id);
+        if(reports.find(iter) != reports.end())
+            continue;
+        reports.insert(iter);
+        std::string from = iter.substr(0, iter.find(" "));
+        std::string to = iter.substr(iter.find(" ")+1);
+        reportlist[from].push_back(to);
+        cnts[to]++;
     }
-    for(const auto& iter : reports_count)
-    {
-        if(iter.second.size() >= k)
-            for(const auto& user : iter.second)
-                report_results[user]++;
-    }
-    
-    std::vector<int> answer;
+    vector<int> answer;
     for(const auto& iter : id_list)
-        answer.push_back(report_results[iter]);
+    {
+        int res = 0;
+        for(const auto& str : reportlist[iter])
+        {
+            if(cnts[str] >= k)
+                res++;
+        }
+        answer.push_back(res);
+    }
     return answer;
 }
