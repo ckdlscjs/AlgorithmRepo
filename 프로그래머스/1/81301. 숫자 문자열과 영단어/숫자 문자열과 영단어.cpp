05c40e424ce4e8054495d733e373c38ec6ec53e1
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2024-10-10 07:06:40 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.cpp
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.cpp
@@ -1,24 +1,35 @@
+//https://school.programmers.co.kr/learn/courses/30/lessons/81301
 #include <bits/stdc++.h>
-
 using namespace std;
-std::unordered_map<std::string, std::string> nums;
-int solution(string s) {
-    nums["zero"] = "0";
-    nums["one"] = "1";
-    nums["two"] = "2";
-    nums["three"] = "3";
-    nums["four"] = "4";
-    nums["five"] = "5";
-    nums["six"] = "6";
-    nums["seven"] = "7";
-    nums["eight"] = "8";
-    nums["nine"] = "9";
-    for(const auto& iter : nums)
+std::unordered_map<std::string, int> nums;
+int solution(string s) 
+{
+    nums["zero"] = 0; 
+    nums["one"] = 1; 
+    nums["two"] = 2; 
+    nums["three"] = 3; 
+    nums["four"] = 4; 
+    nums["five"] = 5; 
+    nums["six"] = 6;
+    nums["seven"] = 7; 
+    nums["eight"] = 8; 
+    nums["nine"] = 9;
+    int answer = 0;
+    std::string temp;
+    for(const auto& iter : s)
     {
-        while(s.find(iter.first) != std::string::npos)
+        if('0' <= iter && iter <= '9')
         {
-            s.replace(s.find(iter.first), iter.first.size(), iter.second);
+            answer = answer*10 + iter-'0';
+        } 
+        else
+        {
+            temp += iter;
+            if(nums.find(temp) == nums.end())
+                continue;
+            answer = answer * 10 + nums[temp];
+            temp.clear();
         }
     }
-    return std::stoi(s);
+    return answer;
 }
