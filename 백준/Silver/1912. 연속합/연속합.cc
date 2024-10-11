bceeb42f43a4604bafb83ed6483080a31b79470f
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2024-10-11 16:06:42 +0900

    [Silver II] Title: 연속합, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.cc
+++ 백준/Silver/1912. 연속합/연속합.cc
@@ -5,15 +5,13 @@ int cur, pre;
 int main()
 {
   ios::sync_with_stdio(false);
-  cin.tie(0);
-  cout.tie(0);
   std::cin >> n;
   for(int i = 0; i < n; i++)
   {
     std::cin >> cur;
     pre += cur;
     res = std::max(res, pre);
-    pre = std::max(pre, 0);
+    if(pre < 0) pre = 0;
   }
   std::cout << res;
   return 0;
