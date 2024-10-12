cd728bb6dfcac0444101df565384aad5ec06f845
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2024-10-12 11:47:41 +0900

    [Gold III] Title: 현욱은 괄호왕이야!!, Time: 0 ms, Memory: 3192 KB -BaekjoonHub

--- 백준/Gold/15926. 현욱은 괄호왕이야！！/현욱은 괄호왕이야！！.cc
+++ 백준/Gold/15926. 현욱은 괄호왕이야！！/현욱은 괄호왕이야！！.cc
@@ -1,29 +1,25 @@
 #include <bits/stdc++.h>
-int n;
+int n, res;
 std::string str;
-int main(void)
+std::stack<int> st;
+int main() 
 {
-    std::ios::sync_with_stdio(false);
-    std::cin.tie(0);
-    std::cout.tie(0);
-    std::cin >> n >> str;
-    std::stack<int> st;
-    st.push(-1);
-    int len = 0;
-    for(int i = 0; i < str.size(); i++)
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(0); 
+  std::cout.tie(0);
+  std::cin >> n;
+  std::cin >> str;
+  st.push(-1);
+  for(int i = 0; i < n; i++)
+  {
+    if(str[i] == '(') st.push(i);  
+    else
     {
-        if(str[i] == '(')
-        {
-            st.push(i);
-        }
-        else
-        {   
-            st.pop();
-            if(st.size()) len = std::max(len, i-st.top());
-            else st.push(i);
-        }
-       
-    }   
-    std::cout << len;
-    return 0;
+      st.pop();
+      if(!st.empty()) res = std::max(res, i - st.top());
+      else st.push(i);
+    }
+  }
+  std::cout << res;
+  return 0;
 }
