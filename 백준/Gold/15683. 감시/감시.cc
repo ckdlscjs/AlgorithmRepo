90bbe635c8f33703bb5e4cfe3123b121a4f29213
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2024-10-12 10:15:45 +0900

    [Gold III] Title: 감시, Time: 8 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/15683. 감시/감시.cc
+++ 백준/Gold/15683. 감시/감시.cc
@@ -5,22 +5,15 @@ int arr[10][10];
 const int dy[] = {-1, 0, 1, 0};
 const int dx[] = {0, 1, 0, -1};
 std::vector<std::pair<std::pair<int, int>, std::string>> Cameras;
-void Check(int (*maps)[10], int idx)
+void Check(int (*arr)[10], int idx)
 {
   if(idx >= Cameras.size())
   {
     int cnt = 0;
     for(int i = 0; i < N; i++)
-    {
       for(int j = 0; j < M; j++)
-      {
-        //std::cout << maps[i][j] << ' ';
-        if(!maps[i][j])
+        if(!arr[i][j])
           cnt++;
-      }
-      //std::cout << '\n';
-    }
-    //std::cout << '\n';
     res = std::min(res, cnt);
     return;
   }
@@ -30,7 +23,7 @@ void Check(int (*maps)[10], int idx)
   for(int rot = 0; rot < 4; rot++)
   {
     int temp[10][10];
-    std::memcpy(temp, maps, sizeof(temp));
+    std::memcpy(temp, arr, sizeof(temp));
     for(int didx = 0; didx < dir.size(); didx++)
     {
       if(dir[didx] == '0')
@@ -58,7 +51,6 @@ int main()
   cin.tie(0);
   cout.tie(0);
   std::cin >> N >> M;
-  //std::vector<std::pair<int, int>> cam5s;
   for(int i = 0; i < N; i++)
   {
     for(int j = 0; j < M; j++)
@@ -76,36 +68,7 @@ int main()
         Cameras.push_back({{i, j}, "1111"});
     }
   }
-  /*
-  for(const auto& iter : cam5s)
-  {
-    for(int i = 0; i < N; i++)
-    {
-      if(arr[i][iter.second] == 6)
-        break;
-      if(i != iter.first && arr[i][iter.second] == 0)
-        arr[i][iter.second] = 7;
-    };
-    for(int j = 0; j < M; j++)
-    {
-      if(arr[iter.first][j] == 6)
-        break;
-      if(j != iter.second && arr[iter.first][j] == 0)
-        arr[iter.first][j] = 7;
-    }
-  }
-  */
   Check(arr, 0);
-  /*
-  for(int i = 0; i < N; i++)
-  {
-    for(int j = 0; j < M; j++)
-    {
-      std::cout << arr[i][j] << ' ';
-    }
-    std::cout << '\n';
-  }
-  */
   std::cout << res;
   return 0;
 }
