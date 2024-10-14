0c358f1532e720144ec80b7c66c68a4f86b99d06
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2024-10-15 04:59:01 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.cpp
+++ 프로그래머스/1/42862. 체육복/체육복.cpp
@@ -1,30 +1,24 @@
 #include <bits/stdc++.h>
-
 using namespace std;
-
-int sportswear[32];
+int arr[35];
 int solution(int n, vector<int> lost, vector<int> reserve) 
 {
-    for(const auto& iter : lost)
-        sportswear[iter]--;
     for(const auto& iter : reserve)
-        sportswear[iter]++;
+        arr[iter]++;
+    for(const auto& iter : lost)
+        arr[iter]--;
+    for(int i = 1; i <= n; i++)
+    {
+        if(arr[i] < 0 && arr[i-1] >= 1)
+            arr[i] += 1, arr[i-1] -= 1;
+        if(arr[i] < 0 && arr[i+1] >= 1)
+            arr[i] += 1, arr[i+1] -= 1;
+    }
     int answer = 0;
     for(int i = 1; i <= n; i++)
     {
-        if(sportswear[i] >= 1 && sportswear[i-1] < 0)
-        {
-            sportswear[i-1] += 1;
-            sportswear[i] -= 1;
-        }
-        if(sportswear[i] >= 1 && sportswear[i+1] < 0)
-        {
-            sportswear[i+1] += 1;
-            sportswear[i] -= 1;
-        }
+       if(arr[i]>=0)
+           answer++;
     }
-    for(int i = 1; i <= n; i++)
-        if(sportswear[i] >= 0)
-            answer++;
     return answer;
 }
