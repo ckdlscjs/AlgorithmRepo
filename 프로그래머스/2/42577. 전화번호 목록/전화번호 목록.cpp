1c36f03ecd5fd7d04d13ea9a08667e371d5509c7
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2024-10-14 20:32:15 +0900

    [level 2] Title: 전화번호 목록, Time: 189.55 ms, Memory: 55.8 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.cpp
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.cpp
@@ -1,25 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
-
 bool solution(vector<string> phone_book) 
 {
-    std::sort(phone_book.begin(), phone_book.end(), std::less<>());
-    std::unordered_set<std::string> us;
+    std::sort(phone_book.begin(), phone_book.end(), [](const std::string& a, const std::string& b)
+              {
+                  return a.size() < b.size();
+              });
+    std::unordered_set<std::string> chks;
+    std::string temp = "";
     for(const auto& iter : phone_book)
     {
-        if(us.size())
+        for(int i = 0; i < iter.size(); i++)
         {
-            std::string it = "";
-            for(int i = 0; i < iter.size(); i++)
-            {
-                it += iter[i];
-                if(us.size() && us.find(it) != us.end())
-                {
-                    return false;
-                }
-            }
+            temp += iter[i];
+            if(chks.find(temp) != chks.end())
+                return false;
         }
-        us.insert(iter);
+        chks.insert(iter);
+        temp.clear();
     }
     return true;
 }
