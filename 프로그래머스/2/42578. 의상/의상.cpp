2e59abf46b3e2216386a52753f4322c3d35a5c9b
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2024-10-17 07:03:42 +0900

    [level 2] Title: 의상, Time: 0.03 ms, Memory: 4.12 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.cpp
+++ 프로그래머스/2/42578. 의상/의상.cpp
@@ -1,14 +1,13 @@
+//https://school.programmers.co.kr/learn/courses/30/lessons/42578
 #include <bits/stdc++.h>
 using namespace std;
+std::unordered_map<std::string, std::vector<std::string>> categorys;
 int solution(vector<vector<string>> clothes) 
 {
-    std::unordered_map<std::string, std::vector<std::string>> hash_map;
+    for(const auto& iter : clothes)
+        categorys[iter[1]].push_back(iter[0]);
     int answer = 1;
-    for(int i = 0; i < clothes.size(); i++)
-        hash_map[clothes[i][1]].push_back(clothes[i][0]);
-        
-    for(const auto& iter : hash_map)
-        answer *= iter.second.size() + 1;
-    
-    return answer-1;
+    for(const auto& iter : categorys)
+        answer *= iter.second.size()+1;
+    return answer - 1;
 }
