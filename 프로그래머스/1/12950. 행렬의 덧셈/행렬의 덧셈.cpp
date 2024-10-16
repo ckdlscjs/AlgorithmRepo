0f1590df6ed561a2fe28a7f578ed2f618f7df292
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2024-10-17 00:17:43 +0900

    [level 1] Title: 행렬의 덧셈, Time: 56.49 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.cpp
+++ 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.cpp
@@ -1,18 +1,11 @@
-#include <string>
-#include <vector>
-
+//https://school.programmers.co.kr/learn/courses/30/lessons/12950
+#include <bits/stdc++.h>
 using namespace std;
 
-vector<vector<int>> solution(vector<vector<int>> arr1, vector<vector<int>> arr2) {
-    vector<vector<int>> answer;
+vector<vector<int>> solution(vector<vector<int>> arr1, vector<vector<int>> arr2) 
+{
     for(int i = 0; i < arr1.size(); i++)
-    {
-        std::vector<int> ans;
         for(int j = 0; j < arr1[0].size(); j++)
-        {
-            ans.push_back(arr1[i][j] + arr2[i][j]);
-        }
-        answer.push_back(ans);
-    }
-    return answer;
+            arr1[i][j] += arr2[i][j];
+    return arr1;
 }
