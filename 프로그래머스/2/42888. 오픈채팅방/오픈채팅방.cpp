b21d954d9fbf5a76069e61d2d45bab7152880813
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2024-10-17 06:07:18 +0900

    [level 2] Title: 오픈채팅방, Time: 107.67 ms, Memory: 56.3 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.cpp
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.cpp
@@ -1,36 +1,34 @@
+//https://school.programmers.co.kr/learn/courses/30/lessons/42888
 #include <bits/stdc++.h>
 using namespace std;
+std::vector<std::pair<std::string, std::string>> results;
+std::unordered_map<std::string, std::string> db;
 vector<string> solution(vector<string> record) 
 {
-    std::unordered_map<std::string, std::string> map_id;
     for(auto iter : record)
     {
-        std::string order = iter.substr(0, iter.find(' '));
-        iter.erase(0, iter.find(' ') + 1);
-        std::string id = iter.substr(0, iter.find(' '));
-        iter.erase(0, iter.find(' ') + 1);
-        std::string nickname = iter;
+        int it = iter.find(' ');
+        std::string order = iter.substr(0, it); 
+        iter = iter.substr(it+1);
+        
+        it = iter.find(' ');
+        std::string uid = iter.substr(0, it); 
+        iter = iter.substr(it+1);
         if(order == "Enter" || order == "Change")
-        {
-            map_id[id] = nickname;
-        }
+            db[uid] = iter;
+        if(order == "Change")
+            continue;
+        results.push_back({uid, order});
     }
     vector<string> answer;
-    for(auto iter : record)
+    std::string str;
+    for(const auto& iter : results)
     {
-        std::string order = iter.substr(0, iter.find(' '));
-        iter.erase(0, iter.find(' ') + 1);
-        std::string id = iter.substr(0, iter.find(' '));
-        iter.erase(0, iter.find(' ') + 1);
-        std::string str = "";
-        str += map_id[id];
-        if(order == "Enter")
-            str += "님이 들어왔습니다.";
-        else if(order == "Leave")
-            str += "님이 나갔습니다.";
-        else
-            continue;
+        str += db[iter.first];
+        str += "님이 ";
+        str += iter.second == "Enter" ? "들어왔습니다." : "나갔습니다.";
         answer.push_back(str);
+        str.clear();
     }
     return answer;
 }
