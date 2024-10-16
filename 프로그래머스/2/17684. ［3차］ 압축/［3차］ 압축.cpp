5f97b17758f98905e448a59a64ac170c5ff95aad
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2024-10-17 08:47:25 +0900

    [level 2] Title: [3차] 압축, Time: 0.33 ms, Memory: 4.02 MB -BaekjoonHub

--- 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.cpp
+++ 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.cpp
@@ -4,37 +4,31 @@ using namespace std;
 std::unordered_map<std::string, int> words;
 vector<int> solution(string msg) 
 {
-    int idx = 1;
-    for(; idx <= 26; idx++)
-    {
-        std::string str = "A";
-        str[0] += idx-1;
-        words[str] = idx;
-    }
-    vector<int> answer;
     int len = 1;
+    for(int i = 0; i < 26; i++)
+        words[std::string(1, 'A' + i)] = words.size()+1;
+    vector<int> answer;
     while(msg.size())
     {
-        std::string word = "";
+        std::string w = "";
         for(int i = len; i >= 1; i--)
         {
             if(msg.size() < i)
                 continue;
-            word = msg.substr(0, i);
-            if(words.find(word) != words.end())
+            w = msg.substr(0, i);
+            if(words.find(w) != words.end())
             {
                 msg = msg.substr(i);
                 break;
             }
         }
-        answer.push_back(words[word]);
+        answer.push_back(words[w]);
         if(msg.size())
         {
-            word += msg[0];
-            len = std::max(len, (int)word.size());
-            words[word] = idx++;
+            w += msg[0];
+            len = std::max(len, (int)w.size());
+            words[w] = words.size()+1;
         }
     }
-    
     return answer;
 }
