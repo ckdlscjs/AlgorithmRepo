15bfea5ed64aeaedf71f5063245e6c61dd174cbf
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2024-10-17 00:13:34 +0900

    [level 1] Title: 소수 만들기, Time: 0.04 ms, Memory: 4.22 MB -BaekjoonHub

--- 프로그래머스/1/12977. 소수 만들기/소수 만들기.cpp
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.cpp
@@ -1,17 +1,17 @@
-#include <vector>
-#include <iostream>
+//https://school.programmers.co.kr/learn/courses/30/lessons/12977
+#include <bits/stdc++.h>
 using namespace std;
-bool NotPrime[3030];
-int solution(vector<int> nums) {
-    int answer = 0;
-    for(int i = 2; i < 3030; i++)
+bool NotPrime[3000];
+int solution(vector<int> nums) 
+{
+    for(int i = 2; i <= std::sqrt(3'000); i++)
     {
         if(NotPrime[i])
             continue;
-        for(int j = i + i; j < 3030; j += i)
-            NotPrime[j] = true;
+        for(int j = 2; i * j <= 3000; j++)
+            NotPrime[i*j] = true;
     }
-    
+    int answer = 0;
     for(int i = 0; i < nums.size(); i++)
     {
         for(int j = i + 1; j < nums.size(); j++)
@@ -23,6 +23,5 @@ int solution(vector<int> nums) {
             }
         }
     }
-
     return answer;
 }
