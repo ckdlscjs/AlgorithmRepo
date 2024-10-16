a5bc87b3c7303e891d96e90d46e5fe208141e426
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2024-10-16 09:15:44 +0900

    [Silver II] Title: 나무 자르기, Time: 136 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.cc
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.cc
@@ -1,35 +1,31 @@
 #include <bits/stdc++.h>
-const int MaxN = 1000005;
-int n, m, ans, left, right;
-int arr[MaxN];
-int main(void)
+
+using namespace std;
+int arr[1'000'005];
+int n, m;
+int main() 
 {
-    std::ios::sync_with_stdio(false);
-    std::cin.tie(0);
-    std::cout.tie(0);
-    std::cin >> n >> m;
-    for(int i = 0; i < n; i++)
-    {
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(0);
+  std::cout.tie(0);
+  std::cin >> n >> m;
+  int lo = 0, hi = 0;
+  for(int i = 0; i < n; i++)
+  {
       std::cin >> arr[i];
-      right = std::max(right, arr[i]);
-    }
-    while(left <= right)
-    {
-      int mid = (left + right) / 2;
-      long long int sum = 0;
-      for(int i = 0; i < n; i++)
-      {
-        if(arr[i] > mid)
-          sum += arr[i] - mid;
-      }
-      if(sum >= m)
-      {
-        left = mid + 1;
-        ans = std::max(ans, mid);
-      }
-      else
-        right = mid - 1;
-    }
-    std::cout << ans;
-    return 0;
+      hi = std::max(hi, arr[i]);
+  }
+  hi += 1; 
+  while(lo + 1 < hi)
+  {
+    int mid = (lo + hi) / 2;
+    long long int sum = 0;
+    for(int i = 0; i < n; i++)
+      if(arr[i] > mid)
+        sum += arr[i] - mid;
+    if(m <= sum) lo = mid;
+    else hi = mid;
+  }
+  std::cout << lo;
+  return 0;
 }
