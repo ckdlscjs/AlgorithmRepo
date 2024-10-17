5c96f0188cb1472471eaf99655a938b2ebba0db5
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2024-10-18 01:32:23 +0900

    [Gold IV] Title: 드래곤 앤 던전, Time: 28 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/16434. 드래곤 앤 던전/드래곤 앤 던전.cc
+++ 백준/Gold/16434. 드래곤 앤 던전/드래곤 앤 던전.cc
@@ -1,50 +1,33 @@
 #include <bits/stdc++.h>
-using namespace std;
-long long int N, Hatk;
-struct d
+long long int n, atk, t, a, h, curhp, maxhp;
+int main()
 {
-  long long int t;
-  long long int a;
-  long long int h;
-};
-d dungeons[123'460];
-bool Check(long long int maxHP)
-{
-  long long int curAtk = Hatk;
-  long long int curHP = maxHP;
-  for(int i = 0; i < N; i++)
-  {
-    if(dungeons[i].t == 1)
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(0);
+    std::cout.tie(0);
+    std::cin >> n >> atk;
+    for(int i = 0; i < n; i++)
     {
-      long long int cntP = dungeons[i].h / curAtk + (dungeons[i].h % curAtk ? 1 : 0);
-      long long int cntE = curHP / dungeons[i].a + (curHP % dungeons[i].a ? 1 : 0);
-      if(cntE < cntP)
-        return false;
-      curHP -= dungeons[i].a * (cntP - 1);
+        std::cin >> t >> a >> h;
+        if(t == 1)
+        {
+            long long int d = a * (std::ceil((double)h / atk) - 1);
+            if(curhp < d)
+            {
+                maxhp += d - curhp;
+                curhp = 0;
+            }
+            else
+            {
+                curhp -= d;
+            }
+        }
+        else
+        {
+            atk += a;
+            curhp = std::min(maxhp, curhp + h);
+        }
     }
-    else
-    {
-      curAtk += dungeons[i].a;
-      curHP = std::min(maxHP, curHP + dungeons[i].h);
-    }
-  }
-  return true;
-}
-int main() 
-{
-  std::ios::sync_with_stdio(false);
-  std::cin.tie(0);
-  std::cout.tie(0);
-  std::cin >> N >> Hatk;
-  for(int i = 0; i < N; i++)
-    std::cin >> dungeons[i].t >> dungeons[i].a >> dungeons[i].h;
-  long long int lo = 0, hi = 123'460'000'000'000'005; //1e18 + 5
-  while(lo + 1 < hi)
-  {
-    long long int mid = (lo + hi) / 2;
-    if(Check(mid)) hi = mid;
-    else lo = mid;
-  }
-  std::cout << hi;
-  return 0;
+    std::cout << maxhp + 1;
+    return 0;
 }
