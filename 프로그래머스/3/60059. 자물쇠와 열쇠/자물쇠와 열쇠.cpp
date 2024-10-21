b383f88df1a1c647424d475cba76c8003a0a425e
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2024-10-21 22:52:06 +0900

    [level 3] Title: 자물쇠와 열쇠, Time: 15.75 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/3/60059. 자물쇠와 열쇠/자물쇠와 열쇠.cpp
+++ 프로그래머스/3/60059. 자물쇠와 열쇠/자물쇠와 열쇠.cpp
@@ -11,31 +11,29 @@ std::vector<std::vector<int>> RotateKey(vector<vector<int>>& key)
 }
 bool Check(const vector<vector<int>>& key, const int& y, const int& x, vector<vector<int>> lock)
 {
-    int k = key.size()-1;
     for(int i = 0; i < key.size(); i++)
         for(int j = 0; j < key.size(); j++)
             lock[y + i][x + j] += key[i][j];
     
-    for(int i = 0; i < lock.size() - k * 2; i++)
-        for(int j = 0; j < lock.size() - k * 2; j++)
-            if(lock[k + i][k + j] != 1) 
+    for(int i = 0; i < lock.size() - key.size() * 2; i++)
+        for(int j = 0; j < lock.size() - key.size() * 2; j++)
+            if(lock[key.size() + i][key.size() + j] != 1) 
                 return false;
     return true;
 }
 bool solution(vector<vector<int>> key, vector<vector<int>> lock) 
 {
-    int k = key.size()-1;
-    int b = lock.size() + k * 2;
+    int b = lock.size() + key.size() * 2;
     std::vector<std::vector<int>> arr(b, std::vector<int>(b, 0));
     
     for(int i = 0; i < lock.size(); i++)
         for(int j = 0; j < lock.size(); j++)
-            arr[k + i][k + j] = lock[i][j];
+            arr[key.size() + i][key.size() + j] = lock[i][j];
     
     for(int r = 0; r < 4; r++)
     {
-        for(int offset_i = 0; offset_i < b - k; offset_i++)
-            for(int offset_j = 0; offset_j < b - k; offset_j++)
+        for(int offset_i = 0; offset_i < b - key.size(); offset_i++)
+            for(int offset_j = 0; offset_j < b - key.size(); offset_j++)
                 if(Check(key, offset_i, offset_j, arr))
                     return true;
         key = RotateKey(key);
