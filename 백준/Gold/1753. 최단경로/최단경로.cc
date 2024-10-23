6240dc2501c3c1bce893889f870a8c03d441c9e4
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2024-10-24 06:10:28 +0900

    [Gold IV] Title: 최단경로, Time: 116 ms, Memory: 9184 KB -BaekjoonHub

---
+++ 백준/Gold/1753. 최단경로/최단경로.cc
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+const int INF = 987654321;
+int V, E, K, u, v, w;
+int costs[20'005];
+std::vector<std::pair<int, int>> graph[20'005];
+int main()
+{
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(0);
+    std::cout.tie(0);
+    std::cin >> V >> E >> K;
+    for(int i = 0; i < E; i++)
+    {
+      std::cin >> u >> v >> w;
+      graph[u].push_back({v, w});
+    }
+    std::fill(costs, costs+20'005, INF);
+    std::priority_queue<std::pair<int, int>, std::vector<std::pair<int, int>>, std::greater<std::pair<int, int>>> pq;
+    pq.push({0, K});
+    costs[K] = 0;
+    while(pq.size())
+    {
+      int w = pq.top().first;
+      int u = pq.top().second;
+      pq.pop();
+      if(w > costs[u])
+        continue;
+      for(const auto& iter : graph[u])
+      {
+        int v = iter.first;
+        int nw = iter.second;
+        if(costs[u] + nw < costs[v])
+        {
+          costs[v] = costs[u] + nw;
+          pq.push({costs[v], v});
+        }
+      }
+    }
+    for(int i = 1; i <= V; i++)
+      if(costs[i] == INF)
+        std::cout << "INF" << '\n';
+      else
+        std::cout << costs[i] << '\n';
+    return 0;
+}
