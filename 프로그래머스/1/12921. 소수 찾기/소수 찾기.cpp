45313c4a16459538c14d39c6d1b63ea4e5c4a651
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2024-10-24 05:04:59 +0900

    [level 1] Title: 소수 찾기, Time: 3.25 ms, Memory: 4.46 MB -BaekjoonHub

--- 프로그래머스/1/12921. 소수 찾기/소수 찾기.cpp
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.cpp
@@ -1,20 +1,18 @@
-#include <string>
-#include <vector>
+//https://school.programmers.co.kr/learn/courses/30/lessons/12921
+#include <bits/stdc++.h>
 
 using namespace std;
-const int MaxN = 1000005;
-int NotPrime[MaxN];
+bool NotPrime[1'000'005];
 int solution(int n) 
-{   
+{
     int answer = 0;
     for(int i = 2; i <= n; i++)
     {
         if(NotPrime[i])
             continue;
         answer++;
-        for(int j = i + i; j <= n; j += i)
-            NotPrime[j] = true;
+        for(int j = 2; i * j <= n; j++)
+            NotPrime[i*j] = true;
     }
-    
     return answer;
 }
