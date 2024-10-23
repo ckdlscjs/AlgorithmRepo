186a719ecd63397a925f8091d75845e72d444975
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2024-10-23 16:24:06 +0900

    [level 3] Title: 기지국 설치, Time: 0.26 ms, Memory: 3.98 MB -BaekjoonHub

--- 프로그래머스/3/12979. 기지국 설치/기지국 설치.cpp
+++ 프로그래머스/3/12979. 기지국 설치/기지국 설치.cpp
@@ -1,34 +1,35 @@
+//https://school.programmers.co.kr/learn/courses/30/lessons/12979
 #include <bits/stdc++.h>
 using namespace std;
-std::vector<std::pair<int, int>> waves;
-
-int solution(int n, vector<int> stations, int w) {
+std::queue<std::pair<int, int>> q;
+int solution(int n, vector<int> stations, int w)
+{
     int answer = 0;
-    waves.clear();  // waves 초기화
-
-    // 각 기지국의 커버 범위를 waves에 추가
-    for(int i = 0; i < stations.size(); i++) {
-        waves.push_back({stations[i] - w, stations[i] + w});
+    std::sort(stations.begin(), stations.end(), std::less<>());
+    for(const auto& iter : stations)
+        q.push({iter-w, std::min(iter+w, n)});
+    q.push({n+1, n+1});
+    int l = 0, r = 0;
+    if(q.front().first > 1)
+    {
+        int cnt = (q.front().first - r - 1) / (w*2+1) + ((q.front().first - r - 1) % (w*2+1) ? 1 : 0);
+        answer += cnt;
+        l = 1;
+        r = (w*2 + 1) * cnt;
     }
-
-    w = w * 2 + 1; // 기지국 하나가 커버할 수 있는 범위
-    int start = 1; // 첫 구간 시작
-
-    for(const auto& iter : waves) {
-        // 현재 기지국 범위가 시작점보다 클 때만 계산
-        if (iter.first > start) {
-            int amount = iter.first - start;  // 커버되지 않은 구간
-            answer += (amount + w - 1) / w;   // 올림 계산하여 기지국 설치
+    
+    while(q.size())
+    {
+        std::pair<int, int> cur = q.front();
+        q.pop();
+        if(cur.first > r)
+        {
+            answer += (cur.first - r - 1) / (w*2+1) + ((cur.first-r-1) % (w*2+1) ? 1 :0);
+            l = cur.first;
+            r = cur.second;
         }
-        // 시작점을 현재 기지국 범위의 끝으로 업데이트
-        start = iter.second + 1;
+        else
+            r = cur.second;
     }
-
-    // 마지막 기지국 이후 남은 구간 처리
-    if (start <= n) {
-        int amount = n - start + 1;
-        answer += (amount + w - 1) / w; // 남은 구간에 대한 처리
-    }
-
     return answer;
 }
