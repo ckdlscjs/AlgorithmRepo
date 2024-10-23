6da616bf71aaa3167e5e2cbaff0b89bb8c82cc12
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2024-10-23 13:51:27 +0900

    [level 2] Title: 예상 대진표, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.cpp
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.cpp
@@ -1,16 +1,14 @@
+//https://school.programmers.co.kr/learn/courses/30/lessons/12985
 #include <bits/stdc++.h>
 using namespace std;
-
 int solution(int n, int a, int b)
 {
-    a += n - 1;
-    b += n - 1;
-    int cnt = 0;
+    int answer = 0;
     while(a != b)
     {
-        cnt++;
-        a /= 2;
-        b /= 2;
+        answer++;
+        a = (a+1) >> 1;
+        b = (b+1) >> 1;
     }
-    return cnt;
+    return answer;
 }
