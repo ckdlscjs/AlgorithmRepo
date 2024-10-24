d27e684e3c1a01b2024aa1cd5480c6a5acfbb0bf
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2024-10-25 08:16:30 +0900

    [level 1] Title: 두 정수 사이의 합, Time: 0.01 ms, Memory: 3.59 MB -BaekjoonHub

--- 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.cpp
+++ 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.cpp
@@ -1,13 +1,12 @@
-#include <string>
-#include <vector>
+//https://school.programmers.co.kr/learn/courses/30/lessons/12912
+#include <bits/stdc++.h>
 
 using namespace std;
 
-long long solution(int a, int b) {
-    long long answer = 0;
-    int minval = std::min(a,b);
-    int maxval = std::max(a,b);
-    for(minval; minval <= maxval; minval++)
-        answer += minval;
-    return answer;
+long long solution(int a, int b) 
+{
+    long long int large = std::max(a, b);
+    long long int small = std::min(a, b);
+    
+    return (large-small+1) * (2*small + (large-small+1)-1) / 2;
 }
