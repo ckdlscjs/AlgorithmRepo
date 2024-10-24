45778e6dc62cced758f9a503abb8c4df4a2c1d38
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2024-10-24 16:56:11 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 8.69 ms, Memory: 4.22 MB -BaekjoonHub

---
+++ 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.cpp
@@ -0,0 +1,44 @@
+//https://school.programmers.co.kr/learn/courses/30/lessons/92335
+#include <bits/stdc++.h>
+using namespace std;
+bool IsPrime(long long int num)
+{
+    if(num < 2) return false;
+    for(int i = 2; i <= std::sqrt(num); i++)
+    {
+        if(num % i == 0)
+            return false;
+    }
+    return true;
+}
+int solution(int n, int k) 
+{
+    std::string num = "";
+    while(n)
+    {
+        num += std::to_string(n % k);
+        n /= k;
+    }
+    int answer = 0;
+    std::reverse(num.begin(), num.end());
+    std::string temp = "";
+    for(const auto& iter : num)
+    {
+        if(temp.empty() && iter == '0')
+            continue;
+        temp += iter;
+        if(temp.size() >= 2 && temp[temp.size()-1] == '0')
+        {
+            std::string numstr = temp.substr(0, temp.size() - 1);
+            if(numstr.find('0') == std::string::npos && IsPrime(std::stoll(numstr)))
+                answer++;
+            temp.clear();
+        }
+    }
+    if(temp.size())
+    {
+        if(temp.find('0') == std::string::npos && IsPrime(std::stoll(temp)))
+            answer++;
+    }
+    return answer;
+}
