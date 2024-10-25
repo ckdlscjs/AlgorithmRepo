fd29bcbcb01581defb071676140d9c015cfd4f1d
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2024-10-25 23:08:24 +0900

    [level 3] Title: 섬 연결하기, Time: 0.02 ms, Memory: 4.13 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.cpp
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.cpp
@@ -1,15 +1,16 @@
+//https://school.programmers.co.kr/learn/courses/30/lessons/42861
 #include <bits/stdc++.h>
 using namespace std;
 struct DisjointSet
 {
-    std::vector<int> parent;
-    std::vector<int> rank;
-    DisjointSet(int _size) : parent(_size, -1), rank(_size, 0) {}
+    std::vector<int> parents;
+    std::vector<int> ranks;
+    DisjointSet(int _size) : parents(_size, -1), ranks(_size, 0) {}
     int Find(int node)
     {
-        if(parent[node] == -1)
+        if(parents[node] == -1)
             return node;
-        return parent[node] = Find(parent[node]);
+        return parents[node] = Find(parents[node]);
     }
     void Union(int node1, int node2)
     {
@@ -17,33 +18,30 @@ struct DisjointSet
         int root2 = Find(node2);
         if(root1 == root2)
             return;
-        if(rank[root1] == rank[root2])
+        if(ranks[root1] == ranks[root2])
         {
-            rank[root1]++;
-            parent[root2] = root1;
+            ranks[root1] ++;
+            parents[root2] = root1;
         }
-        if(rank[root1] > rank[root2])
-            parent[root2] = root1;
+        if(ranks[root1] > ranks[root2])
+            parents[root2] = root1;
         else
-            parent[root1] = root2;
+            parents[root1] = root2;
     }
 };
 int solution(int n, vector<vector<int>> costs) 
 {
     DisjointSet ds(n);
-    std::sort(costs.begin(), costs.end(), [](const std::vector<int>& a, const std::vector<int>& b)
-              {
-                return a[2] < b[2];       
-              });
     int answer = 0;
+    std::sort(costs.begin(), costs.end(), [](const std::vector<int>& a, const std::vector<int>& b){
+        return a[2] < b[2];
+    });
     for(const auto& iter : costs)
     {
-        if(ds.Find(iter[0]) != ds.Find(iter[1]))
-        {
-            ds.Union(iter[0], iter[1]);
-            answer += iter[2];
-        }
+        if(ds.Find(iter[0]) == ds.Find(iter[1]))
+            continue;
+        ds.Union(iter[0], iter[1]);
+        answer += iter[2];
     }
-    
     return answer;
 }
