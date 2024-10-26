5299da52c91652eec329251501a7f5e825f55b76
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2024-10-26 10:55:01 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.cc
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.cc
@@ -10,8 +10,7 @@ int main()
     dp[0] = 0;
     dp[1] = 1;
     dp[2] = 3;
-    dp[3] = 5;
-    for(int i = 4; i <= n; i++)
+    for(int i = 3; i <= n; i++)
         dp[i] = (dp[i-1] + 2*dp[i-2])%10'007;
     std::cout << dp[n];
     return 0;
