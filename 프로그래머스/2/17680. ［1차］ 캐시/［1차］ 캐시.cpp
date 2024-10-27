6943844ace0c2df08f6f364bf8119ea11e421058
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2024-10-27 10:57:22 +0900

    [level 2] Title: [1차] 캐시, Time: 22.60 ms, Memory: 12.3 MB -BaekjoonHub

--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.cpp
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.cpp
@@ -1,42 +1,42 @@
-//https://school.programmers.co.kr/learn/courses/30/lessons/17680
 #include <bits/stdc++.h>
 using namespace std;
-std::unordered_set<std::string> chks;
-std::deque<std::string> cache;
+
+const int cacheHit = 1, cacheMiss = 5;
+
 int solution(int cacheSize, vector<string> cities) 
 {
+    if (cacheSize == 0) return cities.size() * cacheMiss; // 캐시 크기가 0인 경우 모두 cacheMiss입니다.
+    
     int answer = 0;
-    for(auto& iter : cities)
+    list<string> cache; // LRU 순서를 유지하는 리스트
+    unordered_map<string, list<string>::iterator> cacheMap; // 도시 이름을 리스트의 iterator와 매핑
+    
+    for (auto& city : cities) 
     {
-        for(auto& ch : iter)
+        // 대소문자 무시를 위해 소문자를 대문자로 변환
+        transform(city.begin(), city.end(), city.begin(), ::toupper);
+        
+        if (cacheMap.find(city) != cacheMap.end()) // cache hit
         {
-            if(ch >= 'a')
-                ch -= 32;
-        }
-        if(chks.find(iter) == chks.end())
+            // 해당 도시를 캐시의 맨 뒤로 이동
+            cache.erase(cacheMap[city]); 
+            cache.push_back(city);
+            cacheMap[city] = --cache.end();
+            answer += cacheHit;
+        } 
+        else // cache miss
         {
-            chks.insert(iter);
-            cache.push_back(iter);
-            if(cache.size() > cacheSize)
+            if (cache.size() == cacheSize) // 캐시가 가득 찬 경우
             {
-                chks.erase(cache.front());
+                // LRU 도시를 제거
+                cacheMap.erase(cache.front());
                 cache.pop_front();
             }
-            answer += 5;
-            continue;
-        }
-        else
-        {
-            std::stack<std::string> st;
-            while(cache.front() != iter)
-                st.push(cache.front()), cache.pop_front();
-            cache.push_back(cache.front());
-            cache.pop_front();
-            while(st.size())
-                cache.push_front(st.top()), st.pop();
-            answer+=1;
+            // 새로운 도시를 캐시에 추가
+            cache.push_back(city);
+            cacheMap[city] = --cache.end();
+            answer += cacheMiss;
         }
     }
-    
     return answer;
 }
