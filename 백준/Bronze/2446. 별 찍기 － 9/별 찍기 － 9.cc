a77efb734de9016773a98a22d4cecbe4566e84f6
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2024-10-29 22:32:44 +0900

    [Bronze III] Title: 별 찍기 - 9, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2446. 별 찍기 － 9/별 찍기 － 9.cc
+++ 백준/Bronze/2446. 별 찍기 － 9/별 찍기 － 9.cc
@@ -1,23 +1,21 @@
 #include <bits/stdc++.h>
-int n;
+int N;
 int main()
 {
   std::ios::sync_with_stdio(false);
-  std::cin.tie(0);
-  std::cout.tie(0);
-  std::cin >> n;
-  for(int i = 0; i < (n * 2) - 1; i++)
+  std::cin.tie(nullptr);
+  std::cout.tie(nullptr);
+  std::cin >> N;
+  for(int i = 0; i < N; i++)
   {
-    if(i < n)
-    {
-      for(int j = 0; j < i; j++) std::cout << " ";
-      for(int j = 0; j < (n*2) - 2 * i - 1; j++) std::cout << "*";
-    }
-    else
-    {
-      for(int j = 0; j < (n * 2) - 2 - i; j++) std::cout << " ";
-      for(int j = 0; j < (2 * i) - (n*2) + 3; j++) std::cout << "*";
-    }
+    for(int j = 0; j < i; j++) std::cout << ' '; 
+    for(int j = 0; j <= N*2 - (i+1)*2; j++) std::cout << '*';
+    std::cout << '\n';
+  }
+  for(int i = 1; i < N; i++)
+  {
+    for(int j = 0; j < N - i - 1; j++) std::cout << ' '; 
+    for(int j = 0; j <= i*2; j++) std::cout << '*';
     std::cout << '\n';
   }
   return 0;
