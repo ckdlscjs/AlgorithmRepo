43a9a231b14c0c06cefb4dba84f274db6c5abc79
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2024-10-31 04:46:53 +0900

    [Bronze II] Title: Strfry, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/11328. Strfry/Strfry.cc
+++ 백준/Bronze/11328. Strfry/Strfry.cc
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 int N;
 std::string a, b;
+int alp1[26], alp2[26];
 int main()
 {
   std::ios::sync_with_stdio(false);
@@ -9,10 +10,18 @@ int main()
   std::cin >> N;
   for(int i = 0; i < N; i++)
   {
+    std::memset(alp1, 0, sizeof(alp1));
+    std::memset(alp2, 0, sizeof(alp2));
     std::cin >> a >> b;
-    std::sort(a.begin(), a.end());
-    std::sort(b.begin(), b.end());
-    std::cout << (a == b ? "Possible" : "Impossible") << '\n';
+    for(const auto& ch : a)
+      alp1[ch-'a']++;
+    for(const auto& ch : b)
+      alp2[ch-'a']++;
+    bool chk = true;
+    for(int j = 0; j < 26; j++)
+      if(alp1[j] != alp2[j])
+        chk = false;
+    std::cout << (chk ? "Possible" : "Impossible") << '\n';
   }
   return 0;
 }
