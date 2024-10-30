e38f1433eb6109bb8726112aeb31cee8051d2b78
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2024-10-30 18:46:35 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 0.18 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.cpp
+++ 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.cpp
@@ -11,12 +11,14 @@ int solution(string str1, string str2)
             ch-=32;
     std::unordered_map<std::string, int> var_str1;
     std::unordered_map<std::string, int> var_str2;
+    std::unordered_set<std::string> var_total;
     for(int i = 0; i < str1.size()-1; i++)
     {
         std::string temp = str1.substr(i, 2);
         if(!('A' <= temp[0] && temp[0] <= 'Z') || !('A' <= temp[1] && temp[1] <= 'Z'))
             continue;
         var_str1[temp]++;
+        var_total.insert(temp);
     }
     for(int i = 0; i < str2.size()-1; i++)
     {
@@ -24,24 +26,16 @@ int solution(string str1, string str2)
         if(!('A' <= temp[0] && temp[0] <= 'Z') || !('A' <= temp[1] && temp[1] <= 'Z'))
             continue;
         var_str2[temp]++;
+        var_total.insert(temp);
     }
     int val_intersection = 0, val_union = 0;
-    for(const auto& iter : var_str1)
+    for(const auto& iter : var_total)
     {
-        if(var_str2.find(iter.first) == var_str2.end())
-            val_union += iter.second;
-        else
-        {
-            val_intersection += std::min(iter.second, var_str2[iter.first]);
-            val_union += std::max(iter.second, var_str2[iter.first]);
-        }
-    }
-    for(const auto& iter : var_str2)
-    {
-        if(var_str1.find(iter.first) == var_str1.end())
-            val_union += iter.second;
+        if(var_str1[iter] && var_str2[iter])
+            val_intersection += std::min(var_str1[iter], var_str2[iter]);
+        val_union += std::max(var_str1[iter], var_str2[iter]);
     }
     if(val_intersection == val_union)
-        return 65536;
+        return 65'536;
     return (int)((double)val_intersection / (double)val_union * 65'536);
 }
