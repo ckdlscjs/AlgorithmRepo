9e8219e758c10143b7031b5bfd9824aa89b90311
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2024-11-02 16:44:09 +0900

    [Silver II] Title: 랜선 자르기, Time: 4 ms, Memory: 2100 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.cc
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.cc
@@ -1,40 +1,30 @@
-//https://www.acmicpc.net/problem/1654
-#pragma once
 #include <bits/stdc++.h>
-int K, N;
-std::vector<long long int> wires;
+long long int arr[10'005];
+long long int K, N;
+bool Check(long long int mid)
+{
+    long long int cnt = 0;
+    for(int i = 0; i < K; i++)
+    {
+        cnt += arr[i] / mid;
+    }
+    return cnt >= N;
+}
 int main(void)
 {
-	std::ios::sync_with_stdio(false);
-	std::cin.tie(0);
-	std::cout.tie(0);
-	long long int left = 1;
-	long long int right = 0;
-	std::cin >> K >> N;
-	while (K--)
-	{
-		long long int wire;
-		std::cin >> wire;
-		right = std::max(right, wire);
-		wires.push_back(wire);
-	}
-	long long int result = 0;
-	while (left <= right)
-	{
-		int count = 0;
-		long long int mid = (left + right) / 2;
-		for (const auto& wire : wires)
-		{
-			count += wire / mid;
-		}
-		if (N <= count)
-		{
-			left = mid + 1;
-			result = std::max(result, mid);
-		}
-		else
-			right = mid - 1;
-	}
-	std::cout << result;
-	return 0;
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(0);
+    std::cout.tie(0);
+    std::cin >> K >> N;
+    for(int i = 0; i < K; i++)
+      std::cin >> arr[i];
+    long long int lo = 0, hi = 1e18;
+    while(lo + 1 < hi)
+    {
+        long long int mid = (lo + hi) / 2;
+        if(Check(mid)) lo = mid;
+        else hi = mid;
+    }
+    std::cout << lo;
+    return 0;
 }
