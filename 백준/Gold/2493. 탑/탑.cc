155c37bb9e18f8a8f9272ec1e775aed14d8c98f6
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2024-11-04 11:20:36 +0900

    [Gold V] Title: 탑, Time: 96 ms, Memory: 8260 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.cc
+++ 백준/Gold/2493. 탑/탑.cc
@@ -1,20 +1,38 @@
 #include <bits/stdc++.h>
-using namespace std;
-std::stack<std::pair<int, int>> st;
-int n;
-int main() {
+int N;
+int height;
+int main(void)
+{
   std::ios::sync_with_stdio(false);
   std::cin.tie(0);
   std::cout.tie(0);
-  std::cin >> n;
-  for(int i = 1; i <= n; i++)
+  std::cin >> N;
+  std::stack<std::pair<int, int>> st;
+  std::vector<int> answer(N, 0);
+  for(int i = 0; i < N; i++)
   {
-    int height;
     std::cin >> height;
-    while(st.size() && st.top().first < height)
-      st.pop();
-    std::cout << (st.empty() ? 0 : st.top().second) <<' ';
-    st.push({height, i});
+    if(st.empty())
+    {
+      st.push({height, i+1});
+    }
+    else
+    {
+      if(st.top().first >= height)
+      {
+        answer[i] = st.top().second;
+      }
+      while(st.size() && st.top().first < height)
+      {
+        st.pop();
+      }
+      if(st.size())
+        answer[i] = st.top().second;
+      st.push({height, i+1});
+    }
   }
+  for(const auto& iter : answer)
+    std::cout << iter << ' ';
+  
   return 0;
 }
