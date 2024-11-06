6b8432b08432bcd95f5f0c0eced47d77367611b0
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2024-11-06 10:30:36 +0900

    [level 3] Title: [카카오 인턴] 경주로 건설, Time: 0.15 ms, Memory: 4.19 MB -BaekjoonHub

--- 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.cpp
+++ 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.cpp
@@ -1,44 +1,36 @@
+//https://school.programmers.co.kr/learn/courses/30/lessons/67259
 #include <bits/stdc++.h>
 using namespace std;
 const int dy[] = {0, 0, 1, -1};
 const int dx[] = {1, -1, 0, 0};
-int costs[27][27][4];
+int costs[30][30][4];
 int solution(vector<vector<int>> board) 
 {
-    costs[0][0][0] = 1;
-    costs[0][0][2] = 1;
-    std::queue<std::pair<std::pair<int,int>, int>> q;
-    q.push({{0, 0}, 0});
-    q.push({{0, 0}, 2});
+    int n = board.size();
+    std::queue<std::pair<std::pair<int, int>, int>> q;
+    std::fill_n(&costs[0][0][0], 30*30*4, 987654321);
+    costs[0][0][0] = costs[0][0][1] = costs[0][0][2] = costs[0][0][3] = 1;
+    q.push({{0, 0}, 0}); q.push({{0, 0}, 2});
     while(q.size())
     {
         int y = q.front().first.first;
         int x = q.front().first.second;
         int cur = q.front().second;
-        
         q.pop();
         for(int dir = 0; dir < 4; dir++)
         {
             int ny = y + dy[dir];
             int nx = x + dx[dir];
-            if(ny < 0 || nx < 0 || ny >= board.size() || nx >= board.size() || board[ny][nx])
+            if(ny < 0 || nx < 0 || ny >= n || nx >= n || board[ny][nx])
                 continue;
-            int corner = 0;
-            if((cur < 2 && dir >= 2) || (cur >= 2 && dir < 2))
-                corner += 5;
-            if(!costs[ny][nx][dir] || costs[y][x][cur] + 1 + corner < costs[ny][nx][dir])
+            int corner = (dir != cur ? 5 : 0);
+            if(costs[y][x][cur] + 1 + corner < costs[ny][nx][dir])
             {
                 costs[ny][nx][dir] = costs[y][x][cur] + 1 + corner;
-                q.push({{ny,nx},dir});
-                //std::cout << ny << ","<<nx<<":"<<dir <<" " << costs[ny][nx][dir] <<'\n';
+                q.push({{ny, nx}, dir});
             }
         }
     }
-    int answer = 987654321;
-    for(int dir = 0; dir < 4; dir++)
-        if(!costs[board.size()-1][board.size()-1][dir])
-            continue;
-        else
-            answer = std::min(costs[board.size()-1][board.size()-1][dir], answer);
-    return (answer - 1) * 100;
+    
+    return (std::min({costs[n-1][n-1][0], costs[n-1][n-1][1], costs[n-1][n-1][2], costs[n-1][n-1][3]}) - 1) * 100;
 }
