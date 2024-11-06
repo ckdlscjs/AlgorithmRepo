c7b5cb0669067c524ce6c4f39d4d475f2c4835e3
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2024-11-06 21:58:02 +0900

    [Gold IV] Title: 최단경로, Time: 88 ms, Memory: 9184 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.cc
+++ 백준/Gold/1753. 최단경로/최단경로.cc
@@ -1,42 +1,43 @@
 #include <bits/stdc++.h>
-const int INF = 987654321;
+const int INF = 98765421;
 int V, E, K, u, v, w;
-int costs[20'005];
+std::priority_queue<std::pair<int, int>, std::vector<std::pair<int, int>>, std::greater<std::pair<int, int>>> pq;
 std::vector<std::pair<int, int>> graph[20'005];
-int main()
+int costs[20'005];
+int main(void)
 {
-    std::ios::sync_with_stdio(false);
-    std::cin.tie(0);
-    std::cout.tie(0);
-    std::cin >> V >> E >> K;
-    for(int i = 0; i < E; i++)
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(0);
+  std::cout.tie(0);
+  std::fill_n(costs, 20'005, INF);
+  std::cin >> V >> E >> K;
+  for(int i = 0; i < E; i++)
+  {
+    std::cin >> u >> v >> w;
+    graph[u].push_back({v, w});
+  }
+  pq.push({0, K});
+  costs[K] = 0;
+  while(pq.size())
+  {
+    int u = pq.top().second;
+    int w = pq.top().first;
+    pq.pop();
+    if(costs[u] < w)
+      continue;
+    for(const auto& iter : graph[u])
     {
-      std::cin >> u >> v >> w;
-      graph[u].push_back({v, w});
+      if(costs[u] + iter.second >= costs[iter.first]) continue;
+      costs[iter.first] = costs[u] + iter.second;
+      pq.push({costs[iter.first], iter.first});
     }
-    std::fill(costs, costs+20'005, INF);
-    std::priority_queue<std::pair<int, int>, std::vector<std::pair<int, int>>, std::greater<std::pair<int, int>>> pq;
-    pq.push({0, K});
-    costs[K] = 0;
-    while(pq.size())
-    {
-      int u = pq.top().second;
-      pq.pop();
-      for(const auto& iter : graph[u])
-      {
-        int v = iter.first;
-        int w = iter.second;
-        if(costs[u] + w < costs[v])
-        {
-          costs[v] = costs[u] + w;
-          pq.push({costs[v], v});
-        }
-      }
-    }
-    for(int i = 1; i <= V; i++)
-      if(costs[i] == INF)
-        std::cout << "INF" << '\n';
-      else
-        std::cout << costs[i] << '\n';
-    return 0;
+  }
+  for(int i = 1; i <= V; i++)
+  {
+    if(costs[i] == INF)
+      std::cout << "INF" << '\n';
+    else
+      std::cout << costs[i] << '\n';
+  }
+  return 0;
 }
