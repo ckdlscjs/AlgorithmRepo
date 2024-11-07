9b0629787001951b6a5b2a992b4e65a733947897
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2024-11-08 01:46:34 +0900

    [level 3] Title: 순위, Time: 2.06 ms, Memory: 4.16 MB -BaekjoonHub

--- 프로그래머스/3/49191. 순위/순위.cpp
+++ 프로그래머스/3/49191. 순위/순위.cpp
@@ -1,52 +1,44 @@
+//https://school.programmers.co.kr/learn/courses/30/lessons/49191
 #include <bits/stdc++.h>
 using namespace std;
-const int INF = 987654321;
+int costs[105][105];
 int solution(int n, vector<vector<int>> results) 
 {
-    int dist[105][105];
-    std::fill(&dist[0][0], &dist[104][105], INF);
-    for(int i = 1; i <= n; i++)
-        dist[i][i] = 0;
     for(const auto& iter : results)
     {
-        dist[iter[0]][iter[1]] = 1;
-        dist[iter[1]][iter[0]] = -1;
+        costs[iter[1]][iter[0]] = 1;
+        costs[iter[0]][iter[1]] = -1;
     }
-    for(int mid = 1; mid <= n; mid++)
+    for(int m = 1; m <= n; m++)
     {
         for(int i = 1; i <= n; i++)
         {
             for(int j = 1; j <= n; j++)
             {
-                if(dist[i][mid] == INF || dist[mid][j] == INF)
-                    continue;
-                if(dist[i][mid] == 1 && dist[mid][j] == 1)
+                if(!costs[i][m]) continue;
+                if(costs[i][m] == 1 && costs[m][j] == 1)
                 {
-                    dist[i][j] = 1;
-                    dist[j][i] = -1;
+                    costs[i][j] = 1;
+                    costs[j][i] = -1;
                 }
-                if(dist[i][mid] == -1 && dist[mid][j] == -1)
+                else if(costs[i][m] == -1 && costs[m][j] == -1)
                 {
-                    dist[i][j] = -1;
-                    dist[j][i] = 1;
+                    costs[i][j] = -1;
+                    costs[j][i] = 1;
                 }
             }
         }
-    }
+    }     
     int answer = 0;
     for(int i = 1; i <= n; i++)
     {
         bool chk = true;
         for(int j = 1; j <= n; j++)
         {
-            if(dist[i][j] == INF)
-                chk = false;
-            //std::cout << dist[i][j] << ' ';
+            if(i == j) continue;
+            if(!costs[i][j]) chk = false;
         }
-        //std::cout <<'\n';
-        if(chk)
-            answer++;
+        if(chk) answer++;
     }
-    
     return answer;
 }
