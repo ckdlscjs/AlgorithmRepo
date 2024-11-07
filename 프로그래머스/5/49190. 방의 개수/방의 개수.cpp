8122f7801eb351a481ad91dc92f9262e8046ea67
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2024-11-07 18:51:54 +0900

    [level 5] Title: 방의 개수, Time: 147.61 ms, Memory: 25.3 MB -BaekjoonHub

--- 프로그래머스/5/49190. 방의 개수/방의 개수.cpp
+++ 프로그래머스/5/49190. 방의 개수/방의 개수.cpp
@@ -1,43 +1,28 @@
+//https://school.programmers.co.kr/learn/courses/30/lessons/49190
 #include <bits/stdc++.h>
 const int dy[] = {-1, -1, 0, 1, 1, 1, 0, -1};
-const int dx[] = {0, 1, 1, 1, 0, -1, -1, -1};
-/*
-1.정점의 재방문
-2.정점의 재방문시 사용이 안된간선이어야함
-3.범위를 * 2로 두번씩 간선을 추가하면서 계산하여 중간의 교차지점을 섞는다 (모래시계형태)
-*/
+const int dx[] = {0, 1, 1, 1, 0 ,- 1, -1, -1};
 using namespace std;
 std::set<std::pair<int, int>> vertexs;
 std::set<std::pair<std::pair<int, int>, std::pair<int, int>>> edges;
 int solution(vector<int> arrows) 
 {
-    int answer = 0;
-    int y = 0, x = 0;
+    int y = 0, x = 0, answer = 0;
     vertexs.insert({y, x});
     for(const auto& dir : arrows)
     {
-       for(int cnt = 0; cnt < 2; cnt++)
-       {
-           int ny = y + dy[dir];
-           int nx = x + dx[dir];
-           if(vertexs.find({ny, nx}) == vertexs.end())
-           {
-               vertexs.insert({ny, nx});
-               edges.insert({{y, x}, {ny, nx}});
-               edges.insert({{ny, nx}, {y, x}});
-           }
-           else
-           {
-               if(edges.find({{y, x}, {ny, nx}}) == edges.end())
-               {
-                   answer++;
-                   edges.insert({{y, x}, {ny, nx}});
-                   edges.insert({{ny, nx}, {y, x}});
-               }
-           }
-           y = ny;
-           x = nx;
-       }
+        for(int i = 0; i < 2; i++)
+        {
+            int ny = y + dy[dir];
+            int nx = x + dx[dir];
+            if(vertexs.find({ny, nx}) != vertexs.end() && edges.find({{y, x}, {ny, nx}}) == edges.end())
+                answer++;
+            vertexs.insert({ny, nx});
+            edges.insert({{y, x}, {ny, nx}});
+            edges.insert({{ny, nx}, {y, x}});
+            y = ny;
+            x = nx;
+        }
     }
     
     return answer;
