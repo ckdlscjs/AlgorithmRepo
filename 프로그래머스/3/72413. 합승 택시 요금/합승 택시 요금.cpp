0d6c5c186727a415904170a022cb08904cfbcd02
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2024-11-08 10:48:39 +0900

    [level 3] Title: 합승 택시 요금, Time: 10.16 ms, Memory: 7.46 MB -BaekjoonHub

--- 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.cpp
+++ 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.cpp
@@ -2,41 +2,31 @@
 #include <bits/stdc++.h>
 using namespace std;
 const int INF = 987654321;
-std::vector<std::pair<int, int>> graph[205];
 int costs[205][205];
-void Dijkstra(int s)
-{
-    std::priority_queue<std::pair<int, int>, std::vector<std::pair<int, int>>, std::greater<std::pair<int, int>>> pq;
-    costs[s][s] = 0;
-    pq.push({0, s});
-    while(pq.size())
-    {
-        int u = pq.top().second;
-        pq.pop();
-        for(const auto& iter : graph[u])
-        {
-            int v = iter.first;
-            int w = iter.second;
-            if(costs[s][u] == INF || w == INF || costs[s][v] <= costs[s][u] + w) continue;
-            costs[s][v] = costs[s][u] + w;
-            pq.push({costs[s][v], v});
-        }
-    }
-}
 int solution(int n, int s, int a, int b, vector<vector<int>> fares) 
 {
+    std::fill_n(&costs[0][0], 205*205, INF);
+    for(int i = 1; i <= n; i++) costs[i][i] = 0;
     for(const auto& iter : fares)
     {
-        graph[iter[0]].push_back({iter[1], iter[2]});
-        graph[iter[1]].push_back({iter[0], iter[2]});
+        costs[iter[0]][iter[1]] = iter[2];
+        costs[iter[1]][iter[0]] = iter[2];
     }
-    std::fill_n(&costs[0][0], 205*205, INF);
-    Dijkstra(s);
-    Dijkstra(a);
-    Dijkstra(b);
+    for(int m = 1; m <= n; m++)
+    {
+        for(int u = 1; u <= n; u++)
+        {
+            for(int v = 1; v <= n; v++)
+            {
+                if(costs[u][m] == INF || costs[m][v] == INF) continue;
+                costs[u][v] = std::min(costs[u][v], costs[u][m] + costs[m][v]);
+            }
+        }
+    }
+   
     int answer = INT_MAX;
     for(int i = 1; i <= n; i++)
         if(costs[s][i] != INF && costs[a][i] != INF && costs[b][i] != INF)
-            answer = std::min(answer, costs[s][i] + costs[a][i] + costs[b][i]);
+            answer = std::min(answer, costs[s][i] + costs[i][a] + costs[i][b]);
     return answer;
 }
