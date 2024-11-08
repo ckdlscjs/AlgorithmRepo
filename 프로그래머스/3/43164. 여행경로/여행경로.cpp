2a72f1eff4f6b6ee868ae03e9942bbf18ca56e94
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2024-11-08 14:27:00 +0900

    [level 3] Title: 여행경로, Time: 0.04 ms, Memory: 4.22 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.cpp
+++ 프로그래머스/3/43164. 여행경로/여행경로.cpp
@@ -1,42 +1,27 @@
+//https://school.programmers.co.kr/learn/courses/30/lessons/43164
 #include <bits/stdc++.h>
 using namespace std;
-const int MaxN = 10005;
-unordered_map<string, int> idxs;
-vector<pair<string, bool>> graph[MaxN];
-
-void dfs(vector<string>& ans, string cur) 
+std::unordered_map<std::string, std::vector<std::pair<std::string, bool>>> graph;
+bool Check(const int counts, std::vector<std::string>& ans, std::string cur)
 {
-    while (!graph[idxs[cur]].empty()) 
-    {
-        auto next = graph[idxs[cur]].front();
-        graph[idxs[cur]].erase(graph[idxs[cur]].begin());
-        dfs(ans, next.first);
-    }
     ans.push_back(cur);
+    if(ans.size() > counts) return true;
+    for(auto& it : graph[cur])
+    {
+        if(it.second) continue;
+        it.second = true;
+        if(Check(counts, ans, it.first)) return true;
+        it.second = false;
+    }
+    ans.pop_back();
+    return false;
 }
-
 vector<string> solution(vector<vector<string>> tickets) 
 {
-    set<string> inputs;
-    for (const auto& iter : tickets) 
-    {
-        inputs.insert(iter[0]);
-        inputs.insert(iter[1]);
-    }
-    
-    int cnt = 0;
-    for (const auto& iter : inputs)
-        idxs[iter] = cnt++;
-    
-    for (const auto& iter : tickets)
-        graph[idxs[iter[0]]].push_back({iter[1], false});
-    
-    for (int i = 0; i < idxs.size(); i++)
-        sort(graph[i].begin(), graph[i].end(), std::less<>());
-
-    vector<string> answer;
-    dfs(answer, "ICN");
-    reverse(answer.begin(), answer.end());  // 역순으로 경로가 쌓이므로 역으로 뒤집어 줍니다.
-    
+    for(const auto& iter : tickets) graph[iter[0]].push_back({iter[1], false});
+    for(auto& it : graph)
+        std::sort(it.second.begin(), it.second.end(), std::less<>());
+    std::vector<std::string> answer;
+    Check(tickets.size(), answer, "ICN");
     return answer;
 }
