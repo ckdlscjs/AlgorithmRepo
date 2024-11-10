16acfe284ad2665619dec535989f52d5748d60e9
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2024-11-10 23:35:24 +0900

    [Silver III] Title: 피보나치 함수, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.cc
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+int T, N;
+std::unordered_map<int, std::pair<int, int>> fibos;
+std::pair<int, int> Fibo(int n)
+{
+  if(n <= 0)
+    return {1, 0};
+  if(n <= 1)
+    return {0, 1};
+  if(fibos.find(n) != fibos.end())
+    return fibos[n];
+  std::pair<int, int> ret1 = Fibo(n-1);
+  std::pair<int, int> ret2 = Fibo(n-2);
+  ret1.first += ret2.first;
+  ret1.second += ret2.second;
+  fibos[n] = ret1;
+  return fibos[n];
+}
+int main(void)
+{
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(0);
+  std::cout.tie(0);
+  std::cin >> T;
+  for(int t = 0; t < T; t++)
+  {
+    std::cin >> N;
+    std::pair<int, int> ret = Fibo(N);
+    std::cout << ret.first << ' ' << ret.second << '\n';
+  }
+ 
+  return 0;
+}
