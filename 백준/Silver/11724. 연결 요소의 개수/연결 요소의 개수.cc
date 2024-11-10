ca2dfd359df3bb586d632efc414d43a6f1113f55
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2024-11-10 11:58:45 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 68 ms, Memory: 2160 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.cc
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.cc
@@ -1,34 +1,46 @@
 #include <bits/stdc++.h>
 int N, M, u, v, cnt;
-std::vector<int> graph[1005];
-bool visited[1005];
-void DFS(int cur)
+struct DisjointSet
 {
-  visited[cur] = true;
-  for(const auto& iter : graph[cur])
+  std::vector<int> parents;
+  std::vector<int> ranks;
+  DisjointSet(int _size) : parents(_size+1, -1), ranks(_size+1, 0) {}
+  int Find(int node)
   {
-    if(visited[iter]) continue;
-    DFS(iter);
+    if(parents[node] == -1) return node;
+    return parents[node] = Find(parents[node]);
   }
-}
+  void Union(int node1, int node2)
+  {
+    int root1 = Find(node1);
+    int root2 = Find(node2);
+    if(root1 == root2) return;
+    if(ranks[root1] == ranks[root2])
+      ranks[root1]++, parents[root2] = root1;
+    else if(ranks[root1] < ranks[root2])
+      parents[root1] = root2;
+    else
+      parents[root2] = root1;
+  }
+};
 int main(void)
 {
   std::ios::sync_with_stdio(false);
   std::cin.tie(0);
   std::cout.tie(0);
   std::cin >> N >> M;
+  DisjointSet ds(N);
   for(int i = 0; i < M; i++)
   {
     std::cin >> u >> v;
-    graph[u].push_back(v);
-    graph[v].push_back(u);
+    if(ds.Find(u) == ds.Find(v)) continue;
+    ds.Union(u, v);
   }
+  std::unordered_set<int> group;
   for(int i = 1; i <= N; i++)
   {
-    if(visited[i]) continue;
-    DFS(i);
-    cnt++;
+    group.insert(ds.Find(i));
   }
-  std::cout << cnt;
+  std::cout << group.size();
   return 0;
 }
