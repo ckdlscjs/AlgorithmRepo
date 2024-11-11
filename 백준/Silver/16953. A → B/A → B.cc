afb3fc6b6feda682095a0f94045326b4d6073d97
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2024-11-11 19:49:41 +0900

    [Silver II] Title: A → B, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Silver/16953. A → B/A → B.cc
+++ 백준/Silver/16953. A → B/A → B.cc
@@ -1,5 +1,5 @@
 #include <bits/stdc++.h>
-long long int A, B;
+int A, B;
 std::unordered_map<long long int, int> dist;
 int main(void)
 {
@@ -8,20 +8,20 @@ int main(void)
   std::cout.tie(0);
   std::cin >> A >> B;
   std::queue<int> q;
-  dist[A] = 1;
-  q.push(A);
+  dist[B] = 1;
+  q.push(B);
   while(q.size())
   {
     long long int cur = q.front();
     q.pop();
-    if(cur * 2 <= 1e9 && dist.find(cur*2) == dist.end())
-      dist[cur*2] = dist[cur]+1, q.push(cur*2);
-    if(cur *10 + 1 <= 1e9 && dist.find(cur *10 + 1) == dist.end())
-      dist[cur *10 + 1] = dist[cur]+1, q.push(cur *10 + 1);
+    if(cur % 2 == 0 && dist.find(cur/2) == dist.end())
+      dist[cur/2] = dist[cur]+1, q.push(cur/2);
+    if(cur % 10 == 1 && dist.find(cur /10) == dist.end())
+      dist[cur / 10] = dist[cur]+1, q.push(cur /10);
   }
-  if(dist.find(B) == dist.end())
+  if(dist.find(A) == dist.end())
     std::cout << -1;
   else
-    std::cout << dist[B];
+    std::cout << dist[A];
   return 0;
 }
