6d2aa8f39107768d1824b4ecc78b719b8ed30d46
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2024-11-12 14:37:05 +0900

    [Gold V] Title: 토마토, Time: 96 ms, Memory: 8140 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.cc
+++ 백준/Gold/7569. 토마토/토마토.cc
@@ -1,82 +1,47 @@
-#pragma once
 #include <bits/stdc++.h>
-int N, M, H;
-const int MaxN = 105;
-const int dy[] = { 0, 0 ,1, -1, 0, 0 };
-const int dx[] = { 1, -1, 0, 0, 0, 0 };
-const int dz[] = { 0, 0, 0, 0, -1, 1 };
-int arr[MaxN][MaxN][MaxN];
-struct Pos
+const int dz[] = {0, 0, 0, 0, -1, 1};
+const int dy[] = {0, 0, 1, -1, 0, 0};
+const int dx[] = {1, -1, 0, 0, 0, 0};
+int N, M, H, cnt_zero, dist, arr[105][105][105];
+int main()
 {
-	int z;
-	int y;
-	int x;
-	Pos(int _z, int _y, int _x) : z(_z), y(_y), x(_x) {}
-};
-std::queue<Pos> tomatos;
-int count_all;
-int count_tomatos;
-int result = 0;
-int main(void)
-{
-	std::ios::sync_with_stdio(false);
-	std::cin.tie(0);
-	std::cout.tie(0);
-	std::cin >> M >> N >> H;
-	for (int k = 0; k < H; k++)
-	{
-		for (int i = 0; i < N; i++)
-		{
-			for (int j = 0; j < M; j++)
-			{
-				std::cin >> arr[k][i][j];
-				if (arr[k][i][j] == -1) continue;
-				count_all++;
-				if (arr[k][i][j] == 1)
-				{
-					tomatos.push(Pos(k, i, j));
-					count_tomatos++;
-				}
-			}
-		}
-	}
-	
-	if (count_all == tomatos.size())
-	{
-		std::cout << result;
-	}
-	else
-	{
-		while (tomatos.size() && count_tomatos != count_all)
-		{
-			std::queue<Pos> nexttomatos;
-			while (tomatos.size())
-			{
-				Pos cur = tomatos.front();
-				tomatos.pop();
-				for (int dir = 0; dir < 6; dir++)
-				{
-					int nz = cur.z + dz[dir];
-					int ny = cur.y + dy[dir];
-					int nx = cur.x + dx[dir];
-					if (ny < 0 || nx < 0 || nz < 0 || ny >= N || nx >= M || nz >= H || arr[nz][ny][nx])
-						continue;
-					nexttomatos.push(Pos(nz, ny, nx));
-					arr[nz][ny][nx] = 1;
-				}
-			}
-			result++;
-			while (nexttomatos.size())
-			{
-				count_tomatos++;
-				tomatos.push(nexttomatos.front());
-				nexttomatos.pop();
-			}
-		}
-		if (count_tomatos == count_all)
-			std::cout << result;
-		else
-			std::cout << -1;
-	}
-	return 0;
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(0);
+  std::cout.tie(0);
+  std::cin >> M >> N >> H;
+  std::queue<std::pair<int, std::pair<int, int>>> q;
+  for(int h = 0; h < H; h++)
+  {
+    for(int n = 0; n < N; n++)
+    {
+      for(int m = 0; m < M; m++)
+      {
+        std::cin >> arr[h][n][m];
+        if(arr[h][n][m] == 1)
+          q.push({h, {n, m}});
+        if(arr[h][n][m] == 0)
+          cnt_zero++;
+      }
+    }
+  }
+  while(q.size() && cnt_zero)
+  {
+    int cz = q.front().first;
+    int cy = q.front().second.first;
+    int cx = q.front().second.second;
+    q.pop();
+    for(int dir = 0; dir < 6; dir++)
+    {
+      int nz = cz + dz[dir];
+      int ny = cy + dy[dir];
+      int nx = cx + dx[dir];
+      if(nz < 0 || ny < 0 || nx < 0 || nz >= H || ny >= N || nx >= M || arr[nz][ny][nx] != 0) continue;
+      cnt_zero--;
+      arr[nz][ny][nx] = arr[cz][cy][cx] + 1;
+      q.push({nz,{ny, nx}});
+      dist = std::max(dist, arr[nz][ny][nx]);
+    }
+  }
+  std::cout << (cnt_zero ? -1 : std::max(0, dist-1));
+  return 0;
 }
