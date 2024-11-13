07d17feccb728774e07fd0da1492e27565c9c14e
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2024-11-13 15:51:36 +0900

    [Gold IV] Title: 빙산, Time: 72 ms, Memory: 3068 KB -BaekjoonHub

---
+++ 백준/Gold/2573. 빙산/빙산.cc
@@ -0,0 +1,87 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int dy[] = {0, 0, 1, -1};
+const int dx[] = {1, -1, 0, 0};
+int N, M, cnt, arr[305][305];
+bool visited[305][305];
+int main() 
+{
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(0);
+  std::cout.tie(0);
+  std::cin >> N >> M;
+  std::deque<std::pair<int, int>> dq;
+  for(int i = 0; i < N; i++)
+  {
+    for(int j = 0; j < M; j++)
+    {
+      std::cin >> arr[i][j];
+      if(arr[i][j])
+      {
+        dq.push_back({i, j});
+      }
+    }
+  }
+  bool split = false;
+  while(dq.size())
+  {
+    cnt++;
+    std::memset(visited, false, sizeof(visited));
+    int bfscnt = 0;
+    int len = dq.size();
+    std::vector<std::pair<int, std::pair<int, int>>> amounts;
+    for(int i = 0; i < len; i++)
+    {
+      std::pair<int, int> cur = dq.front();
+      dq.pop_front();
+      int amount = 0;
+      for(int dir = 0; dir < 4; dir++)
+      {
+        if(arr[cur.first][cur.second] == 0) break;
+        int ny = cur.first + dy[dir];
+        int nx = cur.second + dx[dir];
+        if(ny < 0 || nx < 0 || ny >= N || nx >= M || arr[ny][nx]) continue;
+        amount++;
+      }
+      amounts.push_back({amount, cur});
+      if(amount < arr[cur.first][cur.second])
+        dq.push_back(cur);
+    }
+    for(const auto& iter : amounts)
+    {
+      arr[iter.second.first][iter.second.second] = std::max(0, arr[iter.second.first][iter.second.second] - iter.first);
+    }
+    for(int i = 0; i < dq.size(); i++)
+    {
+      if(visited[dq[i].first][dq[i].second]) continue;
+      bfscnt++;
+      std::queue<std::pair<int, int>> q;
+      visited[dq[i].first][dq[i].second] = true;
+      q.push({dq[i].first, dq[i].second});
+      while(q.size())
+      {
+        int cy = q.front().first;
+        int cx = q.front().second;
+        q.pop();
+        for(int dir = 0; dir < 4; dir++)
+        {
+          int ny = cy + dy[dir];
+          int nx = cx + dx[dir];
+          if(ny < 0 || nx < 0 || ny >= N || nx >= M || arr[ny][nx] == 0 || visited[ny][nx]) continue;
+          q.push({ny, nx});
+          visited[ny][nx] = true;
+        }
+      }
+    }
+    if(bfscnt > 1)
+    {
+      split = true;
+      break;
+    }
+  }
+  if(split)
+    std::cout << cnt;
+  else
+    std::cout << 0;
+  return 0;
+}
