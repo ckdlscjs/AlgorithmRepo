a42a307530bb2b979eaaa089e3da7272cfbfe2e6
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2024-11-14 19:50:18 +0900

    [Gold III] Title: 다리 만들기, Time: 0 ms, Memory: 2632 KB -BaekjoonHub

--- 백준/Gold/2146. 다리 만들기/다리 만들기.cc
+++ 백준/Gold/2146. 다리 만들기/다리 만들기.cc
@@ -2,18 +2,16 @@
 using namespace std;
 const int dy[] = {0, 0, 1, -1};
 const int dx[] = {1, -1, 0, 0};
-int N, arr[105][105], dist[105][105], cdist[105][105];
-std::unordered_map<int, std::set<std::pair<int, int>>> starts;
+int N, arr[105][105], area[105][105], dist[105][105];
 void DFS(int y, int x, int c)
 {
-  dist[y][x] = c;
+  arr[y][x] = c;
   for(int dir = 0; dir < 4; dir++)
   {
     int ny = y + dy[dir];
     int nx = x + dx[dir];
-    if(ny < 0 || nx < 0 || ny >= N || nx >= N || dist[ny][nx]) continue;
+    if(ny < 0 || nx < 0 || ny >= N || nx >= N || arr[ny][nx] == 0) continue;
     if(arr[ny][nx] == 1) DFS(ny, nx, c);
-    else starts[c].insert({ny, nx});
   }
 }
 int main() 
@@ -25,47 +23,40 @@ int main()
   for(int i = 0; i < N; i++)
     for(int j = 0; j < N; j++)
       std::cin >> arr[i][j];
-  int c = 1;
+  int c = 2;
+  std::queue<std::pair<int, int>> q;
   for(int i = 0; i < N; i++)
   {
     for(int j = 0; j < N; j++)
     {
-      if(arr[i][j] == 0 || dist[i][j]) continue;
-      DFS(i, j, c++);
+      if(arr[i][j] == 1)
+        DFS(i, j, c++);
+      area[i][j] = arr[i][j];
+      if(area[i][j])
+        q.push({i, j});
     }
   }
   int distance = INT_MAX;
-  for(const auto& iter : starts)
+  while(q.size())
   {
-    std::queue<std::pair<int, int>> q;
-    for(const auto& it : iter.second)
+    int cy = q.front().first;
+    int cx = q.front().second;
+    q.pop();
+    for(int dir = 0; dir < 4; dir++)
     {
-      q.push(it);
-      cdist[it.first][it.second] = 1;
-    }
-    while(q.size())
-    {
-      int cy = q.front().first;
-      int cx = q.front().second;
-      q.pop();
-      for(int dir = 0; dir < 4; dir++)
+      int ny = cy + dy[dir];
+      int nx = cx + dx[dir];
+      if(ny < 0 || nx < 0 || ny >= N || nx >= N || arr[ny][nx]) continue;
+      if(area[ny][nx] == 0)
       {
-        int ny = cy + dy[dir];
-        int nx = cx + dx[dir];
-        if(ny < 0 || nx < 0 || ny >= N || nx >= N || cdist[ny][nx] || dist[ny][nx] == iter.first) continue;
-        if(arr[ny][nx] == 0)
-        {
-          cdist[ny][nx] = cdist[cy][cx] + 1;
-          q.push({ny, nx});
-        }
-        else if(dist[ny][nx] != iter.first)
-        {
-          distance = std::min(distance, cdist[cy][cx]);
-        }
+        dist[ny][nx] = dist[cy][cx] + 1;
+        area[ny][nx] = area[cy][cx];
+        q.push({ny, nx});
       }
+      else if(area[ny][nx] != area[cy][cx])
+        distance = std::min(distance, dist[ny][nx] + dist[cy][cx]);
     }
-    std::memset(cdist, 0, sizeof(cdist));
   }
-  std::cout <<distance;
+  std::cout << distance;
   return 0;
 }
