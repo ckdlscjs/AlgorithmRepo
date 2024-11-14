5a9ccef00f275047766ac38039382c41d4ab3794
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2024-11-15 02:49:56 +0900

    [Silver I] Title: RGB거리, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.cc
+++ 백준/Silver/1149. RGB거리/RGB거리.cc
@@ -10,23 +10,13 @@ int main()
   std::cin >> N;
   for(int i = 0; i < N; i++)
   {
-    std::cin >> r >> g >> b;
-    if(i <= 0)
-    {
-      dp[0][0] = r;
-      dp[0][1] = g;
-      dp[0][2] = b;
-    }
-    else if(i > 0)
-    {
-      
-      dp[1][0] = std::min(dp[0][1], dp[0][2]) + r;
-      dp[1][1] = std::min(dp[0][0], dp[0][2]) + g;
-      dp[1][2] = std::min(dp[0][0], dp[0][1]) + b;
-      dp[0][0] = dp[1][0];
-      dp[0][1] = dp[1][1];
-      dp[0][2] = dp[1][2];
-    }
+    std::cin >> dp[1][0] >> dp[1][1] >> dp[1][2];
+    dp[1][0] += std::min(dp[0][1], dp[0][2]);
+    dp[1][1] += std::min(dp[0][0], dp[0][2]);
+    dp[1][2] += std::min(dp[0][0], dp[0][1]);
+    dp[0][0] = dp[1][0];
+    dp[0][1] = dp[1][1];
+    dp[0][2] = dp[1][2];
   }
   std::cout << std::min({dp[1][0], dp[1][1], dp[1][2]});
   return 0;
