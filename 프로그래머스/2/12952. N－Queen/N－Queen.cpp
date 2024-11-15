3ddd9edb1055de3ec2b30584ef47897984a2964f
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2024-11-15 18:45:04 +0900

    [level 2] Title: N-Queen, Time: 133.17 ms, Memory: 4.11 MB -BaekjoonHub

--- 프로그래머스/2/12952. N－Queen/N－Queen.cpp
+++ 프로그래머스/2/12952. N－Queen/N－Queen.cpp
@@ -1,51 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
-int result = 0;
-bool check_cross(std::vector<std::vector<int>>& chess, int y, int x)
+int arr[15];
+bool CheckQueen(int y)
 {
-    for(int i = 0; i < chess.size(); i++)
-    {
-        if((chess[y][i] && i != x) || (chess[i][x] && i != y))
-            return false;
-    }
+    for(int i = 0; i < y; i++)
+        if(arr[y] == arr[i] || y-i == std::abs(arr[y] - arr[i])) return false;
     return true;
 }
-const int diagonal_y[] = {-1, -1, 1, 1};
-const int diagonal_x[] = {-1 ,1 ,-1, 1};
-bool check_diagonal(std::vector<std::vector<int>>& chess, int y, int x, int dir)
+int Check(const int& n, int y)
 {
-    if(y < 0 || x < 0 || y >= chess.size() || x >= chess.size())
-       return true;
-    if(chess[y][x])
-        return false;
-    return check_diagonal(chess, y + diagonal_y[dir], x + diagonal_x[dir], dir);
-}
-void check(std::vector<std::vector<int>>& chess, int cnt)
-{
-    if(cnt >= chess.size())
+    if(y >= n) return 1;
+    int ret = 0;
+    for(int j = 0; j < n; j++)
     {
-        result++;
-        return;
+        arr[y] = j;
+        if(CheckQueen(y))
+            ret += Check(n, y+1);
     }
-    for(int j = 0; j < chess.size(); j++)
-    {
-        if(chess[cnt][j])
-            continue;
-        chess[cnt][j] = 1;
-        if(
-            check_cross(chess, cnt, j) && 
-            check_diagonal(chess, cnt + diagonal_y[0], j + diagonal_x[0], 0) &&
-            check_diagonal(chess, cnt + diagonal_y[1], j + diagonal_x[1], 1) &&
-            check_diagonal(chess, cnt + diagonal_y[2], j + diagonal_x[2], 2) &&
-            check_diagonal(chess, cnt + diagonal_y[3], j + diagonal_x[3], 3))
-                check(chess, cnt+1);
-            
-        chess[cnt][j] = 0;
-    }
+    return ret;
 }
 int solution(int n) 
 {
-    std::vector<std::vector<int>> chess(n, std::vector<int>(n, 0));
-    check(chess, 0);
-    return result;
+    return Check(n, 0);
 }
