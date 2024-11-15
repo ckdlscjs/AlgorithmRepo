06a2c21ef1e5c48f6890aa3ed71a9fbd950276d5
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2024-11-15 17:58:02 +0900

    [level 2] Title: 양궁대회, Time: 0.30 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/2/92342. 양궁대회/README.md
+++ 프로그래머스/2/92342. 양궁대회/README.md
@@ -1,10 +1,10 @@
 # [level 2] 양궁대회 - 92342 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92342#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92342) 
 
 ### 성능 요약
 
-메모리: 4.16 MB, 시간: 8.37 ms
+메모리: 4.2 MB, 시간: 0.30 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 11월 15일 16:20:16
+2024년 11월 15일 17:58:01
 
 ### 문제 설명
 
--- 프로그래머스/2/92342. 양궁대회/양궁대회.cpp
+++ 프로그래머스/2/92342. 양궁대회/양궁대회.cpp
@@ -1,50 +1,55 @@
 #include <bits/stdc++.h>
 using namespace std;
-int res = 0;
-std::vector<int> res_ans;
-void Check(const int& n, const vector<int>& info, int cnt, int idx, std::vector<int>& ans)
+struct comp
 {
-    if(idx >= ans.size())
+    bool operator()(const std::pair<int, std::vector<int>>& a, const std::pair<int, std::vector<int>>& b)
     {
-        if(cnt != n) return;
-        int sum_l = 0, sum_a = 0;
-        for(int i = 0; i < ans.size(); i++)
+        if(a.first == b.first)
         {
-            if(ans[i] == 0 && info[i] == 0) continue;
-            if(ans[i] <= info[i]) sum_a += 10-i;
-            else if(ans[i] > info[i]) sum_l += 10-i;
-        }
-        if(res < sum_l - sum_a)
-        {
-            res = sum_l-sum_a;
-            res_ans = ans;
-        }
-        else if (res != 0 && res == sum_l - sum_a)
-        {
-            for (int i = ans.size() - 1; i >= 0; i--)
+            for(int i = a.second.size()-1; i >= 0; i--)
             {
-                if (ans[i] < res_ans[i]) break; 
-                if (ans[i] > res_ans[i])
-                {
-                    res_ans = ans;
-                    break;
-                }
+                if(a.second[i] == b.second[i]) continue;
+                return a.second[i] < b.second[i];
             }
         }
+        return a.first < b.first;
+    }
+};
+std::priority_queue<std::pair<int, std::vector<int>>, std::vector<std::pair<int, std::vector<int>>>, comp> pq;
+void DFS(const int& n, const std::vector<int>& info, int idx, int cnt, std::vector<int>& ans)
+{
+    if(idx >= info.size())
+    {
+        if(n != cnt) return;
+        int sum_ryan = 0, sum_appeach = 0;
+        for(int i = 0; i < idx; i++)
+        {
+            if(ans[i] == 0 && info[i] == 0) continue;
+            if(ans[i] <= info[i]) sum_appeach += 10-i;
+            else sum_ryan += 10-i;
+        }
+        if(sum_ryan > sum_appeach)
+            pq.push({sum_ryan-sum_appeach, ans});
         return;
     }
-    for(int j = 0; j <= n - cnt; j++)
+    DFS(n, info, idx+1, cnt, ans);
+    if(n-cnt > info[idx])
     {
-        ans[idx] = j;
-        Check(n, info, cnt+j, idx+1, ans);
+        ans[idx] = info[idx]+1;
+        DFS(n, info, idx+1, cnt + ans[idx], ans);
+        ans[idx] = 0;
+    }
+    else
+    {
+        ans[idx] = n-cnt;
+        DFS(n, info, idx+1, cnt + ans[idx], ans);
         ans[idx] = 0;
     }
 }
 vector<int> solution(int n, vector<int> info) 
 {
-    std::vector<int> answer(11, 0);
-    Check(n, info, 0, 0, answer);
-    if(res == 0 || res_ans.empty())
-        return {-1};
-    return res_ans;
+    pq.push({-1, {-1}});
+    vector<int> answer(11, 0);
+    DFS(n, info, 0, 0, answer);
+    return pq.top().second;
 }
