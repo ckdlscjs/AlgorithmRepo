885433e37360ec72e53cb2f5a01f0d4606455e64
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2024-11-15 15:57:03 +0900

    [level 2] Title: 양궁대회, Time: 27.63 ms, Memory: 20.1 MB -BaekjoonHub

--- 프로그래머스/2/92342. 양궁대회/양궁대회.cpp
+++ 프로그래머스/2/92342. 양궁대회/양궁대회.cpp
@@ -1,14 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
+int res = 0;
+std::vector<int> res_ans;
 struct compare
 {
     bool operator()(const std::pair<int, std::vector<int>>& a, const std::pair<int, std::vector<int>>& b)
     {
-        if(a.first != b.first)
-        {
-            return a.first < b.first;
-        }
-        else
+        if(a.first == b.first)
         {
             for(int i = 0; i < a.second.size(); i++)
             {
@@ -17,43 +15,38 @@ struct compare
                 return a.second[10-i] < b.second[10-i];
             }
         }
+        return a.first < b.first;
     }
 };
 std::priority_queue<std::pair<int, std::vector<int>>, std::vector<std::pair<int, std::vector<int>>>, compare> pq;
-void check(const std::vector<int>& info_appeach, int cnt, int idx, std::vector<int>& info_lion)
+void Check(const int& n, const vector<int>& info, int cnt, int idx, std::vector<int>& ans)
 {
-    if(idx >= info_appeach.size())
+    if(idx >= ans.size())
     {
-        if(cnt != 0)
-            return;
-        int sum_appeach = 0;
-        int sum_lion = 0;
-        for(int i = 0; i < info_lion.size(); i++)
+        if(cnt != n) return;
+        int sum_l = 0, sum_a = 0;
+        for(int i = 0; i < ans.size(); i++)
         {
-            if(info_lion[i] == info_appeach[i] && info_lion[i] == 0)
-                continue;
-            if(info_lion[i] > info_appeach[i])
-                sum_lion += 10-i;
-            else
-                sum_appeach += 10-i;
+            if(ans[i] == 0 && info[i] == 0) continue;
+            if(ans[i] <= info[i]) sum_a += 10-i;
+            else if(ans[i] > info[i]) sum_l += 10-i;
         }
-        if(sum_lion > sum_appeach)
-            pq.push({sum_lion - sum_appeach, info_lion});
+        if(sum_l > sum_a)
+            pq.push({sum_l-sum_a, ans});
+        return;
     }
-    else
+    for(int j = n-cnt; j >= 0; j--)
     {
-        for(int shot = 0; shot <= cnt; shot++)
-        {
-            info_lion[idx] = shot;
-            check(info_appeach, cnt - shot, idx+1, info_lion);
-            info_lion[idx] = 0;
-        }
+        ans[idx] = j;
+        Check(n, info, cnt+j, idx+1, ans);
+        ans[idx] = 0;
     }
 }
 vector<int> solution(int n, vector<int> info) 
 {
+    std::vector<int> answer(11, 0);
     pq.push({-1, {-1}});
-    std::vector<int> lion = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-    check(info, n, 0, lion);
+    Check(n, info, 0, 0, answer);
+
     return pq.top().second;
 }
