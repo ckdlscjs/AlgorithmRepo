6a038158b6a6d833e570a9f31091ececf5efe353
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2024-11-17 18:41:44 +0900

    [level 3] Title: 외벽 점검, Time: 16.18 ms, Memory: 3.63 MB -BaekjoonHub

--- 프로그래머스/3/60062. 외벽 점검/외벽 점검.cpp
+++ 프로그래머스/3/60062. 외벽 점검/외벽 점검.cpp
@@ -1,30 +1,27 @@
 //https://school.programmers.co.kr/learn/courses/30/lessons/60062
 #include <bits/stdc++.h>
 using namespace std;
+
 int solution(int n, vector<int> weak, vector<int> dist) 
 {
-    std::vector<int> weaks = weak;
-    for(const auto& iter : weak)
-        weaks.push_back(iter + n);
-    int res = dist.size() + 1;
+    int len = weak.size();
+    for(int i = 0; i < len; i++) weak.push_back(n + weak[i]);
+    int answer = dist.size() + 1;
     do
     {
-        for(int i = 0; i < weaks.size()/2; i++)
+        for(int i = 0; i < weak.size() / 2; i++)
         {
             int cnt = 0;
-            int pos = weaks[i] + dist[cnt];
-            for(int j = i+1; j < i + weaks.size()/2; j++)
+            int pos = weak[i] + dist[cnt];
+            for(int j = i + 1; j < i+weak.size()/2; j++)
             {
-                if(pos < weaks[j])
-                {
-                    cnt++;
-                    if(cnt >= dist.size())
-                        break;
-                    pos = weaks[j] + dist[cnt];
-                }
+                if(pos >= weak[j]) continue;
+                cnt++;
+                if(cnt >= dist.size()) break;
+                pos = weak[j] + dist[cnt];
             }
-            res = std::min(res, cnt+1);
-        }
+            answer = std::min(answer, cnt + 1);
+        } 
     }while(next_permutation(dist.begin(), dist.end()));
-    return res > dist.size()? -1 : res;
+    return (answer > dist.size() ? -1 : answer);
 }
