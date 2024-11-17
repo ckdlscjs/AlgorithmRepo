f2cab4cfa81377597b166295f521cf36a7c21fb4
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2024-11-17 19:38:04 +0900

    [level 3] Title: 사라지는 발판, Time: 11.61 ms, Memory: 4.16 MB -BaekjoonHub

--- 프로그래머스/3/92345. 사라지는 발판/사라지는 발판.cpp
+++ 프로그래머스/3/92345. 사라지는 발판/사라지는 발판.cpp
@@ -1,27 +1,26 @@
+//https://school.programmers.co.kr/learn/courses/30/lessons/92345
 #include <bits/stdc++.h>
 using namespace std;
 const int dy[] = {0, 0, 1, -1};
 const int dx[] = {1, -1, 0, 0};
-int check(std::vector<std::vector<int>>& boards, std::vector<int> cur, std::vector<int> next)
+int Check(std::vector<std::vector<int>>& board, std::vector<int> cur, std::vector<int> nxt)
 {
-    if(!boards[cur[0]][cur[1]])
-        return 0;
     int beststep = 0;
+    if(board[cur[0]][cur[1]] == 0) return beststep; //탐색할곳이 없다면 패배이므로 0
     for(int dir = 0; dir < 4; dir++)
     {
-        int ny = dy[dir] + cur[0];
-        int nx = dx[dir] + cur[1];
-        if(ny < 0 || nx < 0 || ny >= boards.size() || nx >= boards[0].size() || !boards[ny][nx])
-            continue;
-        boards[cur[0]][cur[1]] = 0;
-        int step = check(boards, next, {ny, nx}) + 1;
-        boards[cur[0]][cur[1]] = 1;
+        int ny = cur[0] + dy[dir];
+        int nx = cur[1] + dx[dir];
+        if(ny < 0 || nx < 0 || ny >= board.size() || nx >= board[0].size() || board[ny][nx] == 0) continue;
+        board[cur[0]][cur[1]] = 0;
+        int step = Check(board, nxt, {ny, nx}) + 1; //이동을 한번은 진행 하였으므로 + 1
+        board[cur[0]][cur[1]] = 1;
         
-        if(beststep % 2 == 0 && step % 2 == 1)
+        if(beststep % 2 == 0 && step % 2 == 1) //현재 이동의 결과가 이기는횟수, 최선결과가 지는횟수면 갱신
             beststep = step;
-        else if(beststep % 2 == 0 && step % 2 == 0)
+        else if(beststep % 2 == 0 && step % 2 == 0) //둘다 지는조건이라면 최대한 이동횟수를늘린다
             beststep = std::max(beststep, step);
-        else if(beststep % 2 == 1 && step % 2 == 1)
+        else if(beststep % 2 == 1 && step % 2 == 1) //둘다 이기는 조건이라면 최소한으로 이동한다
             beststep = std::min(beststep, step);
     }
     return beststep;
@@ -29,5 +28,5 @@ int check(std::vector<std::vector<int>>& boards, std::vector<int> cur, std::vect
 
 int solution(vector<vector<int>> board, vector<int> aloc, vector<int> bloc) 
 {
-    return check(board, aloc, bloc);
+    return Check(board, aloc, bloc);
 }
