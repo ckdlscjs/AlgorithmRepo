d171fc360f6932b732f756b4da60d0ec81cc1f7e
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2024-11-20 16:38:03 +0900

    [Silver II] Title: 최소 힙, Time: 16 ms, Memory: 2916 KB -BaekjoonHub

--- 백준/Silver/1927. 최소 힙/최소 힙.cc
+++ 백준/Silver/1927. 최소 힙/최소 힙.cc
@@ -1,29 +1,30 @@
-#pragma once
 #include <bits/stdc++.h>
-int N, M;
+using namespace std;
+int N, val;
 std::priority_queue<int, std::vector<int>, std::greater<int>> pq;
-int main(void)
+int main() 
 {
-	std::ios::sync_with_stdio(false);
-	std::cin.tie(0);
-	std::cout.tie(0);
-	std::cin >> N;
-	int num = 0;
-	for (int i = 0; i < N; i++)
-	{
-		std::cin >> num;
-		if (num == 0)
-		{
-			if (pq.size())
-			{
-				std::cout << pq.top() << '\n';
-				pq.pop();
-			}
-			else
-				std::cout << 0 << '\n';
-		}
-		else
-			pq.push(num);
-	}
-	return 0;
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(0);
+  std::cout.tie(0);
+  std::cin >> N;
+  for(int i = 0; i < N; i++)
+  {
+    std::cin >> val;
+    if(val == 0)
+    {
+      if(pq.empty())
+      {
+        std::cout << 0 <<'\n';
+      }
+      else
+      {
+        std::cout <<pq.top() << '\n';
+        pq.pop();
+      }
+    }
+    else
+      pq.push(val);
+  }
+  return 0;
 }
