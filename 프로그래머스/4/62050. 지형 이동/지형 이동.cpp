677a55902353acd96d4f3caaddce830affe2dd47
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2024-11-24 02:43:35 +0900

    [level 4] Title: 지형 이동, Time: 30.32 ms, Memory: 8.2 MB -BaekjoonHub

--- 프로그래머스/4/62050. 지형 이동/지형 이동.cpp
+++ 프로그래머스/4/62050. 지형 이동/지형 이동.cpp
@@ -1,40 +1,33 @@
+//https://school.programmers.co.kr/learn/courses/30/lessons/62050
 #include <bits/stdc++.h>
+using namespace std;
 const int dy[] = {0, 0, 1, -1};
 const int dx[] = {1, -1, 0, 0};
-using namespace std;
-struct comp
-{
-    bool operator()(const std::pair<int, std::pair<int, int>>& a, const std::pair<int, std::pair<int, int>>& b)
-    {
-        return a.first > b.first;
-    }
-};
-std::priority_queue<std::pair<int, std::pair<int, int>>, std::vector<std::pair<int, std::pair<int, int>>>, comp> pq;
 bool visited[305][305];
+std::priority_queue<std::pair<int, std::pair<int, int>>, std::vector<std::pair<int, std::pair<int, int>>>, std::greater<std::pair<int, std::pair<int, int>>>> pq;
 int solution(vector<vector<int>> land, int height) 
 {
-    int answer = 0;
+    int res = 0;
     pq.push({0, {0, 0}});
     while(pq.size())
     {
-        int amount = pq.top().first;
-        int y = pq.top().second.first;
-        int x = pq.top().second.second;
+        int cc = pq.top().first;
+        int cy = pq.top().second.first;
+        int cx = pq.top().second.second;
         pq.pop();
-        if(visited[y][x])
+        if(visited[cy][cx])
             continue;
-        visited[y][x] = true;
-        answer += amount;
+        visited[cy][cx] = true;
+        res += cc;
         for(int dir = 0; dir < 4; dir++)
         {
-            int ny = y + dy[dir];
-            int nx = x + dx[dir];
-            if(ny < 0 || nx < 0 || ny >= land.size() || nx >= land[0].size() || visited[ny][nx])
-                continue;
-            int dist = std::abs(land[ny][nx] - land[y][x]);
-            dist = dist <= height ? 0 : dist;
-            pq.push({dist, {ny, nx}});
+            int ny = cy + dy[dir];
+            int nx = cx + dx[dir];
+            if(ny < 0 || nx < 0 || ny >= land.size() || nx >= land.size() || visited[ny][nx]) continue;
+            int diff = std::abs(land[cy][cx] - land[ny][nx]);
+            diff = (diff <= height ? 0 : diff);
+            pq.push({diff, {ny, nx}});
         }
     }
-    return answer;
+    return res;
 }
