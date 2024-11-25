85c89db1d5d0aec69d191837867276e8f83e5032
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2024-11-25 18:25:06 +0900

    [Silver III] Title: N과 M (2), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.cc
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.cc
@@ -1,23 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 int N, M, arr[15];
-void Check(int idx, int mask)
+void Check(int idx, int cnt)
 {
-  if(idx >= M)
+  if(cnt >= M)
   {
     for(int i = 0; i < M; i++)
       std::cout << arr[i] << ' ';
     std::cout << '\n';
     return;
   }
-  for(int i = 0; i < N; i++)
+  for(int i = idx+1; i <= N; i++)
   {
-    if(mask & 1 << i) continue;
-    if(idx >= 1 && arr[idx-1] >= i+1) continue;
-    mask |= 1 << i;
-    arr[idx] = i+1;
-    Check(idx+1, mask);
-    mask &= ~(1 << i);
+    arr[cnt] = i;
+    Check(i, cnt+1);
   }
 }
 int main() 
