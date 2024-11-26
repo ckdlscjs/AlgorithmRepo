03fb9576d6f09fe974575e1275fbab25d3f9e5bb
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2024-11-26 17:20:28 +0900

    [Gold III] Title: 소문난 칠공주, Time: 88 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Gold/1941. 소문난 칠공주/소문난 칠공주.cc
+++ 백준/Gold/1941. 소문난 칠공주/소문난 칠공주.cc
@@ -3,7 +3,7 @@ using namespace std;
 const int dy[] = {0, 0, 1, -1};
 const int dx[] = {1, -1, 0, 0};
 std::string arr[5];
-bool visited[6][6];
+bool visited[5][5];
 int Check(int num, std::vector<int> combi)
 {
   int ret = 0;
@@ -13,8 +13,8 @@ int Check(int num, std::vector<int> combi)
     int scnt = 0, ycnt = 0;
     for(const auto& iter : combi)
     {
-      int y = (iter-1)/5;
-      int x = (iter-1)%5;
+      int y = iter/5;
+      int x = iter%5;
       scnt += arr[y][x] == 'S' ? 1 : 0;
       ycnt += arr[y][x] == 'Y' ? 1 : 0;
       visited[y][x] = false;
@@ -23,8 +23,8 @@ int Check(int num, std::vector<int> combi)
       return 0;
     int visitcnt = 1;
     std::queue<std::pair<int, int>> q;
-    q.push({(combi[0]-1)/5, (combi[0]-1)%5});
-    visited[(combi[0]-1)/5][(combi[0]-1)%5] = true;
+    q.push({combi[0]/5, combi[0]%5});
+    visited[combi[0]/5][combi[0]%5] = true;
     while(q.size())
     {
       int y = q.front().first;
@@ -45,7 +45,7 @@ int Check(int num, std::vector<int> combi)
       return 0;
     return 1;
   }
-  for(int i = num; i <= 25; i++)
+  for(int i = num; i < 25; i++)
   {
     combi.push_back(i);
     ret += Check(i+1, combi);
@@ -60,6 +60,6 @@ int main()
   std::cout.tie(0);
   for(int i = 0; i < 5; i++)
     std::cin >> arr[i];
-  std::cout << Check(1, {});
+  std::cout << Check(0, {});
   return 0;
 }
