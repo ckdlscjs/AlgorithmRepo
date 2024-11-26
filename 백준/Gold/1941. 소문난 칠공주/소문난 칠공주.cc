ecf16a5f7b6de2abe7d7116834e8ae988c63fdc4
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2024-11-26 17:35:05 +0900

    [Gold III] Title: 소문난 칠공주, Time: 60 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/1941. 소문난 칠공주/소문난 칠공주.cc
+++ 백준/Gold/1941. 소문난 칠공주/소문난 칠공주.cc
@@ -4,17 +4,17 @@ const int dy[] = {0, 0, 1, -1};
 const int dx[] = {1, -1, 0, 0};
 std::string arr[5];
 bool visited[5][5];
-int Check(int num, std::vector<int> combi)
+int Check(int num, int cnt, int* combi)
 {
   int ret = 0;
-  if(combi.size() >= 7)
+  if(cnt >= 7)
   {
     std::memset(visited, true, sizeof(visited));
     int scnt = 0, ycnt = 0;
-    for(const auto& iter : combi)
+    for(int i = 0; i < 7; i++)
     {
-      int y = iter/5;
-      int x = iter%5;
+      int y = combi[i] / 5;
+      int x = combi[i] % 5;
       scnt += arr[y][x] == 'S' ? 1 : 0;
       ycnt += arr[y][x] == 'Y' ? 1 : 0;
       visited[y][x] = false;
@@ -47,9 +47,8 @@ int Check(int num, std::vector<int> combi)
   }
   for(int i = num; i < 25; i++)
   {
-    combi.push_back(i);
-    ret += Check(i+1, combi);
-    combi.pop_back();
+    combi[cnt] = i;
+    ret += Check(i+1, cnt+1, combi);
   }
   return ret;
 }
@@ -60,6 +59,7 @@ int main()
   std::cout.tie(0);
   for(int i = 0; i < 5; i++)
     std::cin >> arr[i];
-  std::cout << Check(0, {});
+  int combi[8];
+  std::cout << Check(0, 0, combi);
   return 0;
 }
