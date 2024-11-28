35b346826cb869857f1fc2f963cbdc5c664fc9b0
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2024-11-28 17:27:07 +0900

    [Gold I] Title: Gaaaaaaaaaarden, Time: 1496 ms, Memory: 2184 KB -BaekjoonHub

--- 백준/Gold/18809. Gaaaaaaaaaarden/Gaaaaaaaaaarden.cc
+++ 백준/Gold/18809. Gaaaaaaaaaarden/Gaaaaaaaaaarden.cc
@@ -2,10 +2,85 @@
 using namespace std;
 const int dy[] = {0, 0, 1, -1};
 const int dx[] = {1, -1, 0, 0};
-int N, M, G, R, arr[55][55], field[55][55], res;
-std::unordered_set<int> poss;
-std::unordered_set<int> chks;
+int N, M, G, R, arr_chk[15], arr[55][55], field[55][55], res;
 std::vector<std::pair<int, int>> inputs;
+void CheckR(int idx, int cnt)
+{
+  if(cnt >= R)
+  {
+    int sum = 0;
+    std::queue<std::pair<int, int>> q;
+    std::queue<std::pair<int, std::pair<int, int>>> nq;
+    for(int i = 0; i < inputs.size(); i++)
+    {
+      if(!arr_chk[i]) continue;
+      int y = inputs[i].first;
+      int x = inputs[i].second;
+      field[y][x] = arr_chk[i];
+      q.push({y, x});
+    }
+    while(q.size())
+    {
+      int cy = q.front().first;
+      int cx = q.front().second;
+      q.pop();
+      for(int dir = 0; dir < 4; dir++)
+      {
+        int ny = cy + dy[dir];
+        int nx = cx + dx[dir];
+        if(ny < 0 || nx < 0 || ny >= N || nx >= M || field[ny][nx] || arr[ny][nx] == 0) continue;
+        nq.push({field[cy][cx], {ny, nx}});
+      }
+      if(q.empty() && nq.size())
+      {
+        std::set<std::pair<int, int>> chks;
+        while(nq.size())
+        {
+          int cc = nq.front().first;
+          int cy = nq.front().second.first;
+          int cx = nq.front().second.second;
+          nq.pop();
+          field[cy][cx] += (field[cy][cx] != cc ? cc : 0);
+          chks.insert({cy, cx});
+        }
+        for(const auto& iter : chks)
+        {
+          if(field[iter.first][iter.second] < 7)
+          {
+            q.push({iter.first, iter.second});
+            continue;
+          }
+          sum++;
+        }
+      }
+    }
+    res = std::max(res, sum);
+    std::memset(field, 0, sizeof(field));
+    return;
+  }
+  for(int i = idx; i < inputs.size(); i++)
+  {
+    if(arr_chk[i]) continue;
+    arr_chk[i] = 4;
+    CheckR(i+1, cnt+1);
+    arr_chk[i] = 0;
+  }
+}
+void CheckG(int idx, int cnt)
+{
+  if(cnt >= G)
+  {
+    CheckR(0, 0);
+    return;
+  }
+  for(int i = idx; i < inputs.size(); i++)
+  {
+    if(arr_chk[i]) continue;
+    arr_chk[i] = 3;
+    CheckG(i+1, cnt+1);
+    arr_chk[i] = 0;
+  }
+}
 int main() 
 {
   std::ios::sync_with_stdio(false);
@@ -21,77 +96,7 @@ int main()
         inputs.push_back({i, j});
     }
   }
-  for(int i = 1; i < (1 << inputs.size()); i++)
-  {
-    int cnt = 0;
-    for(int j = 0; j < inputs.size(); j++)
-      cnt += (i & (1 << j)) ? 1 : 0;
-    if(cnt == G+R)
-      poss.insert(i);
-  }
-  for(int i = 1; i < (1 << G+R); i++)
-  {
-    int gcnt = 0, rcnt = 0;
-    for(int j = 0; j < G+R; j++)
-    {
-      gcnt += (i & (1 << j)) ? 0 : 1;
-      rcnt += (i & (1 << j)) ? 1 : 0;
-    }
-    if(gcnt == G && rcnt == R)
-      chks.insert(i);
-  }
-  for(const auto& pos : poss)
-  {
-    for(const auto& iter : chks)
-    {
-      int sum = 0, idx = 0;
-      std::queue<std::pair<int, int>> q;
-      std::queue<std::pair<int, std::pair<int, int>>> nq;
-      for(int i = 0; i < inputs.size(); i++)
-      {
-        if(!(pos & 1 << i)) continue;
-        int y = inputs[i].first;
-        int x = inputs[i].second;
-        field[y][x] = (iter & 1 << idx++) ? 4 : 3;
-        q.push({y, x});
-      }
-      while(q.size())
-      {
-        int cy = q.front().first;
-        int cx = q.front().second;
-        q.pop();
-        for(int dir = 0; dir < 4; dir++)
-        {
-          int ny = cy + dy[dir];
-          int nx = cx + dx[dir];
-          if(ny < 0 || nx < 0 || ny >= N || nx >= M || arr[ny][nx] == 0 || field[ny][nx]) continue;
-          nq.push({field[cy][cx], {ny, nx}});
-        }
-        if(q.empty() && nq.size())
-        {
-          std::set<std::pair<int, int>> nexts;
-          while(nq.size())
-          {
-            int c = nq.front().first;
-            int y = nq.front().second.first;
-            int x = nq.front().second.second;
-            nq.pop();
-            field[y][x] += (field[y][x] != c) ? c : 0;
-            nexts.insert({y, x});
-          }
-          for(const auto& iter : nexts)
-          {
-            if(field[iter.first][iter.second] >= 7)
-              sum++;
-            else
-              q.push({iter.first, iter.second});
-          }
-        }
-      }
-      res = std::max(res, sum);
-      std::memset(field, 0, sizeof(field));
-    }
-  }
+  CheckG(0, 0);
   std::cout << res;
   return 0;
 }
