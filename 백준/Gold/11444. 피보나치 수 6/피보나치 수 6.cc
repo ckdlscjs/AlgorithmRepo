8e61c486461b2e1be356073c9097fe60f98eb799
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2024-12-03 00:17:10 +0900

    [Gold II] Title: 피보나치 수 6, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/11444. 피보나치 수 6/피보나치 수 6.cc
+++ 백준/Gold/11444. 피보나치 수 6/피보나치 수 6.cc
@@ -1,31 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
 long long int N;
-std::vector<std::vector<long long int>> arr = {{1, 1}, {1, 0}};
-std::vector<std::vector<long long int>> Multiply(const std::vector<std::vector<long long int>>& mat1, const std::vector<std::vector<long long int>>& mat2) 
+long long int mod = 1'000'000'007;
+std::vector<std::vector<long long int>> Multiply(const std::vector<std::vector<long long int>> mat1, const std::vector<std::vector<long long int>> mat2)
 {
-  std::vector<std::vector<long long int>> temp(2, std::vector<long long int>(2, 0));
+  std::vector<std::vector<long long int>> temp = {{0, 0}, {0, 0}};
   for(int i = 0; i < 2; i++)
   {
     for(int j = 0; j < 2; j++)
     {
       for(int idx = 0; idx < 2; idx++)
       {
-        temp[i][j] += (mat1[i][idx] * mat2[idx][j])%1'000'000'007;
+        temp[i][j] += (mat1[i][idx] * mat2[idx][j]) % mod;
       }
-      temp[i][j] %= 1'000'000'007;
+      temp[i][j] %= mod;
     }
   }
   return temp;
 }
-std::vector<std::vector<long long int>> Fibo(const std::vector<std::vector<long long int>>& mat, long long int k)
+std::vector<std::vector<long long int>> Fibo(long long int k)
 {
   if(k <= 0) return {{1, 0}, {0, 1}};
-  if(k == 1) return mat;
-  std::vector<std::vector<long long int>> temp = Fibo(mat, k/2);
-  std::vector<std::vector<long long int>> ans = Multiply(temp, temp);
-  if(k % 2) ans = Multiply(ans, mat);
-  return ans;
+  if(k <= 1) return {{1, 1}, {1, 0}};
+  std::vector<std::vector<long long int>> temp = Fibo(k/2);
+  temp = Multiply(temp, temp);
+  if(k%2) temp = Multiply(temp, {{1, 1}, {1, 0}});
+  return temp;
 }
 int main() 
 {
@@ -33,8 +33,7 @@ int main()
   std::cin.tie(0);
   std::cout.tie(0);
   std::cin >> N;
-  
-  std::vector<std::vector<long long int>> ans = Fibo(arr, N-1);
-  std::cout << ans[0][0];
+  std::vector<std::vector<long long int>> ret = Fibo(N-1);
+  std::cout << ret[0][0];
   return 0;
 }
