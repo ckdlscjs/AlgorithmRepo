100f430b1aeb4a707352be834bdeaa2236d5498f
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2024-12-12 17:44:33 +0900

    [Silver I] Title: 카드 합체 놀이, Time: 0 ms, Memory: 2160 KB -BaekjoonHub

--- 백준/Silver/15903. 카드 합체 놀이/카드 합체 놀이.cc
+++ 백준/Silver/15903. 카드 합체 놀이/카드 합체 놀이.cc
@@ -1,22 +1,34 @@
 #include <bits/stdc++.h>
 using namespace std;
-long long int N, M, arr[1'005];
+long long int N, M, val;
+std::priority_queue<long long int, std::vector<long long int>, std::greater<long long int>> pq;
 int main()
 {
   std::ios::sync_with_stdio(false);
   std::cin.tie(0);
   std::cout.tie(0);
   std::cin >> N >> M;
-  for(int i = 0; i < N; i++) std::cin >> arr[i];
+  for(int i = 0; i < N; i++)
+  {
+    std::cin >> val;
+    pq.push(val);
+  }
   for(int i = 0; i < M; i++)
   {
-    std::sort(arr, arr+N);
-    long long int sum = arr[0] + arr[1];
-    arr[0] = sum;
-    arr[1] = sum;
+    long long int sum = 0;
+    sum += pq.top();
+    pq.pop();
+    sum += pq.top();
+    pq.pop();
+    pq.push(sum);
+    pq.push(sum);
   }
   long long int sum = 0;
-  for(int i = 0; i < N; i++) sum += arr[i];
+  for(int i = 0; i < N; i++)
+  {
+    sum += pq.top();
+    pq.pop();
+  }
   std::cout << sum;
   return 0;
 }
