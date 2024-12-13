d57df9c070c0067927d0bd67b0d6bb42b79e03dd
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2024-12-13 15:41:09 +0900

    [Gold I] Title: 멀티탭 스케줄링, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/1700. 멀티탭 스케줄링/멀티탭 스케줄링.cc
+++ 백준/Gold/1700. 멀티탭 스케줄링/멀티탭 스케줄링.cc
@@ -1,45 +1,41 @@
 #include <bits/stdc++.h>
-using namespace std;
-int n, k, ans;
-int arr[105];
+int N, K, arr[105], res;
+std::set<int> uses;
 int main()
 {
-    std::ios::sync_with_stdio(false);
-    std::cin.tie(0);
-    std::cout.tie(0);
-    std::cin >> n >> k;
-    for(int i = 0; i < k; i++)
-      std::cin >> arr[i];
-    std::set<int> uses;
-    for(int i = 0; i < k; i++)
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(0);
+  std::cout.tie(0);
+  std::cin >> N >> K;
+  for(int i = 0; i < K; i++) std::cin >> arr[i];
+  for(int i = 0; i < K; i++)
+  {
+    if(uses.find(arr[i]) != uses.end()) continue;
+    if(uses.size() >= N)
     {
-      if(uses.find(arr[i]) != uses.end())
-        continue;
-      if(uses.size() >= n)
+      int erase, idx_late = 0;
+      for(const auto& iter : uses)
       {
-        int erase, late_idx = 0;
-        for(const auto& iter : uses)
+        int idx = K;
+        for(int j = i + 1; j < K; j++)
         {
-          int iter_idx = k;
-          for(int j = i+1; j < k; j++)
+          if(arr[j] == iter)
           {
-            if(iter == arr[j])
-            {
-              iter_idx = j;
-              break;
-            }
-          }
-          if(iter_idx > late_idx)
-          {
-            erase = iter;
-            late_idx = iter_idx;
+            idx = j;
+            break;
           }
         }
-        ans++;
-        uses.erase(erase);
+        if(idx > idx_late)
+        {
+          erase = iter;
+          idx_late = idx;
+        }
       }
-      uses.insert(arr[i]);
+      res++;
+      uses.erase(erase);
     }
-    std::cout << ans;
-    return 0;
+    uses.insert(arr[i]);
+  }
+  std::cout << res;
+  return 0;
 }
