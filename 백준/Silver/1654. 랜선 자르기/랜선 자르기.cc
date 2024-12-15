c079f8b9b138850f840c179c23a3298f248d602f
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2024-12-16 03:34:05 +0900

    [Silver II] Title: 랜선 자르기, Time: 4 ms, Memory: 2100 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.cc
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.cc
@@ -1,30 +1,26 @@
 #include <bits/stdc++.h>
-long long int arr[10'005];
-long long int K, N;
+long long int K, N, arr[10'005];
 bool Check(long long int mid)
 {
-    long long int cnt = 0;
-    for(int i = 0; i < K; i++)
-    {
-        cnt += arr[i] / mid;
-    }
-    return cnt >= N;
+  long long int cnt = 0;
+  for(int i = 0; i < K; i++)
+    cnt += arr[i] / mid;
+  return cnt >= N;
 }
-int main(void)
+int main()
 {
-    std::ios::sync_with_stdio(false);
-    std::cin.tie(0);
-    std::cout.tie(0);
-    std::cin >> K >> N;
-    for(int i = 0; i < K; i++)
-      std::cin >> arr[i];
-    long long int lo = 0, hi = 1e18;
-    while(lo + 1 < hi)
-    {
-        long long int mid = (lo + hi) / 2;
-        if(Check(mid)) lo = mid;
-        else hi = mid;
-    }
-    std::cout << lo;
-    return 0;
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(0);
+  std::cout.tie(0);
+  std::cin >> K >> N;
+  for(int i = 0; i < K; i++) std::cin >> arr[i];
+  long long int lo = -1, hi = (long long int)(1LL<<32) - 1LL;
+  while(lo + 1 < hi)
+  {
+    long long int mid = (lo + hi) / 2;
+    if(Check(mid)) lo = mid;
+    else hi = mid;
+  }
+  std::cout << lo;
+  return 0;
 }
