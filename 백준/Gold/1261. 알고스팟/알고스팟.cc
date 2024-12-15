7ae26248dc0f70243fae2b6df30059ddeab57718
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2024-12-15 23:07:25 +0900

    [Gold IV] Title: 알고스팟, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/1261. 알고스팟/알고스팟.cc
+++ 백준/Gold/1261. 알고스팟/알고스팟.cc
@@ -1,42 +1,72 @@
-#include <bits/stdc++.h>
-const int dy[] = {0, 0, -1, 1};
-const int dx[] = {-1, 1, 0, 0};
-const int INF = 987654321;
-int N, M, dist[105][105];
-std::string arr[105];
-int main()
-{
-  std::ios::sync_with_stdio(false);
-  std::cin.tie(0);
-  std::cout.tie(0);
-  std::cin >> M >> N;
-  for(int i = 0; i < N; i++)
-    std::cin >> arr[i];
-  std::fill_n(&dist[0][0], 105*105, INF);
-  dist[0][0] = 0;
-  std::queue<std::pair<int, int>> q;
-  q.push({0, 0});
-  while(q.size())
-  {
-    int cy = q.front().first;
-    int cx = q.front().second;
-    q.pop();
-    for(int dir = 0; dir < 4; dir++)
-    {
-      int ny = cy + dy[dir];
-      int nx = cx + dx[dir];
-      if(ny < 0 || nx < 0 || ny >= N || nx >= M) continue;
-      if(arr[ny][nx] == '0' && dist[cy][cx] < dist[ny][nx])
-      {
-        dist[ny][nx] = dist[cy][cx];
-        q.push({ny, nx});
+#include <iostream>
+#include <deque>
+#include <string>
+using namespace std;
+
+struct point {
+   int x, y, d;
+};
+
+int main() {
+   point pt;
+   int N, M;
+   string map1D;
+   int map[105][105];
+   int dist[105][105];
+   for (int i = 0; i < 105; i++) {
+      for (int j = 0; j < 105; j++) {
+         dist[i][j] = 999999;
       }
-      else if(arr[ny][nx] == '1' && dist[cy][cx] + 1 < dist[ny][nx])
-      {
-        dist[ny][nx] = dist[cy][cx] + 1;
-        q.push({ny, nx});
+   }
+   int dx[4] = { 1, -1, 0, 0 };
+   int dy[4] = { 0,0,1,-1 };
+   int top_x, top_y, top_d;
+   int newx, newy, newd;
+   // 맵 추가
+   cin >> M >> N;
+   for (int i = 0; i < N; i++) {
+      cin >> map1D;
+      for (int j = 0; j < M; j++) {
+         map[i][j] = map1D[j] - 48;
       }
-    }
-  }
-  std::cout << dist[N-1][M-1];
+   }
+
+   deque <point> Q_room;
+   pt.x = 0;
+   pt.y = 0;
+   pt.d = 0;
+   Q_room.push_back(pt);
+   dist[0][0] = 0;
+   while (!Q_room.empty()) {
+      top_x = Q_room.front().x;
+      top_y = Q_room.front().y;
+      top_d = Q_room.front().d;
+      Q_room.pop_front();
+      if (top_x == (M - 1) && top_y == (N - 1)) { // 정답 찾음
+            cout << top_d << endl;
+            return 0;
+         }
+      for (int dir = 0; dir < 4; dir++) {
+         newx = top_x + dx[dir];
+         newy = top_y + dy[dir];
+         //cout << "시작좌표>> " << top_x << ", " << top_y << " 거리: " << top_d << "방향: " << dir << endl;
+         //cout << "디버그 코드>> " << newx << ", " << newy << endl;
+         if (newx < 0 || newx >= M || newy < 0 || newy >= N) continue; // 맵 바깥으로 나감
+         
+         if (dist[newy][newx] <= dist[top_y][top_x] + 1) continue; // 이미 확인한 지점
+
+         
+         newd = top_d + map[newy][newx];
+         dist[newy][newx] = newd;
+         pt.x = newx;
+         pt.y = newy;
+         pt.d = newd;
+         if (map[newy][newx] == 0) {
+            Q_room.push_front(pt);
+         }
+         else {
+            Q_room.push_back(pt);
+         }
+      }
+   }
 }
