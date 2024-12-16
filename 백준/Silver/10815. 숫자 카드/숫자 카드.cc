4205ff74d178079a7d84b5dc7e89e4b24f3a2d1f
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2024-12-16 12:37:17 +0900

    [Silver V] Title: 숫자 카드, Time: 156 ms, Memory: 21552 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.cc
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.cc
@@ -1,25 +1,22 @@
 #include <bits/stdc++.h>
-int N, M, val, arr[500'005];
+int N, M, val;
+bool arr[20'000'005];
 int main()
 {
   std::ios::sync_with_stdio(false);
   std::cin.tie(0);
   std::cout.tie(0);
   std::cin >> N;
-  for(int i = 0; i < N; i++) std::cin >> arr[i];
-  std::sort(arr, arr+N);
+  for(int i = 0; i < N; i++)
+  {
+    std::cin >> val;
+    arr[val + 10'000'000] = true;
+  }
   std::cin >> M;
   for(int i = 0; i < M; i++)
   {
     std::cin >> val;
-    int lo = -1, hi = N;
-    while(lo + 1 < hi)
-    {
-      int mid = (lo + hi) / 2;
-      if(arr[mid] <= val) lo = mid;
-      else hi = mid;
-    }
-    std::cout << (arr[lo] == val ? 1 : 0) << ' ';
+    std::cout << arr[val + 10'000'000] << ' ';
   }
   return 0;
 }
