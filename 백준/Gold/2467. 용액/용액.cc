4c66f88c8e8fed429791ed4693ba60a4c3d32a16
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2024-12-17 16:30:26 +0900

    [Gold V] Title: 용액, Time: 12 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Gold/2467. 용액/용액.cc
+++ 백준/Gold/2467. 용액/용액.cc
@@ -7,7 +7,6 @@ int main()
   std::cout.tie(0);
   std::cin >> N;
   for(int i = 0; i < N; i++) std::cin >> arr[i];
-  std::sort(arr, arr+N);
   int l = 0, r = N-1, res = INT_MAX;
   std::pair<int, int> result;
   while(l < r)
