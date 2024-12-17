e15b8268f674112937095ddba8a76a71351c6447
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2024-12-17 16:28:07 +0900

    [Gold V] Title: 용액, Time: 12 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Gold/2467. 용액/용액.cc
+++ 백준/Gold/2467. 용액/용액.cc
@@ -1,37 +1,29 @@
 #include <bits/stdc++.h>
-int N;
-int arr[100'005];
+int N, arr[100'005];
 int main()
 {
   std::ios::sync_with_stdio(false);
   std::cin.tie(0);
   std::cout.tie(0);
   std::cin >> N;
-  for(int i = 0; i < N; i++)
-    std::cin >> arr[i];
-  std::pair<int, int> res;
-  int ans = INT_MAX;
-  for(int i = 0; i < N; i++)
+  for(int i = 0; i < N; i++) std::cin >> arr[i];
+  std::sort(arr, arr+N);
+  int l = 0, r = N-1, res = INT_MAX;
+  std::pair<int, int> result;
+  while(l < r)
   {
-    int lo = i, hi = N;
-    while(lo + 1 < hi)
+    int sum = arr[l] + arr[r];
+    if(std::abs(sum) < res)
     {
-      int mid = (lo + hi) / 2;
-      int sum = arr[i] + arr[mid];
-      if(std::abs(sum) < ans)
-      {
-        ans = std::abs(sum);
-        res.first = arr[i];
-        res.second = arr[mid];
-        if(ans == 0)
-          break;
-      }
-      if(sum > 0) hi = mid;
-      else lo = mid;
+      res = std::abs(sum);
+      result.first = arr[l];
+      result.second = arr[r];
     }
-    if(ans == 0)
-      break;
+    if(sum > 0)
+      r--;
+    else
+      l++;
   }
-  std::cout << res.first << ' ' << res.second;
+  std::cout << result.first << ' ' << result.second;
   return 0;
 }
