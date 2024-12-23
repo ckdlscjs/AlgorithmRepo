e313640f118ce91f6d56fddf437d8cadf15363bf
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2024-12-23 20:28:18 +0900

    [Gold I] Title: 놀이 공원, Time: 4 ms, Memory: 2100 KB -BaekjoonHub

--- 백준/Gold/1561. 놀이 공원/놀이 공원.cc
+++ 백준/Gold/1561. 놀이 공원/놀이 공원.cc
@@ -1,42 +1,37 @@
 #include <bits/stdc++.h>
-using namespace std;
-long long int arr[10'005];
-long long int N, M;
-bool Check(long long int mid)
-{
-  long long int cnt = M;
-  for(int i = 0; i < M; i++)
-    cnt += mid / arr[i];
-  return N <= cnt;
-}
-int main() 
+long long int N, M, arr[10'005], cnt;
+int main()
 {
   std::ios::sync_with_stdio(false);
   std::cin.tie(0);
   std::cout.tie(0);
   std::cin >> N >> M;
-  for(int i = 0; i < M; i++)
-    std::cin >> arr[i];
+  for(int i = 0; i < M; i++) std::cin >> arr[i];
   if(N <= M)
   {
     std::cout << N;
     return 0;
   }
-  long long int lo = 0, hi = 60'000'000'005;
+  long long int lo = -1, hi = 60'000'000'005;
   while(lo + 1 < hi)
   {
     long long int mid = (lo + hi) / 2;
-    if(Check(mid)) hi = mid;
-    else lo = mid;
+    if([&](){
+      cnt = M;
+      for(int i = 0; i < M; i++)
+        cnt += mid / arr[i];
+      return cnt < N;
+    }()) lo = mid;
+    else hi = mid;
   }
-  long long int sum = M;
-  for(int i = 0; i < M; i++) sum += lo/arr[i];
+  cnt = M;
+  for(int i = 0; i < M; i++) cnt += lo / arr[i];
   for(int i = 0; i < M; i++)
   {
-    if(hi % arr[i] == 0) sum++;
-    if(sum == N)
+    if(hi % arr[i] == 0) cnt++;
+    if(cnt == N)
     {
-      std::cout << i+1;
+      std::cout << i + 1;
       return 0;
     }
   }
