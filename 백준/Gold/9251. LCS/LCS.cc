d37e4e1bcaacf40f2178c8fe9394d42d74f6179f
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2024-12-25 03:58:27 +0900

    [Gold V] Title: LCS, Time: 4 ms, Memory: 5972 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.cc
+++ 백준/Gold/9251. LCS/LCS.cc
@@ -1,15 +1,13 @@
 #include <bits/stdc++.h>
-using namespace std;
 std::string str1;
 std::string str2;
-int dp[1005][1005];
-int main() {
+int dp[1005][1005], len;
+int main()
+{
   std::ios::sync_with_stdio(false);
   std::cin.tie(0);
   std::cout.tie(0);
-  std::cin >> str1;
-  std::cin >> str2;
-  int res = 0;
+  std::cin >> str1 >> str2;
   for(int i = 1; i <= str1.size(); i++)
   {
     for(int j = 1; j <= str2.size(); j++)
@@ -18,9 +16,9 @@ int main() {
         dp[i][j] = dp[i-1][j-1] + 1;
       else
         dp[i][j] = std::max(dp[i-1][j], dp[i][j-1]);
-      res = std::max(res, dp[i][j]);
+      len = std::max(len, dp[i][j]);
     }
   }
-  std::cout << res;
+  std::cout << len;
   return 0;
 }
