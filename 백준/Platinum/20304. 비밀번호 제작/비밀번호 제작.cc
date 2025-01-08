c2588d50f46d8211de303b69a1abfc8f0cd5db63
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2025-01-09 04:11:50 +0900

    [Platinum V] Title: 비밀번호 제작, Time: 72 ms, Memory: 8764 KB -BaekjoonHub

---
+++ 백준/Platinum/20304. 비밀번호 제작/비밀번호 제작.cc
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+int N, M, dist[1'000'005], res;
+std::queue<int> q;
+int main()
+{
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(0);
+  std::cout.tie(0);
+  std::cin >> N >> M;
+  for(int i = 0; i < M; i++)
+  {
+    int val;
+    std::cin >> val;
+    dist[val] = 1;
+    q.push(val);
+  }
+  while(q.size())
+  {
+    int cur = q.front();
+    q.pop();
+    res = std::max(res, dist[cur]);
+    for(int i = 0; i <= 20; i++)
+    {
+      int nxt = cur ^ (1 << i);
+      if(nxt > N) continue;
+      if(dist[nxt]) continue;
+      dist[nxt] = dist[cur] + 1;
+      q.push(nxt);
+    }
+  }
+  std::cout << res-1;
+  return 0;
+}
