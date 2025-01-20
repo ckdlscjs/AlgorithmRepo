f50d123b7832ae86e5099867ba4ff97f99d3d4e5
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2025-01-20 17:11:31 +0900

    [Gold V] Title: 블랙 프라이데이, Time: 0 ms, Memory: 2060 KB -BaekjoonHub

--- 백준/Gold/18114. 블랙 프라이데이/블랙 프라이데이.cc
+++ 백준/Gold/18114. 블랙 프라이데이/블랙 프라이데이.cc
@@ -1,36 +1,47 @@
 #include <bits/stdc++.h>
-long long int N, C, arr[5'005];
-
+long long int N, C, W[5005];
 int main()
 {
   std::ios::sync_with_stdio(false);
   std::cin.tie(0);
   std::cout.tie(0);
   std::cin >> N >> C;
-  for(int n = 0; n < N; n++) std::cin >> arr[n];
-  std::sort(arr, arr+N, std::less<>());
-  for(int i = 0; i < N; i++)
+  for(int n = 0; n < N; n++)
   {
-    if(arr[i] == C)
+    std::cin >> W[n];
+    if(W[n] == C)
     {
       std::cout << 1;
       return 0;
     }
-    for(int j = i + 1; j < N; j++)
+  }
+  std::sort(W, W + N, std::less<>());
+  int left = 0, right = N-1;
+  while(left < right)
+  {
+    if(W[left] + W[right] > C)
     {
-      int lo = j, hi = N;
-      while(lo + 1 < hi)
-      {
-        int mid = (lo + hi) / 2;
-        if(arr[i] + arr[j] + arr[mid] >= C) hi = mid;
-        else lo = mid;
-      }
-      if(arr[i] + arr[j] + arr[hi] == C || arr[i] + arr[j] == C)
-      {
-        std::cout << 1;
-        return 0;
-      }
+      right--;
+      continue;
     }
+    else if(W[left] + W[right] == C)
+    {
+      std::cout << 1;
+      return 0;
+    }
+    int lo = 0, hi = N;
+    while(lo + 1 < hi)
+    {
+      int mid = (lo + hi) / 2;
+      if(W[left] + W[right] + W[mid] >= C) hi = mid;
+      else lo = mid;
+    }
+    if(W[left] + W[right] + W[hi] == C && hi != left && hi != right)
+    {
+      std::cout << 1;
+      return 0;
+    }
+    left++;
   }
   std::cout << 0;
   return 0;
