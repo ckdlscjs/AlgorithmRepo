9cfbe0b971bbb289eeafc21c839c88a4d128e41c
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2025-01-31 06:23:46 +0900

    [Gold IV] Title: 타임머신, Time: 8 ms, Memory: 2336 KB -BaekjoonHub

--- 백준/Gold/11657. 타임머신/타임머신.cc
+++ 백준/Gold/11657. 타임머신/타임머신.cc
@@ -1,46 +1,40 @@
 #include <bits/stdc++.h>
-const int INF = 98765421;
-int N, M, u, v, w;
-std::vector<std::pair<std::pair<int, int>, int>> edges;
+const long long int INF = 30'000'000'005;
+int N, M, a, b, c;
 long long int costs[505];
-bool Bellmanford(int s)
-{
-  costs[s] = 0;
-  for(int i = 0; i < N; i++)
-  {
-    for(const auto& iter : edges)
-    {
-      int u = iter.first.first;
-      int v = iter.first.second;
-      int w = iter.second;
-      if(costs[u] != INF && costs[u] + w < costs[v])
-      {
-        costs[v] = costs[u] + w;
-        if(i == N-1)
-          return false;
-      }
-    }
-  }
-  return true;
-}
-int main(void)
+std::vector<std::tuple<int, int, int>> edges;
+int main()
 {
   std::ios::sync_with_stdio(false);
   std::cin.tie(0);
   std::cout.tie(0);
-  std::fill_n(costs, 505, INF);
   std::cin >> N >> M;
-  for(int i = 0; i < M; i++)
+  std::fill_n(costs, 505, INF);
+  for(int m = 0; m < M; m++)
   {
-    std::cin >> u >> v >> w;
-    edges.push_back({{u, v}, w});
+    std::cin >> a >> b >> c;
+    edges.push_back(std::make_tuple(a, b, c));
   }
-  if(Bellmanford(1))
+  costs[1] = 0;
+  for(int i = 0; i < N; i++)
   {
-    for(int i = 2; i <= N; i++)
-      std::cout << (costs[i] == INF ? -1 : costs[i]) << '\n';
+    for(const auto& iter : edges)
+    {
+      a = std::get<0>(iter);
+      b = std::get<1>(iter);
+      c = std::get<2>(iter);
+      if(costs[a] != INF && costs[a] + c < costs[b])
+      {
+        costs[b] = costs[a] + c;
+        if(i == N-1)
+        {
+          std::cout << -1;
+          return 0;
+        }
+      }
+    }
   }
-  else
-    std::cout << -1;
+  for(int i = 2; i <= N; i++)
+    std::cout << (costs[i] == INF ? -1 : costs[i]) << '\n';
   return 0;
 }
