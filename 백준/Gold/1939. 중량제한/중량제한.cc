508e1a9633854e9ababd5683a4755abb8047e395
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2025-02-26 00:48:40 +0900

    [Gold III] Title: 중량제한, Time: 64 ms, Memory: 5552 KB -BaekjoonHub

---
+++ 백준/Gold/1939. 중량제한/중량제한.cc
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+int N, M, a, b, c, lo = 0, hi = 0;
+std::vector<std::pair<int, int>> graph[10'005];
+bool visited[10'005];
+void dfs(int cur, int mid)
+{
+  visited[cur] = true;
+  for(const auto& iter : graph[cur])
+  {
+    if(visited[iter.first] || mid > iter.second) continue;
+    dfs(iter.first, mid);
+  }
+}
+int main() 
+{
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(0);
+  std::cout.tie(0);
+  std::cin >> N >> M;
+  for(int m = 0; m < M; m++)
+  {
+    std::cin >> a >> b >> c;
+    graph[a].push_back({b, c});
+    graph[b].push_back({a, c});
+    hi = std::max(c, hi);
+  }
+  std::cin >> a >> b;
+  hi++;
+  while(lo + 1 < hi)
+  {
+    int mid = (lo + hi) / 2;
+    dfs(a, mid);
+    if(visited[b]) lo = mid;
+    else hi = mid;
+    std::memset(visited, false, sizeof(visited));
+  }
+  std::cout << lo;
+  return 0;
+}
