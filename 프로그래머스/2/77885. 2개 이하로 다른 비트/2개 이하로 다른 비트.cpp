c628c726d6f4488b60d94d15eaef8b88030cb22a
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2025-03-16 23:22:24 +0900

    [level 2] Title: 2개 이하로 다른 비트, Time: 30.84 ms, Memory: 27.8 MB -BaekjoonHub

--- 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.cpp
+++ 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.cpp
@@ -1,22 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
-//1'000'000'000'000'000 10^15
-long long int Check(const long long int num, int idx, int curidx, long long int cur)
-{
-    if(idx >= 2) return (cur > num ? cur : (long long int)1e16);
-    long long int ret = (long long int)1e16;
-    if(idx >= 1 && cur > num)
-        ret = std::min(ret, cur);
-    for(long long int i = curidx; i < 63; i++)
-        ret = std::min(ret, Check(num, idx+1, i+1, cur ^ (1LL << i)));
-    return ret;
-}
 vector<long long> solution(vector<long long> numbers) 
 {
     vector<long long> answer;
     for(const auto& iter : numbers)
     {
-        answer.push_back(Check(iter, 0, 0, iter));
+        long long int bit = 1;
+        while((bit & iter)) bit <<= 1;
+        answer.push_back(iter + bit - (bit>>1));
     }
     return answer;
 }
