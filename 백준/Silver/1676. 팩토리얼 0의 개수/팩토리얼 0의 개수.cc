ed273a170c52444da76bc7f794a95c8d525d75c9
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2025-03-18 17:03:34 +0900

    [Silver V] Title: 팩토리얼 0의 개수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.cc
+++ 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.cc
@@ -1,36 +1,20 @@
-#pragma once
 #include <bits/stdc++.h>
-int N;
-int main(void)
+int N, cnt5;
+int main()
 {
-	std::ios::sync_with_stdio(false);
-	std::cin.tie(0);
-	std::cout.tie(0);
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(NULL);
 	std::cin >> N;
-	int num2 = 0;
-	int num5 = 0;
-	for (int i = N; i > 0; i--)
+	while(N)
 	{
-		int num = i;
-		while (!(num % 2))
-		{
-			num2++;
-			num /= 2;
-		}
-		num = i;
-		while (!(num%5))
-		{
-			num5++;
-			num /= 5;
-		}
+	    int cur = N;
+	    while(cur % 5 == 0)
+	    {
+	        cnt5 ++;
+	        cur /= 5;
+	    }
+	    N--;
 	}
-	int result = 0;
-	while (num2 && num5)
-	{
-		num2--;
-		num5--;
-		result++;
-	}
-	std::cout << result;
+	std::cout << cnt5;
 	return 0;
 }
