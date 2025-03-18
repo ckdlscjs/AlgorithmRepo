ea6a2340984f42bcac8d3d4cc031800325ada0a4
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2025-03-19 03:14:29 +0900

    [Silver III] Title: 소수 구하기, Time: 8 ms, Memory: 2996 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.cc
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.cc
@@ -1,26 +1,21 @@
-//https://www.acmicpc.net/problem/1929
-#pragma once
 #include <bits/stdc++.h>
-
+bool notPrime[1'000'005];
 int M, N;
-bool Prime[1000005];
-int main(void)
+int main()
 {
-	std::ios::sync_with_stdio(false);
-	std::cin.tie(0);
-	std::cout.tie(0);
-	std::cin >> M >> N;
-	Prime[0] = Prime[1] = true;
-	for (int i = 0; i < 1000005; i++)
-	{
-		if (Prime[i])
-			continue;
-		for (int j = i+i; j < 1000005; j += i)
-			Prime[j] = true;
-	}
-	for (int i = M; i <= N; i++)
-		if (!Prime[i])
-			std::cout << i << '\n';
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    notPrime[0] = notPrime[1] = true;
+    for(int i = 2; i <= std::sqrt(1'000'000); i++)
+    {
+        if(notPrime[i]) continue;
+        for(int j = 2; i*j <= 1'000'000; j++) notPrime[i*j] = true;
+    }
+    std::cin >> M >> N;
+    for(int i = M; i <= N; i++)
+    {
+        if(notPrime[i]) continue;
+        std::cout << i << '\n';
+    }
 	return 0;
 }
-
