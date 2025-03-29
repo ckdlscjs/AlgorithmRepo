d2b6aa1746e9aede88e1dd80e4fe99f30276e700
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2025-03-29 10:27:38 +0900

    [Silver III] Title: 돌 게임 5, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9659. 돌 게임 5/돌 게임 5.cc
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+long long int N;
+int main() 
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    std::cin >> N;
+    std::cout << (N % 2 ? "SK" : "CY");
+    return 0;
+}
