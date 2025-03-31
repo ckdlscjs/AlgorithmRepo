1da5e43c805f31c477f181e9b91fac2ffeb45fa0
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2025-04-01 00:50:59 +0900

    [Gold III] Title: 파티, Time: 0 ms, Memory: 2336 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.cc
+++ 백준/Gold/1238. 파티/파티.cc
@@ -1,52 +1,47 @@
 #include <bits/stdc++.h>
-#define pii std::pair<int, int>
+using namespace std;
+using pii = std::pair<int, int>;
 const int INF = 987654321;
-int N, M, X, U, V, T, costs_xv[1005], costs_xu[1005];
-std::vector<pii> graph_uv[1005], graph_vu[1005];
-void Dijkstra(const std::vector<pii>* graph, int* costs)
+int N, M, X, u, v, w, res;
+std::vector<pii> UV[1'005];
+std::vector<pii> VU[1'005];
+void Dijkstra(const std::vector<pii>* graph, std::vector<int>& costs)
 {
-  std::priority_queue<pii, std::vector<pii>, std::greater<pii>> pq;
-  std::fill_n(costs, 1005, INF);
-  costs[X] = 0;
-  pq.push({0, X});
-  while(pq.size())
-  {
-    int cost = pq.top().first;
-    int from = pq.top().second;
-    pq.pop();
-    if(costs[from] < cost) continue;
-    for(const auto& iter : graph[from])
+    std::priority_queue<pii, std::vector<pii>, std::greater<pii>> pq;
+    costs[X] = 0;
+    pq.push({0, X});
+    while(pq.size())
     {
-      int to = iter.first;
-      int weight = iter.second;
-      if(costs[from] + weight < costs[to])
-      {
-        costs[to] = costs[from] + weight;
-        pq.push({costs[to], to});
-      }
+        int u = pq.top().second;
+        int w = pq.top().first;
+        pq.pop();
+        if(costs[u] < w) continue;
+        for(const auto& iter : graph[u])
+        {
+            if(costs[u] + iter.second > costs[iter.first]) continue;
+            costs[iter.first] = costs[u] + iter.second;
+            pq.push({costs[iter.first], iter.first});
+        }
     }
-  }
 }
-int main()
+int main() 
 {
-  std::ios::sync_with_stdio(false);
-  std::cin.tie(0);
-  std::cout.tie(0);
-  std::cin >> N >> M >> X;
-  for(int m = 0; m < M; m++)
-  {
-    std::cin >> U >> V >> T;
-    graph_uv[U].push_back({V, T});
-    graph_vu[V].push_back({U, T});
-  }
-  Dijkstra(graph_uv, costs_xv);
-  Dijkstra(graph_vu, costs_xu);
-  int res = 0;
-  for(int i = 1; i <= N; i++)
-  {
-    if(costs_xv[i] != INF && costs_xu[i] != INF)
-      res = std::max(res, costs_xv[i] + costs_xu[i]);
-  }
-  std::cout << res;
-  return 0;
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    std::cin >> N >> M >> X;
+    for(int m = 0; m < M; m++)
+    {
+        std::cin >> u >> v >> w;
+        UV[u].push_back({v, w});
+        VU[v].push_back({u, w});
+    }
+    std::vector<int> costs_uv(N+1, INF);
+    std::vector<int> costs_vu(N+1, INF);
+    Dijkstra(UV, costs_uv);
+    Dijkstra(VU, costs_vu);
+    
+    for(int i = 1; i <= N; i++)
+        res = std::max(res, costs_uv[i] + costs_vu[i]);
+    std::cout << res;
+    return 0;
 }
