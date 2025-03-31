c55c10e9e9393817ba4498c413f8ec6647696fc5
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2025-03-31 14:13:13 +0900

    [Gold V] Title: 회장뽑기, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Gold/2660. 회장뽑기/회장뽑기.cc
+++ 백준/Gold/2660. 회장뽑기/회장뽑기.cc
@@ -1,37 +1,45 @@
 #include <bits/stdc++.h>
 using namespace std;
-const int INF = 52;
-int N, a, b, dp[52][52];
+int N, a, b;
+std::vector<int> graph[52];
+std::vector<int> dists[52];
 int main() 
 {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
     std::cin >> N;
-    std::fill_n(&dp[0][0], 52*52, INF);
-    for(int i = 1; i <= N; i++) dp[i][i] = 0;
     while(std::cin >> a >> b)
     {
         if(a == -1 && b == -1) break;
-        dp[a][b] = 1;
-        dp[b][a] = 1;
+        graph[a].push_back(b);
+        graph[b].push_back(a);
     }
-    for(int k = 1; k <= N; k++)
+    for(int i = 1; i <= N; i++)
     {
-        for(int i = 1; i <= N; i++)
+        int maxdist = 1;
+        std::vector<int> dist(N+1, 0);
+        std::queue<int> q;
+        q.push(i);
+        dist[i] = 1;
+        while(q.size())
         {
-            for(int j = 1; j <= N; j++)
+            int cur = q.front();
+            q.pop();
+            for(const auto& iter : graph[cur])
             {
-                dp[i][j] = std::min(dp[i][j], dp[i][k] + dp[k][j]);
+                if(dist[iter]) continue;
+                q.push(iter);
+                dist[iter] = dist[cur] + 1;
+                maxdist = std::max(maxdist, dist[iter]);
             }
         }
+        dists[maxdist-1].push_back(i);
     }
-    std::vector<int> results[52];
-    for(int i = 1; i <= N; i++) results[*std::max_element(dp[i]+1, dp[i] + 1 + N)].push_back(i);
     for(int i = 1; i <= 52; i++)
     {
-        if(results[i].empty()) continue;
-        std::cout << i << ' ' << results[i].size() << '\n';
-        for(const auto& iter : results[i])
+        if(dists[i].empty()) continue;
+        std::cout << i << ' ' << dists[i].size() << '\n';
+        for(const auto& iter : dists[i])
             std::cout << iter << ' ';
         return 0;
     }
