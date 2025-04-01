c2baa96e4cfc13b13704f985b98cd2df0f1e5ce2
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2025-04-01 11:56:41 +0900

    [Gold III] Title: 웜홀, Time: 16 ms, Memory: 2336 KB -BaekjoonHub

--- 백준/Gold/1865. 웜홀/웜홀.cc
+++ 백준/Gold/1865. 웜홀/웜홀.cc
@@ -1,46 +1,51 @@
 #include <bits/stdc++.h>
 using namespace std;
-int T, N, M, W, u, v, w, costs[505];
-const int INF = 987654321;
+using tiii = std::tuple<int, int, int>;
+const int INF = 10'000*500 + 5;
+int TC, N, M, W, S, E, T;
 int main() 
 {
-  std::ios::sync_with_stdio(false);
-  std::cin.tie(0);
-  std::cout.tie(0);
-  std::cin >> T;
-  for(int t = 0; t < T; t++)
-  {
-    std::fill_n(costs, 505, INF);
-    std::cin >> N >> M >> W;
-    std::vector<std::pair<std::pair<int, int>, int>> edges;
-    for(int i = 0; i < M; i++)
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    std::cin >> TC;
+    for(int tc = 0; tc < TC; tc++)
     {
-      std::cin >> u >> v >> w;
-      edges.push_back({{u, v}, w});
-      edges.push_back({{v, u}, w});
-    }
-    for(int i = 0; i < W; i++)
-    {
-      std::cin >> u >> v >> w;
-      edges.push_back({{u, v}, -w});
-    }
-    bool negative = false;
-    for(int i = 0; i < N; i++)
-    {
-      for(const auto& iter : edges)
-      {
-        int u = iter.first.first;
-        int v = iter.first.second;
-        int w = iter.second;
-        if(costs[u] + w < costs[v])
+        std::cin >> N >> M >> W;
+        std::vector<int> costs(N+1, INF);
+        std::vector<tiii> edges;
+        //도로는 양방향, 웜홀은 단방향이다
+        for(int m = 0; m < M; m++)
         {
-          if(i == N-1)
-            negative = true;
-          costs[v] = costs[u] + w;
+            std::cin >> S >> E >> T;
+            edges.push_back({S, E, T});
+            edges.push_back({E, S, T});
         }
-      }
+        for(int w = 0; w < W; w++)
+        {
+            std::cin >> S >> E >> T;
+            edges.push_back({S, E, -T});
+        }
+        bool chk = false;
+        for(int i = 0; i < N; i++)
+        {
+            for(const auto& iter : edges)
+            {
+                int u = std::get<0>(iter);
+                int v = std::get<1>(iter);
+                int w = std::get<2>(iter);
+                if(costs[u] + w < costs[v]) //음수간선은 알아서 줄기때문에 relaxation에서 costs[임의정점] = 0 의초기화가 필요없다
+                {
+                    if(i == N-1)
+                    {
+                        chk = true;
+                        break;
+                    }
+                    costs[v] = costs[u] + w;
+                }
+            }
+            if(chk) break;
+        }
+        std::cout << (chk ? "YES" : "NO") << '\n';
     }
-    std::cout << (negative ? "YES" : "NO") << '\n';
-  }
-  return 0;
+    return 0;
 }
