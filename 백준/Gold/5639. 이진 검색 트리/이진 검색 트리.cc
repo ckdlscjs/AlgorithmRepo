b362f84b934174cac51eb6e9c6a226e15c3243d0
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2025-04-03 18:50:40 +0900

    [Gold IV] Title: 이진 검색 트리, Time: 24 ms, Memory: 2496 KB -BaekjoonHub

--- 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.cc
+++ 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.cc
@@ -1,23 +1,25 @@
 #include <bits/stdc++.h>
-int node;
+using namespace std;
+int val;
 std::vector<int> trees;
 void PostOrder(int s, int e)
 {
-	if(s >= e) return;
-	int delim = s+1;
-	while(delim < e)
-	{
-		if(trees[s] < trees[delim]) break;
-		delim++;
-	}
-	PostOrder(s+1, delim);
-	PostOrder(delim, e);
-	std::cout << trees[s] << '\n';
+    if(s > e) return;
+    int delim = s + 1;
+    while(delim <= e)
+    {
+        if(trees[s] < trees[delim]) break;
+        delim++;
+    }
+    PostOrder(s+1, delim-1);
+    PostOrder(delim, e);
+    std::cout << trees[s] << '\n';
 }
 int main() 
 {
-	while(std::cin >> node)
-		trees.push_back(node);
-	PostOrder(0, trees.size());
-	return 0;
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    while(std::cin >> val)
+        trees.push_back(val);
+    PostOrder(0, trees.size()-1);
 }
