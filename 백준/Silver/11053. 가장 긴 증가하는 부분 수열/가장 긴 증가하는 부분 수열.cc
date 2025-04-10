d1d49835d85b99573688909ae92f0db0159fc4c7
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2025-04-11 04:50:14 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.cc
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.cc
@@ -1,31 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
-int N, len, arr[1'005], lis[1'005];
+int N, lis[1'005], len, val;
 int main() 
 {
-  std::ios::sync_with_stdio(false);
-  std::cin.tie(0);
-  std::cout.tie(0);
-  std::cin >> N;
-  for(int i = 0; i < N; i++)
-  {
-    std::cin >> arr[i];
-    if(i == 0 || lis[len-1] < arr[i])
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    std::cin >> N;
+    for(int i = 0; i < N; i++)
     {
-      lis[len++] = arr[i];
+        std::cin >> val;
+        if(len == 0 || lis[len-1] < val)
+        {
+            lis[len++] = val;
+        }
+        else
+        {
+            int lo = -1, hi = len;
+            while(lo + 1 < hi)
+            {
+                int mid = (lo + hi) / 2;
+                if(val <= lis[mid]) hi = mid;
+                else lo = mid;
+            }
+            lis[hi] = val;
+        }
     }
-    else
-    {
-      int lo = -1, hi = len;
-      while(lo + 1 < hi)
-      {
-        int mid = (lo + hi) /2;
-        if(arr[i] <= lis[mid]) hi = mid;
-        else lo = mid;
-      }
-      lis[hi] = arr[i];
-    }
-  }
-  std::cout << len << '\n';
-  return 0;
+    std::cout << len;
+    return 0;
 }
