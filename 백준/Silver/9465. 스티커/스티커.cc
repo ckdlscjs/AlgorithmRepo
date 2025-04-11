be6950d70e214c3692b91c531baa0be9c4b4d609
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2025-04-11 20:38:11 +0900

    [Silver I] Title: 스티커, Time: 72 ms, Memory: 3584 KB -BaekjoonHub

--- 백준/Silver/9465. 스티커/스티커.cc
+++ 백준/Silver/9465. 스티커/스티커.cc
@@ -16,8 +16,8 @@ int main()
         std::cin >> arr[i][j];
     for(int j = 2; j <= n+1; j++)
     {
-      dp[0][j] = std::max({dp[0][j-2], dp[1][j-2], dp[1][j-1]}) + arr[0][j];
-      dp[1][j] = std::max({dp[1][j-2], dp[0][j-2], dp[0][j-1]}) + arr[1][j];
+      dp[0][j] = std::max({dp[1][j-2], dp[1][j-1]}) + arr[0][j];
+      dp[1][j] = std::max({dp[0][j-2], dp[0][j-1]}) + arr[1][j];
     }
     std::cout << std::max(dp[0][n+1], dp[1][n+1]) << '\n';
   }
