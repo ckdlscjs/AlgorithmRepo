81c056b7b4444e00cb3957c806b6f42a037d8a7f
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2025-04-14 13:55:43 +0900

    [Gold V] Title: 회문, Time: 12 ms, Memory: 2500 KB -BaekjoonHub

---
+++ 백준/Gold/17609. 회문/회문.cc
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+int T;
+std::string str;
+int main() 
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    std::cin >> T;
+    while(T--)
+    {
+        std::cin >> str;
+        int l = 0, r = str.size()-1;
+        std::function<int(int, int, int)> isPalindrome = [&](int l, int r, int cnt) -> int {
+            while(l <= r)
+            {
+                if(str[l] == str[r])
+                {
+                    l++;
+                    r--;
+                    continue;
+                }
+                if(cnt <= 0)
+                {
+                    if(isPalindrome(l+1, r, cnt+1) == 0 || isPalindrome(l, r-1, cnt+1) == 0)
+                        return 1;
+                    else
+                        return 2;
+                }
+                else
+                    return 2;
+            }
+            return 0;
+        };
+        std::cout << isPalindrome(l, r, 0) << '\n';
+    }
+    return 0;
+}
