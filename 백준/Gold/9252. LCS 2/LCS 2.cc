48bad1b4b8fee9af72bd7c3a713bed30cf0c76ef
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2025-04-16 04:24:45 +0900

    [Gold IV] Title: LCS 2, Time: 4 ms, Memory: 5968 KB -BaekjoonHub

--- 백준/Gold/9252. LCS 2/LCS 2.cc
+++ 백준/Gold/9252. LCS 2/LCS 2.cc
@@ -1,60 +1,56 @@
 #include <bits/stdc++.h>
-std::string str1;
-std::string str2;
-int dp[1005][1005], len;
-std::pair<int, int> pos;
-int main()
+using namespace std;
+std::string str1, str2;
+const int dy[] = {-1, 0};
+const int dx[] = {0, -1};
+int dp[1005][1005];
+int main() 
 {
-  std::ios::sync_with_stdio(false);
-  std::cin.tie(0);
-  std::cout.tie(0);
-  std::cin >> str1 >> str2;
-  for(int i = 1; i <= str1.size(); i++)
-  {
-    for(int j = 1; j <= str2.size(); j++)
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    std::cin >> str1 >> str2;
+    for(int i = 1; i <= str1.size(); i++)
     {
-      if(str1[i-1] == str2[j-1])
-        dp[i][j] = dp[i-1][j-1] + 1;
-      else
-        dp[i][j] = std::max(dp[i-1][j], dp[i][j-1]);
-      if(len < dp[i][j])
-      {
-        len = dp[i][j];
-        pos = std::make_pair(i, j);
-      }
+        for(int j = 1; j <= str2.size(); j++)
+        {
+            if(str1[i-1] == str2[j-1])
+                dp[i][j] = dp[i-1][j-1] + 1;
+            else
+                dp[i][j] = std::max(dp[i-1][j], dp[i][j-1]);
+        }
     }
-  }
-  std::cout << len;
-  const int dy[] = {0, -1};
-  const int dx[] = {-1, 0};
-  std::stack<char> st;
-  while(len)
-  {
-    int nd = -1;
-    for(int dir = 0; dir < 2; dir++)
+    std::cout << dp[str1.size()][str2.size()] << '\n';
+    int y = str1.size(), x = str2.size();
+    std::stack<char> st;
+    while(dp[y][x])
     {
-      int ny = pos.first + dy[dir];
-      int nx = pos.second + dx[dir];
-      if(dp[ny][nx] == len)
-      {
-        nd = dir;
-        break;
-      }
+        int nd = -1;
+        for(int dir = 0; dir < 2; dir++)
+        {
+            int ny = y + dy[dir];
+            int nx = x + dx[dir];
+            if(dp[ny][nx] == dp[y][x])
+            {
+                nd = dir;
+                break;
+            }
+        }
+        if(nd != -1)
+        {
+            y += dy[nd];
+            x += dx[nd];
+        }
+        else
+        {
+            st.push(str1[y-1]);
+            y--;
+            x--;
+        }
     }
-    if(nd != -1)
+    while(st.size())
     {
-      pos.first = pos.first + dy[nd];
-      pos.second = pos.second + dx[nd];
+        std::cout << st.top();
+        st.pop();
     }
-    else
-    {
-      st.push(str1[pos.first-1]);
-      pos.first -= 1;
-      pos.second -= 1;
-      len--;
-    }
-  }
-  while(st.size())
-    std::cout << st.top(), st.pop();
-  return 0;
+    return 0;
 }
