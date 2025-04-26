873f8e31bfb6a090d78b68bcd6bde4f572588ac2
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2025-04-26 15:25:54 +0900

    [Gold IV] Title: 수 나누기 게임, Time: 32 ms, Memory: 7292 KB -BaekjoonHub

--- 백준/Gold/27172. 수 나누기 게임/수 나누기 게임.cc
+++ 백준/Gold/27172. 수 나누기 게임/수 나누기 게임.cc
@@ -1,7 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 int N, arr[100'005];
-std::map<int, int> cnts;
+bool chks[1'000'005];
+int cnts[1'000'005];
 int main() 
 {
     ios::sync_with_stdio(false);
@@ -10,16 +11,17 @@ int main()
     for(int n = 0; n < N; n++)
     {
         std::cin >> arr[n];
-        cnts[arr[n]] = 0;
+        chks[arr[n]] = true;
     }
-    for(const auto& iter : cnts)
+    for(int i = 1; i <= 1'000'000; i++)
     {
-        for(int s = 2; iter.first * s <= (--cnts.end())->first; s++)
+        if(!chks[i]) continue;
+        for(int s = 2; i * s <= 1'000'000; s++)
         {
-            if(cnts.find(iter.first * s) != cnts.end())
+            if(chks[i*s])
             {
-                cnts[iter.first]++;
-                cnts[iter.first*s]--;
+                cnts[i]++;
+                cnts[i*s]--;
             }
         }
     }
