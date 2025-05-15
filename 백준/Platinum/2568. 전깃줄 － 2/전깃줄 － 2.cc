85525ec26f66610efb933d6eef537906f3f38e20
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2025-05-15 16:00:59 +0900

    [Platinum V] Title: 전깃줄 - 2, Time: 32 ms, Memory: 3984 KB -BaekjoonHub

--- 백준/Platinum/2568. 전깃줄 － 2/전깃줄 － 2.cc
+++ 백준/Platinum/2568. 전깃줄 － 2/전깃줄 － 2.cc
@@ -1,51 +1,53 @@
 #include <bits/stdc++.h>
 using namespace std;
-int N, len, lis[100'005], idxs[100'005];
-std::stack<int> st;
+int N, A, B, lis[100'005], idx[100'005], len;
 std::pair<int, int> inputs[100'005];
 int main() 
 {
-  std::ios::sync_with_stdio(false);
-  std::cin.tie(0);
-  std::cout.tie(0);
-  std::cin >> N;
-  for(int i = 0; i < N; i++)
-    std::cin >> inputs[i].first >> inputs[i].second;
-  std::sort(inputs, inputs+N, [](const std::pair<int, int>& a, const std::pair<int, int>& b)
-  {
-    return a.first < b.first;
-  });
-  for(int i = 0; i < N; i++)
-  {
-    if(i == 0 || inputs[lis[len-1]].second < inputs[i].second)
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    std::cin >> N;
+    for(int n = 0; n < N; n++) std::cin >> inputs[n].first >> inputs[n].second;
+    std::sort(inputs, inputs + N);
+    for(int i = 0; i < N; i++)
     {
-      idxs[i] = len;
-      lis[len++] = i;
+        if(len == 0 || lis[len-1] < inputs[i].second)
+        {
+            idx[i] = len;
+            lis[len++] = inputs[i].second;
+        }
+        else
+        {
+            int lo = -1, hi = len;
+            while(lo + 1 < hi)
+            {
+                int mid = (lo + hi) / 2;
+                if(inputs[i].second <= lis[mid]) hi = mid;
+                else lo = mid;
+            }
+            idx[i] = hi;
+            lis[hi] = inputs[i].second;
+        }
     }
-    else
+    std::cout << N - len << '\n';
+    len--;
+    std::stack<int> st;
+    for(int i = N-1; i >= 0; i--)
     {
-      int lo = -1, hi = len;
-      while(lo + 1 < hi)
-      {
-        int mid = (lo + hi) / 2;
-        if(inputs[i].second <= inputs[lis[mid]].second) hi = mid;
-        else lo = mid;
-      }
-      idxs[i] = hi;
-      lis[hi] = i;
+        if(idx[i] == len)
+        {
+            len--;
+        }
+        else
+        {
+            st.push(inputs[i].first);
+        }
     }
-  }
-  len--;
-  for(int i = N-1; i >= 0; i--)
-    if(idxs[i] == len)
-      len--;
-    else
-      st.push(i);
-  std::cout << st.size() << '\n';
-  while(st.size())
-  {
-    std::cout << inputs[st.top()].first << '\n';
-    st.pop();
-  }
-  return 0;
+    while(st.size())
+    {
+        std::cout << st.top() << '\n';
+        st.pop();
+    }
+    
+    return 0;
 }
