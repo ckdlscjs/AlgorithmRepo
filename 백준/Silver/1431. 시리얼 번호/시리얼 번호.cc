e318e3f95cf03202324288b323401f107fc9d60d
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2025-05-19 01:30:58 +0900

    [Silver III] Title: 시리얼 번호, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Silver/1431. 시리얼 번호/시리얼 번호.cc
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+int N;
+std::vector<std::string> arr;
+int main() 
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    std::cin >> N;
+    arr.resize(N);
+    for(int n = 0; n < N; n++) std::cin >> arr[n];
+    std::sort(arr.begin(), arr.end(), [](const std::string& a, const std::string& b)->bool {
+        if(a.size() == b.size())
+        {
+            int sum_a = 0;
+            for(const auto& iter : a)
+                if(std::isdigit(iter)) sum_a += iter - '0';
+            int sum_b = 0;
+            for(const auto& iter : b)
+                if(std::isdigit(iter)) sum_b += iter - '0';
+            if(sum_a == sum_b)
+            {
+                for(int i = 0; i < a.size(); i++)
+                {
+                    if(a[i] == b[i]) continue;
+                    return a[i] < b[i];
+                }
+            }
+            return sum_a < sum_b;
+        }
+        return a.size() < b.size();
+    });
+    for(const auto& iter : arr)
+        std::cout << iter << '\n';
+    return 0;
+}
