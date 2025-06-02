6cc071e3a3d0787ac81bb4562bbff65d3acadcad
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2025-06-02 15:26:08 +0900

    [Gold III] Title: 소문난 칠공주, Time: 0 ms, Memory: 6116 KB -BaekjoonHub

--- 백준/Gold/1941. 소문난 칠공주/소문난 칠공주.cc
+++ 백준/Gold/1941. 소문난 칠공주/소문난 칠공주.cc
@@ -1,49 +1,49 @@
+/*
+1.접근방식:
+
+2.시간복잡도:
+
+*/
 #include <bits/stdc++.h>
-using namespace std;
-const int dy[] = {0, 0, 1, -1};
-const int dx[] = {1, -1, 0, 0};
-std::string arr[5];
-bool chks[1 << 25];
-int sum = 0;
-void Check(int cnt, int scnt, int mask)
+const int dy[] = {-1, 1, 0, 0};
+const int dx[] = {0, 0, -1, 1};
+char arr[27][27];
+std::bitset<(1<<25)> visited;
+int Check(int Scnt, int cur, int idx, int mask)
 {
-  chks[mask] = true;
-  if(cnt == 7)
-  {
-    if(scnt >= 4)
-      sum++;
-    return;
-  }
-  for(int i = 0; i < 25; i++)
-  {
-    if((mask & (1 << i)) == 0) continue;
-    int y = i/5;
-    int x = i%5;
-    for(int dir = 0; dir < 4; dir++)
+    if(visited[mask]) return 0;
+    visited[mask] = true;
+    if(cur >= 7) return (Scnt >= 4);
+    int ret = 0;
+    for(int i = 0; i < 25; i++)
     {
-      int ny = y + dy[dir];
-      int nx = x + dx[dir];
-      int num = ny * 5 + nx;
-      if(ny < 0 || nx < 0 || ny >= 5 || nx >= 5) continue;
-      if(chks[mask | (1 << num)]) continue;
-      Check(cnt + 1, scnt + (arr[ny][nx] == 'S' ? 1 : 0), mask | (1 << num)); 
+        if((mask & (1 << i)) == 0) continue;
+        int cy = i / 5;
+        int cx = i % 5;
+        for(int dir = 0; dir < 4; dir++)
+        {
+            int ny = cy + dy[dir];
+            int nx = cx + dx[dir];
+            int num = ny*5 + nx;
+            if(ny < 0 || nx < 0 || ny >= 5 || nx >= 5) continue;
+            ret += Check(Scnt + (arr[ny][nx] == 'S'), cur+1, i+1, mask | (1<<num));
+        }
     }
-  }
+    return ret;
 }
 int main() 
 {
-  std::ios::sync_with_stdio(false);
-  std::cin.tie(0);
-  std::cout.tie(0);
-  for(int i = 0; i < 5; i++)
-    std::cin >> arr[i];
-  for(int i = 0; i < 5; i++)
-  {
-    for(int j = 0; j < 5; j++)
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    for(int i = 0; i < 5; i++) std::cin >> arr[i];
+    int res = 0;
+    for(int i = 0; i < 25; i++)
     {
-      Check(1, (arr[i][j] == 'S' ? 1 : 0), 1 << (i * 5 + j));
+        int cy = i / 5;
+        int cx = i % 5;
+        int num = cy*5 + cx;
+        res += Check((arr[cy][cx] == 'S'), 1, 0, (1<<num));
     }
-  }
-  std::cout << sum;
-  return 0;
+    std::cout << res;
+    return 0;
 }
