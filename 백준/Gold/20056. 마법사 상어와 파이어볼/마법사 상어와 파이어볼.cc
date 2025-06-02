2167387ccd52ad779a2834bb9473a99f9afa484d
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2025-06-03 05:52:49 +0900

    [Gold IV] Title: 마법사 상어와 파이어볼, Time: 12 ms, Memory: 2352 KB -BaekjoonHub

--- 백준/Gold/20056. 마법사 상어와 파이어볼/마법사 상어와 파이어볼.cc
+++ 백준/Gold/20056. 마법사 상어와 파이어볼/마법사 상어와 파이어볼.cc
@@ -6,11 +6,13 @@
 */
 #include <bits/stdc++.h>
 using pii = std::pair<int, int>;
-using ti5 = std::tuple<int, int, int, int, int>;
 using ti3 = std::tuple<int, int, int>;
+using ti5 = std::tuple<int, int, int, int, int>;
+using ti6 = std::tuple<int, int, int, int, int, int>;
 const int dy[] = {-1, -1, 0, 1, 1, 1, 0, -1};
 const int dx[] = {0, 1, 1, 1, 0, -1, -1, -1};
-int N, M, K, r, c, m, s, d, res;
+int N, M, K, res;
+ti6 boards[52][52];
 std::queue<ti5> q;
 int main() 
 {
@@ -19,65 +21,77 @@ int main()
     std::cin >> N >> M >> K;
     for(int i = 0; i < M; i++)
     {
+        int r, c, m, s, d;
         std::cin >> r >> c >> m >> s >> d;
         q.push({r-1, c-1, m, s, d});
     }
     for(int k = 0; k < K; k++)
     {
         res = 0;
-        std::map<pii, std::vector<ti3>> boards;
         while(q.size())
         {
             auto cur = q.front();
             q.pop();
-            r = std::get<0>(cur);
-            c = std::get<1>(cur);
-            m = std::get<2>(cur);
-            s = std::get<3>(cur);
-            d = std::get<4>(cur);
-            int ny = r + dy[d] * (s%N);
-            if(ny < 0) ny += N;
+            int y = std::get<0>(cur);
+            int x = std::get<1>(cur);
+            int m = std::get<2>(cur);
+            int s = std::get<3>(cur);
+            int d = std::get<4>(cur);
+            int ny = y + dy[d] * s;
+            if(ny < 0) ny = N - (-ny % N);
             ny %= N;
-            int nx = c + dx[d] * (s%N);
-            if(nx < 0) nx += N;
+            int nx = x + dx[d] * s;
+            if(nx < 0) nx = N - (-nx % N);
             nx %= N;
-            boards[{ny, nx}].push_back({m, s, d});
+            std::get<0>(boards[ny][nx]) += (d % 2 ? 1 : 0);     //odd
+            std::get<1>(boards[ny][nx]) += (d % 2 ? 0 : 1);     //even
+            std::get<2>(boards[ny][nx]) += m;                   //m
+            std::get<3>(boards[ny][nx]) += s;                   //s
+            std::get<4>(boards[ny][nx]) = d;                    //d
+            std::get<5>(boards[ny][nx]) += 1;                   //cnt
         }
-        for(const auto& iter : boards)
+        for(int i = 0; i < N; i++)
         {
-            if(iter.second.size() <= 1)
+            for(int j = 0; j < N; j++)
             {
-                q.push({iter.first.first, iter.first.second, std::get<0>(iter.second[0]), std::get<1>(iter.second[0]), std::get<2>(iter.second[0])});
-                res += std::get<0>(iter.second[0]);
-            }
-            else
-            {
-                int sum_m = 0, sum_s = 0, cnt_odd = 0, cnt_even = 0;
-                for(const auto& fb : iter.second)
+                int odd = std::get<0>(boards[i][j]);      //odd
+                int even = std::get<1>(boards[i][j]);     //even
+                int m = std::get<2>(boards[i][j]);        //m
+                int s = std::get<3>(boards[i][j]);        //s
+                int d = std::get<4>(boards[i][j]);        //d
+                int cnt = std::get<5>(boards[i][j]);      //cnt
+                std::get<0>(boards[i][j]) = 0;
+                std::get<1>(boards[i][j]) = 0;
+                std::get<2>(boards[i][j]) = 0;
+                std::get<3>(boards[i][j]) = 0;
+                std::get<4>(boards[i][j]) = 0;
+                std::get<5>(boards[i][j]) = 0;
+                if(cnt == 1)
                 {
-                    sum_m += std::get<0>(fb);
-                    sum_s += std::get<1>(fb);
-                    cnt_odd += std::get<2>(fb) % 2 ? 1 : 0;
-                    cnt_even += std::get<2>(fb) % 2 ? 0 : 1;
+                    q.push({i, j, m, s, d});
+                    res += m;
                 }
-                sum_m = std::floor(sum_m/5);
-                sum_s = std::floor(sum_s/iter.second.size());
-                if(sum_m <= 0) continue;
-                if(cnt_odd >= iter.second.size() || cnt_even >= iter.second.size())
+                else if(cnt > 1)
                 {
-                    q.push({iter.first.first, iter.first.second, sum_m, sum_s, 0});
-                    q.push({iter.first.first, iter.first.second, sum_m, sum_s, 2});
-                    q.push({iter.first.first, iter.first.second, sum_m, sum_s, 4});
-                    q.push({iter.first.first, iter.first.second, sum_m, sum_s, 6});
+                    m = std::floor(m/5);
+                    s = std::floor(s/cnt);
+                    if(m <= 0) continue;
+                    if(odd >= cnt || even >= cnt)
+                    {
+                        q.push({i, j, m, s, 0});
+                        q.push({i, j, m, s, 2});
+                        q.push({i, j, m, s, 4});
+                        q.push({i, j, m, s, 6});
+                    }
+                    else
+                    {
+                        q.push({i, j, m, s, 1}); 
+                        q.push({i, j, m, s, 3});
+                        q.push({i, j, m, s, 5});
+                        q.push({i, j, m, s, 7});
+                    }
+                    res += m*4;
                 }
-                else
-                {
-                    q.push({iter.first.first, iter.first.second, sum_m, sum_s, 1});
-                    q.push({iter.first.first, iter.first.second, sum_m, sum_s, 3});
-                    q.push({iter.first.first, iter.first.second, sum_m, sum_s, 5});
-                    q.push({iter.first.first, iter.first.second, sum_m, sum_s, 7});
-                }
-                res += sum_m*4;
             }
         }
     }
