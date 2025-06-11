f2e9ae1fc85ed30e7ed1fce990b51e09f217af59
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2025-06-11 22:03:01 +0900

    [Silver IV] Title: 비밀번호 찾기, Time: 108 ms, Memory: 16448 KB -BaekjoonHub

--- 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.cc
+++ 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.cc
@@ -1,21 +1,28 @@
+/*
+1.접근방식:
+
+2.시간복잡도:
+
+*/
 #include <bits/stdc++.h>
 int N, M;
-std::string key, value;
-std::unordered_map<std::string, std::string> chks;
-int main()
+std::unordered_map<std::string, std::string> sp;
+int main() 
 {
     std::ios::sync_with_stdio(false);
     std::cin.tie(NULL);
     std::cin >> N >> M;
     for(int n = 0; n < N; n++)
     {
-        std::cin >> key >> value;
-        chks[key] = value;
+        std::string site, pass;
+        std::cin >> site >> pass;
+        sp[site] = pass;
     }
     for(int m = 0; m < M; m++)
     {
-        std::cin >> key;
-        std::cout << chks[key] << '\n';
+        std::string site;
+        std::cin >> site;
+        std::cout << sp[site] << '\n';
     }
-	return 0;
+    return 0;
 }
