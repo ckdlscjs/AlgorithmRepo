785fe1e90b3310c531ebb78bee0333b2027f035d
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2025-06-23 21:41:30 +0900

    [Gold II] Title: 원판 돌리기, Time: 0 ms, Memory: 2172 KB -BaekjoonHub

--- 백준/Gold/17822. 원판 돌리기/원판 돌리기.cc
+++ 백준/Gold/17822. 원판 돌리기/원판 돌리기.cc
@@ -1,83 +1,107 @@
+/*
+1.접근방식:
+
+2.시간복잡도:
+
+*/
 #include <bits/stdc++.h>
-using namespace std;
-const int dy[] = {0, 0, 1, -1};
-const int dx[] = {1, -1, 0, 0};
-bool visited[55][55];
-int arr[55][55];
-int N, M, T, x, d, k, res;
+const int dy[] = {1, 0, 0};
+const int dx[] = {0, -1, 1};
+int N, M, T, arr[52][52], res;
 int main()
 {
-  ios::sync_with_stdio(false);
-  cin.tie(0);
-  cout.tie(0);
-  std::cin >> N >> M >> T;
-  for(int i = 1; i <= N; i++)
-    for(int j = 0; j < M; j++)
-      std::cin >> arr[i][j];
-  for(int t = 0; t < T; t++)
-  {
-    std::cin >> x >> d >> k;
-    d = d ? M-1 : 1;
-    std::queue<std::pair<int, std::pair<int, int>>> q;
-    for(int m = 1; m*x <= N; m++)
+	std::ios::sync_with_stdio(false);
+	std::cin.tie(NULL);
+    std::cin >> N >> M >> T;
+    for(int i = 0; i < N; i++)
+        for(int j = 0; j < M; j++)
+            std::cin >> arr[i][j];
+    for(int t = 0; t < T; t++)
     {
-      int xidx = m*x;
-      int temp[55];
-      std::memset(temp, 0, sizeof(temp));
-      for(int j = 0; j < M; j++) temp[(j+k*d) % M] = arr[xidx][j];
-      for(int j = 0; j < M; j++) arr[xidx][j] = temp[j];
+        int x, d, k;
+        std::cin >> x >> d >> k;
+        d = d == 0? 1 : -1;
+        k %= M;
+        for(int i = x-1; i < N; i += x)
+        {
+            int temp[52];
+            for(int j = 0; j < M; j++)
+            {
+                int nj = j + d*k;
+                if(nj < 0) 
+                    nj += M;
+                nj %= M;
+                temp[nj] = arr[i][j];
+            }
+            std::memcpy(arr[i], temp, sizeof(temp));
+        }
+        /*
+        for(int i = 0; i < N; i++)
+        {
+           for(int j = 0; j < M; j++)
+           {
+               std::cout << arr[i][j] << ' ';
+           }
+           std::cout << '\n';
+        }
+        std::cout << '\n';
+        */
+        std::set<std::pair<int, int>> chks;
+        std::queue<std::pair<int, int>> tempq;
+        int tempsum = 0, tempcnt = 0;
+        for(int i = 0; i < N; i++)
+        {
+           for(int j = 0; j < M; j++)
+           {
+               if(arr[i][j] == 0) continue;
+               tempq.push({i, j});
+               tempsum += arr[i][j];
+               tempcnt += 1;
+               for(int dir = 0; dir < 3; dir++)
+               {
+                   int ny = i + dy[dir];
+                   if(ny >= N) continue;
+                   int nx = j + dx[dir];
+                   if(nx < 0) nx += M;
+                   nx %= M;
+                   if(arr[i][j] == arr[ny][nx])
+                   {
+                       chks.insert({i, j});
+                       chks.insert({ny, nx});
+                   }
+               }
+           }
+        }
+        if(chks.size())
+        {
+            for(const auto& iter : chks)
+                arr[iter.first][iter.second] = 0;
+        }
+        else
+        {
+            float avg = tempsum / (float)tempcnt;
+            while(tempq.size())
+            {
+                auto cur = tempq.front();
+                tempq.pop();
+                arr[cur.first][cur.second] += (arr[cur.first][cur.second] == avg ? 0 : arr[cur.first][cur.second] > avg ? -1 : 1);
+            }
+        }
+        /*
+        for(int i = 0; i < N; i++)
+        {
+           for(int j = 0; j < M; j++)
+           {
+               std::cout << arr[i][j] << ' ';
+           }
+           std::cout << '\n';
+        }
+        std::cout << '\n';
+        */
     }
-    int sum = 0;
-    std::vector<std::pair<int, int>> nums;
-    bool chk = false;
-    for(int i = 1; i <= N; i++)
-    {
-      for(int j = 0; j < M; j++)
-      {
-        if(!arr[i][j])
-          continue;
-        q.push({arr[i][j], {i, j}});
-        sum += arr[i][j];
-        nums.push_back({i, j});
-      }
-    }
-    while(q.size())
-    {
-      int value = q.front().first;
-      int y = q.front().second.first;
-      int x = q.front().second.second;
-      q.pop();
-      for(int dir = 0; dir < 4; dir++)
-      {
-        int ny = y + dy[dir];
-        int nx = x + dx[dir];
-        if(nx < 0)
-          nx = M-1;
-        if(nx >= M)
-          nx = 0;
-        if(ny <= 0 || ny > N || visited[ny][nx] || arr[ny][nx] != value)
-          continue;
-        chk = true;
-        visited[ny][nx] = true;
-        q.push({value, {ny, nx}});
-        arr[ny][nx] = arr[y][x] = 0;
-      }
-    }
-    if(!chk)
-    {
-      for(const auto& iter : nums)
-      {
-        if(arr[iter.first][iter.second] && arr[iter.first][iter.second] > sum / (float)nums.size())
-          arr[iter.first][iter.second] -= 1;
-        else if(arr[iter.first][iter.second] && arr[iter.first][iter.second] < sum / (float)nums.size())
-          arr[iter.first][iter.second] += 1;
-      }
-    }
-    std::memset(visited, false, sizeof(visited));
-  }
-  for(int i = 1; i <= N; i++)
-    for(int j = 0; j < M; j++)
-      res += arr[i][j];
-  std::cout << res;
-  return 0;
+    for(int i = 0; i < N; i++)
+        for(int j = 0; j < M; j++)
+            res += arr[i][j];
+    std::cout << res;
+	return 0;
 }
