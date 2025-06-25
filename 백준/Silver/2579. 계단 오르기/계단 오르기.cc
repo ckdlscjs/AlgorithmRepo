28ab4a16e8b77a08f1e716a9ef4f7472978f1cf4
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2025-06-25 21:58:27 +0900

    [Silver III] Title: 계단 오르기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.cc
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.cc
@@ -1,23 +1,16 @@
 #include <bits/stdc++.h>
-int dp[305];
-int n;
-int arr[305];
-int main()
+int N, arr[305], dp[305];
+int main() 
 {
-    std::ios::sync_with_stdio(false);
-    std::cin.tie(0);
-    std::cout.tie(0);
-    std::cin >> n;
-    for(int i = 1; i <= n; i++)
-      std::cin >> arr[i];
-    if(n >= 1)
-      dp[1] = arr[1];
-    if(n >= 2)
-      dp[2] = arr[1] + arr[2];
-    for(int i = 3; i <= n; i++)
-    {
-      dp[i] = std::max(dp[i-2], dp[i-3] + arr[i-1]) + arr[i];
-    }
-    std::cout << dp[n];
-    return 0;
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(0);
+  std::cout.tie(0);
+  std::cin >> N;
+  for(int n = 1; n <= N; n++) std::cin >> arr[n];
+  dp[1] = arr[1];
+  dp[2] = dp[1] + arr[2];
+  for(int i = 3; i <= N; i++)
+    dp[i] = std::max(arr[i] + arr[i-1] + dp[i-3], arr[i] + dp[i-2]);
+  std::cout << dp[N];
+  return 0;
 }
