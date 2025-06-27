e530c5f2de187ec5d162d700a4a748c900bf13c1
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2025-06-27 19:39:05 +0900

    [Silver I] Title: 쉬운 계단 수, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.cc
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.cc
@@ -1,27 +1,26 @@
 #include <bits/stdc++.h>
-using namespace std;
-long long int N, dp[105][10];
-const long long int mod = 1e9;
+const int mod = 1'000'000'000;
+int N;
+long long int dp[105][10];
 int main() 
 {
   std::ios::sync_with_stdio(false);
   std::cin.tie(0);
   std::cout.tie(0);
-  for(int j = 1; j <= 9; j++)
-    dp[1][j] = 1;
   std::cin >> N;
-  for(int i = 2; i <= N; i++)
+  for(int i = 1; i <= 9; i++) dp[1][i] = 1;
+  for(int n = 2; n <= N; n++)
   {
-    dp[i][0] += dp[i-1][1] % mod;
-    for(int j = 1; j < 9; j++)
-    {
-      dp[i][j] += (dp[i-1][j-1] + dp[i-1][j+1]) % mod;
-    }
-    dp[i][9] += dp[i-1][8] % mod;
+    dp[n][0] = dp[n-1][1];
+    for(int i = 1; i <= 8; i++) dp[n][i] = (dp[n-1][i-1] + dp[n-1][i+1]) % mod;
+    dp[n][9] = dp[n-1][8];
   }
-  long long int ret = 0;
+  long long int res = 0;
   for(int i = 0; i <= 9; i++)
-    ret += dp[N][i] % mod, ret %= mod;
-  std::cout << ret;
+  {
+    res += dp[N][i];
+    res %= mod;
+  }
+  std::cout << res;
   return 0;
 }
