d8addf35f93d3cf8e660df79ca2bc7f687e2e30d
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2025-07-03 07:00:17 +0900

    [Gold V] Title: 회장뽑기, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Gold/2660. 회장뽑기/회장뽑기.cc
+++ 백준/Gold/2660. 회장뽑기/회장뽑기.cc
@@ -1,47 +1,48 @@
+/*
+1.접근방식:
+
+2.시간복잡도:
+
+*/
 #include <bits/stdc++.h>
-using namespace std;
-int N, a, b;
+int N, A, B;
 std::vector<int> graph[52];
-std::vector<int> dists[52];
-int main() 
+std::map<int, std::vector<int>> results;
+int main()
 {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    std::cin >> N;
-    while(std::cin >> a >> b)
+	std::ios::sync_with_stdio(false);
+	std::cin.tie(NULL);
+	std::cin >> N;
+    while(std::cin >> A >> B)
     {
-        if(a == -1 && b == -1) break;
-        graph[a].push_back(b);
-        graph[b].push_back(a);
+        if(A == -1 && B == -1) break;
+        graph[A].push_back(B);
+        graph[B].push_back(A);
     }
     for(int i = 1; i <= N; i++)
     {
-        int maxdist = 1;
+        int maxdist = 0;
         std::vector<int> dist(N+1, 0);
+        dist[i] = 1;
         std::queue<int> q;
         q.push(i);
-        dist[i] = 1;
         while(q.size())
         {
-            int cur = q.front();
+            auto cur = q.front();
             q.pop();
+            maxdist = std::max(maxdist, dist[cur]-1);
             for(const auto& iter : graph[cur])
             {
                 if(dist[iter]) continue;
                 q.push(iter);
                 dist[iter] = dist[cur] + 1;
-                maxdist = std::max(maxdist, dist[iter]);
             }
         }
-        dists[maxdist-1].push_back(i);
+        results[maxdist].push_back(i);
     }
-    for(int i = 1; i <= 52; i++)
-    {
-        if(dists[i].empty()) continue;
-        std::cout << i << ' ' << dists[i].size() << '\n';
-        for(const auto& iter : dists[i])
-            std::cout << iter << ' ';
-        return 0;
-    }
-    return 0;
+    std::cout << results.begin()->first << ' ' << results.begin()->second.size() << '\n';
+    for(const auto& iter : results.begin()->second)
+        std::cout << iter << ' ';
+	return 0;
 }
+
