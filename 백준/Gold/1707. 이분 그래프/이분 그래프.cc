287a4b90a1dc914128f92aebfc7e1fa85f4c5490
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2025-07-03 16:05:02 +0900

    [Gold IV] Title: 이분 그래프, Time: 244 ms, Memory: 6624 KB -BaekjoonHub

--- 백준/Gold/1707. 이분 그래프/이분 그래프.cc
+++ 백준/Gold/1707. 이분 그래프/이분 그래프.cc
@@ -1,43 +1,52 @@
+/*
+1.접근방식:
+
+2.시간복잡도:
+
+*/
 #include <bits/stdc++.h>
-int K, V, E, u, v;
-bool DFS(std::unordered_map<int, std::vector<int>>& graph, int cur, std::vector<int>& colors, int color)
+int K;
+bool DFS(const std::vector<std::vector<int>>& graph, std::vector<int>& color, int cur, int c)
 {
-  bool ret = true;
-  colors[cur] = color;
-  for(const auto& iter : graph[cur])
-  {
-    if(colors[iter] && colors[iter] == color) return false;
-    if(colors[iter] == 0) 
-      if(!DFS(graph, iter, colors, color*-1)) return false;
-  }
-  return ret;
+    bool ret = true;
+    color[cur] = c;
+    c *= -1;
+    for(const auto& iter : graph[cur])
+    {
+        if(color[iter] == c) continue;
+        else if(color[iter] == color[cur]) return false;
+        ret &= DFS(graph, color, iter, c);
+    }
+    return ret;
 }
-int main(void)
+int main()
 {
-  std::ios::sync_with_stdio(false);
-  std::cin.tie(0);
-  std::cout.tie(0);
-  std::cin >> K;
-  for(int k = 0; k < K; k++)
-  {
-    std::cin >> V >> E;
-    std::unordered_map<int, std::vector<int>> graph;
-    std::vector<int> colors(V+1, 0);
-    for(int i = 0; i < E; i++)
+	std::ios::sync_with_stdio(false);
+	std::cin.tie(NULL);
+    std::cin >> K;
+    for(int k = 0; k < K; k++)
     {
-      std::cin >> u >> v;
-      graph[u].push_back(v);
-      graph[v].push_back(u);
+        int V, E;
+        std::cin >> V >> E;
+        std::vector<std::vector<int>> graph(V+1);
+        std::vector<int> color(V+1, 0);
+        for(int e = 0; e < E; e++)
+        {
+            int u, v;
+            std::cin >> u >> v;
+            graph[u].push_back(v);
+            graph[v].push_back(u);
+        }
+        bool ret = true;
+        for(int i = 1; i <= V; i++)
+        {
+            if(color[i] != 0) continue;
+            ret &= DFS(graph, color, i, 1);
+        }
+        if(ret) std::cout << "YES";
+        else std::cout << "NO";
+        std::cout << '\n';
     }
-    bool chk = true;
-    for(int i = 1; i <= V; i++)
-    {
-      if(colors[i]) continue;
-      chk = DFS(graph, i, colors, 1);
-      if(!chk)
-        break;
-    }
-    std::cout << (chk ? "YES" : "NO") << '\n';
-  }
-  return 0;
+	return 0;
 }
+
