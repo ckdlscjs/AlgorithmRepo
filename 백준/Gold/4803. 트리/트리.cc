4e723f6c14920f8acd2c3a6e120a4eb0a92d70b4
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2025-07-04 09:30:09 +0900

    [Gold IV] Title: 트리, Time: 28 ms, Memory: 3068 KB -BaekjoonHub

--- 백준/Gold/4803. 트리/트리.cc
+++ 백준/Gold/4803. 트리/트리.cc
@@ -1,50 +1,51 @@
+/*
+1.접근방식:
+
+2.시간복잡도:
+
+*/
 #include <bits/stdc++.h>
-int N, M, a, b, tc = 1;
-bool DFS(std::vector<bool>& visited, std::vector<int>* graph, int cur, int prev)
-{
-  visited[cur] = true;
-  for(const auto& iter : graph[cur])
-  {
-    if(iter == prev) continue;
-    if(visited[iter]) return false;
-    if(!DFS(visited, graph, iter, cur))
-      return false;
-  }
-  return true;
-}
+int N, M, cnt;
 int main()
 {
-  std::ios::sync_with_stdio(false);
-  std::cin.tie(0);
-  std::cout.tie(0);
-  while(1)
-  {
-    std::cin >> N >> M;
-    if(N == 0 && M == 0)
-      return 0;
-    std::vector<int> graph[505];
-    std::vector<bool> visited(N+1, false);
-    int cnt = 0;
-    for(int m = 0; m < M; m++)
-    {
-      std::cin >> a >> b;
-      graph[a].push_back(b);
-      graph[b].push_back(a);
-    }
-    for(int i = 1; i <= N; i++)
-    {
-      if(visited[i]) continue;
-      if(!DFS(visited, graph, i, -1))
-        continue;
-      cnt++;
-    }
-    std::cout << "Case " <<tc++<<": ";
-    if(cnt > 1)
-      std::cout << "A forest of " << cnt << " trees.\n";
-    else if(cnt == 1)
-      std::cout << "There is one tree.\n";
-    else
-      std::cout << "No trees.\n";
-  }
-  return 0;
+	std::ios::sync_with_stdio(false);
+	std::cin.tie(NULL);
+	while(std::cin >> N >> M)
+	{
+	    cnt++;
+	    if(N == 0 && M == 0) break;
+	    std::vector<std::vector<int>> graph(N+1);
+	    std::vector<bool> visited(N+1, false);
+	    for(int m = 0; m < M; m++)
+	    {
+	        int A, B;
+	        std::cin >> A >> B;
+	        graph[A].push_back(B);
+	        graph[B].push_back(A);
+	    }
+	    std::function<bool(int, int)> DFS = [&](int prv, int cur)->bool{
+	        visited[cur] = true;
+	        for(const auto& iter : graph[cur])
+	        {
+	            if(prv == iter) continue;
+	            if(visited[iter]) return false;
+	            if(!DFS(cur, iter)) return false;
+	        }
+	        return visited[cur];
+	    };
+	    int res = 0;
+	    
+	    for(int i = 1; i <= N; i++)
+	    {
+	        if(visited[i]) continue;
+	        if(DFS(-1, i)) res++;
+	    }
+	    
+	    std::cout << "Case " <<cnt<<": ";
+	    if(res > 1) std::cout <<"A forest of "<<res<<" trees."<<'\n';
+	    else if(res == 1) std::cout <<"There is one tree." << '\n';
+	    else std::cout <<"No trees." << '\n';
+	}
+	return 0;
 }
+
