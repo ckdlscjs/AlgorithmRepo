a06b9301b5fb34dd7a2a659a9eb7e1727d127258
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2025-07-05 02:59:10 +0900

    [Gold V] Title: 노드사이의 거리, Time: 0 ms, Memory: 6124 KB -BaekjoonHub

--- 백준/Gold/1240. 노드사이의 거리/노드사이의 거리.cc
+++ 백준/Gold/1240. 노드사이의 거리/노드사이의 거리.cc
@@ -1,36 +1,64 @@
+/*
+1.접근방식:
+
+2.시간복잡도:
+
+*/
 #include <bits/stdc++.h>
-int N, M, u, v, w;
-std::vector<std::pair<int, int>> graph[1005];
-int DFS(std::vector<bool>& visited, const int& target, int cur, int dist)
+int N, M, cost[1'005][1'005], depth[1'005];
+std::vector<int> graph[1'005];
+void DFS(int d, int cur)
 {
-  if(cur == target)
-    return dist;
-  visited[cur] = true;
-  for(const auto& iter : graph[cur])
-  {
-    if(visited[iter.first]) continue;
-    int temp = DFS(visited, target, iter.first, dist + iter.second);
-    if(temp) return temp;
-  }
-  return 0;
+    depth[cur] = d;
+    for(const auto& iter : graph[cur])
+    {
+        if(depth[iter]) continue;
+        DFS(d+1, iter);
+    }
 }
 int main()
 {
-  std::ios::sync_with_stdio(false);
-  std::cin.tie(0);
-  std::cout.tie(0);
-  std::cin >> N >> M;
-  for(int n = 0; n < N-1; n++)
-  {
-    std::cin >> u >> v >> w;
-    graph[u].push_back({v, w});
-    graph[v].push_back({u, w});
-  }
-  for(int m = 0; m < M; m++)
-  {
-    std::cin >> u >> v;
-    std::vector<bool> visited(N+1, false);
-    std::cout << DFS(visited, v, u, 0) << '\n';
-  }
-  return 0;
+	std::ios::sync_with_stdio(false);
+	std::cin.tie(NULL);
+    std::cin >> N >> M;
+    for(int n = 0; n < N-1; n++)
+    {
+        int u, v, w;
+        std::cin >> u >> v >> w;
+        cost[u][v] = cost[v][u] = w;
+        graph[u].push_back(v);
+        graph[v].push_back(u);
+    }
+    DFS(1, 1);
+    for(int m = 0; m < M; m++)
+    {
+        int u, v, dist = 0;
+        std::cin >> u >> v;
+        while(u != v)
+        {
+            if(depth[u] > depth[v])
+            {
+                for(const auto& iter : graph[u])
+                {
+                    if(depth[iter] > depth[u]) continue;
+                    dist += cost[iter][u];
+                    u = iter;
+                    break;
+                }
+            }
+            else
+            {
+                for(const auto& iter : graph[v])
+                {
+                    if(depth[iter] > depth[v]) continue;
+                    dist += cost[iter][v];
+                    v = iter;
+                    break;
+                }
+            }
+        }
+        std::cout << dist << '\n';
+    }
+	return 0;
 }
+
