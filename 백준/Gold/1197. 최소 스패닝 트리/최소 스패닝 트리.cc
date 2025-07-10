8c04448ca3eb46fbf813ee0b79b2db98b3cbb2b4
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2025-07-11 05:17:29 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 40 ms, Memory: 4496 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.cc
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.cc
@@ -1,40 +1,50 @@
+/*
+1.접근방식:
+
+2.시간복잡도:
+
+*/
 #include <bits/stdc++.h>
-int v, e;
-int main() 
+int V, E, res;
+std::vector<std::tuple<int, int, int>> edges;
+std::vector<int> parents;
+int Find(int node)
 {
-  std::ios::sync_with_stdio(false);
-  std::cin.tie(0); 
-  std::cout.tie(0);
-  std::cin >> v >> e;
-  std::vector<std::pair<int, long long int>> graph[10005];
-  for(int i = 0; i < e; i++)
-  {
-    int a, b, w;
-    std::cin >> a >> b >> w;
-    graph[a].push_back({b, w});
-    graph[b].push_back({a, w});
-  }
-  std::priority_queue<std::pair<int, int>, std::vector<std::pair<int, int>>, std::greater<std::pair<int, int>>> pq;
-  bool visited[10005];
-  std::memset(visited, false, sizeof(visited));
-  pq.push({0, 1});
-  int weights = 0;
-  while(pq.size())
-  {
-    int w = pq.top().first;
-    int cur = pq.top().second;
-    pq.pop();
-    if(visited[cur])
-      continue;
-    visited[cur] = true;
-    weights += w;
-    for(const auto& iter : graph[cur])
-    {
-      if(visited[iter.first])
-        continue;
-      pq.push({iter.second, iter.first});
-    }
-  }
-  std::cout << weights;
-  return 0;
+    if(parents[node] == node) return node;
+    if(parents[node] == -1) return parents[node] = node;
+    return parents[node] = Find(parents[node]);
 }
+void Union(int node1, int node2)
+{
+    int root1 = Find(node1);
+    int root2 = Find(node2);
+    if(root1 == root2) return;
+    if(root1 > root2) parents[root1] = root2;
+    else parents[root2] = root1;
+}
+int main()
+{
+	std::ios::sync_with_stdio(false);
+	std::cin.tie(NULL);
+    std::cin >> V >> E;
+    parents.resize(V+1, -1);
+    for(int e = 0; e < E; e++)
+    {
+        int u, v, w;
+        std::cin >> u >> v >> w;
+        edges.push_back({w, u, v});
+    }
+    std::sort(edges.begin(), edges.end());
+    for(const auto& iter : edges)
+    {
+        int w = std::get<0>(iter);
+        int u = std::get<1>(iter);
+        int v = std::get<2>(iter);
+        if(Find(u) == Find(v)) continue;
+        Union(u, v);
+        res += w;
+    }
+    std::cout << res;
+	return 0;
+}
+
