65299b76d42183fdc9b5e66be2d49676511486dc
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2025-07-14 03:46:39 +0900

    [Gold IV] Title: 거짓말, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/1043. 거짓말/거짓말.cc
+++ 백준/Gold/1043. 거짓말/거짓말.cc
@@ -1,71 +1,71 @@
+/*
+1.접근방식:
+
+2.시간복잡도:
+
+*/
 #include <bits/stdc++.h>
-using namespace std;
-int N, M, T, val, res, adj[52][52];
+std::vector<int> sets;
+std::vector<int> ranks;
+int Find(int node)
+{
+    if(sets[node] == node) return node;
+    if(sets[node] == -1) return sets[node] = node;
+    return sets[node] = Find(sets[node]);
+}
+void Union(int node1, int node2)
+{
+    int root1 = Find(node1);
+    int root2 = Find(node2);
+    if(root1 == root2) return;
+    if(ranks[root1] == ranks[root2]) 
+        ranks[root1]++;
+    if(ranks[root1] > ranks[root2])
+    {
+        sets[root2] = root1;
+    }
+    else
+    {
+        sets[root1] = root2;
+    }
+}
+int N, M, T, res;
 std::vector<std::vector<int>> partys;
-struct DisjointSet
+int main()
 {
-    std::vector<int> ranks;
-    std::vector<int> parents;
-    DisjointSet(int _size) : parents(_size + 1, -1), ranks(_size+1, 0) {}
-    int Find(int node)
-    {
-        if(parents[node] == -1)
-            return node;
-        return parents[node] = Find(parents[node]);
-    }
-    void Union(int node1, int node2)
-    {
-        int root1 = Find(node1);
-        int root2 = Find(node2);
-        if(root1 == root2) return;
-        if(ranks[root1] == ranks[root2])
-        {
-            ranks[root1]++;
-            parents[root2] = root1;
-        }
-        else if(ranks[root1] > ranks[root2])
-        {
-            parents[root2] = root1;
-        }
-        else
-        {
-            parents[root1] = root2;
-        }
-    }
-};
-int main() 
-{
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
+	std::ios::sync_with_stdio(false);
+	std::cin.tie(NULL);
     std::cin >> N >> M;
-    DisjointSet ds(N);
-    partys.resize(M);
-    ds.ranks[0] = 55;
+    sets.resize(N+1, -1);
+    ranks.resize(N+1, 0);
     std::cin >> T;
-    for(int t = 0; t < T; t++)
+    for(T; T > 0; T--)
     {
-        std::cin >> val;
-        ds.Union(0, val);
+        int t;
+        std::cin >> t;
+        Union(0, t);
     }
     for(int m = 0; m < M; m++)
     {
         std::cin >> T;
+        std::vector<int> ts(T);
         for(int t = 0; t < T; t++)
         {
-            std::cin >> val;
-            partys[m].push_back(val);
-            if(t >= 1)
-                ds.Union(partys[m][t-1], partys[m][t]);
+            std::cin >> ts[t];
+            if(t >= 1) 
+                Union(ts[t], ts[t-1]);
         }
+        partys.push_back(ts);
     }
     for(int m = 0; m < M; m++)
     {
-        bool liar = false;
+        bool chk = true;
         for(const auto& iter : partys[m])
-            liar |= ds.Find(0) == ds.Find(iter);
-        if(!liar) res++;
+            if(Find(0) == Find(iter)) 
+                chk = false;
+        if(chk) res++;
     }
-
     std::cout << res;
-    return 0;
+	return 0;
 }
+
