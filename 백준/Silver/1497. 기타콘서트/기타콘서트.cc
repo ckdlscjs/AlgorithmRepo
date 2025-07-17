3e65af41cd244f0cc114258511d273111ab73453
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2025-07-18 04:41:46 +0900

    [Silver I] Title: 기타콘서트, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1497. 기타콘서트/기타콘서트.cc
+++ 백준/Silver/1497. 기타콘서트/기타콘서트.cc
@@ -1,49 +1,42 @@
+/*
+1.접근방식:
+
+2.시간복잡도:
+
+*/
+
 #include <bits/stdc++.h>
-using namespace std;
-long long int N, M;
-int res;
-std::string str;
-std::vector<long long int> masks;
-int main() 
+int N, M, mincnt = 11;
+long long arr[12], maxmask;
+int main()
 {
-  std::ios::sync_with_stdio(false);
-  std::cin.tie(0);
-  std::cout.tie(0);
-  std::cin >> N >> M;
-  res = N;
-  for(int n = 0; n < N; n++)
-  {
-    std::cin >> str >> str;
-    long long int mask = 0;
-    for(int i = 0; i < str.size(); i++)
+	std::ios::sync_with_stdio(false);
+	std::cin.tie(NULL);
+    std::cin >> N >> M;
+    for(int n = 0; n < N; n++)
     {
-      long long int pos = str.size()-1-i;
-      if(str[pos] == 'Y')
-        mask |= (1L << pos);
+        std::string str;
+        std::cin >> str >> str;
+        for(int i = 0; i < str.size(); i++)
+            arr[n] |= (str[i] == 'Y' ? (1LL << i) : 0);
+        maxmask |= arr[n];
     }
-    masks.push_back(mask);
-  }
-  for(int i = 0; i < (1L << N); i++)
-  {
-    long long int test = 0;
-    int cur = 0;
-    for(int j = 0; j < N; j++)
+    for(int mask = 1; mask < (1LL<<N); mask++)
     {
-      if(i & (1L << j))
-      {
-        cur++;
-        test |= masks[j];
-      }
+        int curcnt = 0;
+        long long curmask = 0;
+        for(int idx = 0; idx < N; idx++)
+        {
+            if(mask & (1LL << idx))
+            {
+                curcnt++;
+                curmask |= arr[idx];
+            }
+        }
+        if(maxmask != 0 && curmask == maxmask)
+            mincnt = std::min(curcnt, mincnt);
     }
-    int cnt = 0;
-    for(int j = 0; j < N; j++)
-    {
-      if((test & masks[j]) == masks[j])
-        cnt++;
-    }
-    if(cnt >= N)
-      res = std::min(res, cur);
-  }
-  std::cout << (res <= 0 ? -1 : res);
-  return 0;
+    std::cout << (mincnt > 10 ? -1 : mincnt);
+	return 0;
 }
+
