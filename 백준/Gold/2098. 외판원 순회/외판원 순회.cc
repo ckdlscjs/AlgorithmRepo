16de0dd5e6e463921abc7651e61cb4d1e7dd9daf
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2025-08-04 17:38:28 +0900

    [Gold I] Title: 외판원 순회, Time: 28 ms, Memory: 6116 KB -BaekjoonHub

--- 백준/Gold/2098. 외판원 순회/외판원 순회.cc
+++ 백준/Gold/2098. 외판원 순회/외판원 순회.cc
@@ -1,31 +1,39 @@
+/*
+1.접근방식:
+
+2.시간복잡도:
+
+*/
 #include <bits/stdc++.h>
 const int INF = 987654321;
-int N, W[17][17], costs[17][1<<17];
+int N, costs[16][16], dp[16][1<<16];
 int Check(int cur, int mask)
 {
-  if(mask == (1 << N) - 1)
-    return W[cur][0] ? W[cur][0] : INF;
-  int& ret = costs[cur][mask];
-  if(ret != -1) return ret;
-  ret = INF;
-  for(int i = 0; i < N; i++)
-  {
-    if(mask & (1 << i)) continue;
-    if(!W[cur][i]) continue;
-    ret = std::min(ret, Check(i, mask | 1 << i) + W[cur][i]);
-  }
-  return ret;
+    if(mask == (1 << N) - 1)
+        return costs[cur][0] == 0 ? INF : costs[cur][0];
+    int& ret = dp[cur][mask];
+    if(ret != -1) return ret;
+    ret = INF;
+    for(int i = 0; i < N; i++)
+    {
+        if(cur == i) continue;
+        if(!costs[cur][i]) continue;
+        if(mask & 1 << i) continue;
+        ret = std::min(ret, Check(i, mask | 1 << i) + costs[cur][i]);
+    }
+    return ret;
 }
 int main()
 {
-  std::ios::sync_with_stdio(false);
-  std::cin.tie(0);
-  std::cout.tie(0);
-  std::cin >> N;
-  for(int i = 0; i < N; i++)
-    for(int j = 0; j < N; j++)
-      std::cin >> W[i][j];
-  std::fill_n(&costs[0][0], 17 * 1 << 17, -1);
-  std::cout << Check(0, 1);
-  return 0;
+	std::ios::sync_with_stdio(false);
+	std::cin.tie(NULL);
+    std::memset(dp, -1, sizeof(dp));
+    std::cin >> N;
+    for(int i = 0; i < N; i++)
+        for(int j = 0; j < N; j++)
+            std::cin >> costs[i][j];
+    std::cout << Check(0, 1 << 0);
+    
+	return 0;
 }
+
