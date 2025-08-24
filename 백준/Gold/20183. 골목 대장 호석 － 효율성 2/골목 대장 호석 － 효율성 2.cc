c0fd2a55942666d2ddc11067836de4cb2bad40c1
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2025-08-25 03:38:18 +0900

    [Gold II] Title: 골목 대장 호석 - 효율성 2, Time: 1064 ms, Memory: 36004 KB, Score: 43 point -BaekjoonHub

--- 백준/Gold/20183. 골목 대장 호석 － 효율성 2/골목 대장 호석 － 효율성 2.cc
+++ 백준/Gold/20183. 골목 대장 호석 － 효율성 2/골목 대장 호석 － 효율성 2.cc
@@ -1,48 +1,54 @@
+/*
+1.접근방식:
+
+2.시간복잡도:
+
+*/
 #include <bits/stdc++.h>
-#define ll long long int
-const ll INF = 1e14+5;
-ll N, M, A, B, C, u, v, w, maxW, costs[100'005];
-std::vector<std::pair<ll, ll>> graph[100'005];
+#define pll std::pair<long long, long long>
+const long long INF = (long long)1e14 + 100LL;
+int N, M, A, B;
+long long costs[100'005], C, lo, hi = (long long)1e9 + 1LL;
+std::vector<pll> graph[100'005];
+
 int main()
 {
-  std::ios::sync_with_stdio(false);
-  std::cin.tie(0);
-  std::cout.tie(0);
-  std::cin >> N >> M >> A >> B >> C;
-  for(int i = 0; i < M; i++)
-  {
-    std::cin >> u >> v >> w;
-    graph[u].push_back({w, v});
-    graph[v].push_back({w, u});
-    maxW = std::max(maxW, w);
-  }
-  ll lo = -1, hi = maxW+1;
-  while(lo + 1 < hi)
-  {
-    ll mid = (lo + hi) / 2;
-    std::fill_n(costs, 100'005, INF);
-    costs[A] = 0;
-    std::priority_queue<std::pair<ll, ll>, std::vector<std::pair<ll, ll>>, std::greater<std::pair<ll, ll>>> pq;
-    pq.push({0, A});
-    while(pq.size())
+	std::ios::sync_with_stdio(false);
+	std::cin.tie(NULL);
+    std::cin >> N >> M >> A >> B >> C;
+    for(int m = 0; m < M; m++) 
     {
-      ll u = pq.top().second;
-      ll c = pq.top().first;
-      pq.pop();
-      if(u == B) break;
-      if(costs[u] < c) continue;
-      for(const auto& iter : graph[u])
-      {
-        if(iter.first <= mid && costs[u] + iter.first < costs[iter.second] && c + iter.first <= C)
-        {
-          costs[iter.second] = costs[u] + iter.first;
-          pq.push({costs[iter.second], iter.second});
-        }
-      }
+        long long u, v, w;
+        std::cin >> u >> v >> w;
+        graph[u].push_back({v, w});
+        graph[v].push_back({u, w});
     }
-    if(costs[B] <= C) hi = mid;
-    else lo = mid;
-  }
-  std::cout << (hi > maxW ? -1 : hi);
-  return 0;
+    while(lo + 1 < hi)
+    {
+        long long mid = (lo + hi) / 2;
+        std::priority_queue<pll, std::vector<pll>, std::greater<pll>> pq;
+        std::fill_n(costs, 100'005, INF);
+        costs[A] = 0;
+        pq.push({0, A});
+        while(pq.size())
+        {
+            auto cur = pq.top();
+            pq.pop();
+            if(cur.second == B) break;
+            if(cur.first > costs[cur.second]) continue;
+            for(const auto& iter : graph[cur.second])
+            {
+                if(iter.second > mid) continue;
+                if(costs[cur.second] + iter.second >= costs[iter.first] || costs[cur.second] + iter.second > C) continue;
+                costs[iter.first] = costs[cur.second] + iter.second;
+                pq.push({costs[iter.first], iter.first});
+            }
+        }
+        if(costs[B] > C) lo = mid;
+        else hi = mid;
+    }
+    if(hi >= (long long)1e9 + 1LL) std::cout << -1;
+    else std::cout << hi;
+	return 0;
 }
+
