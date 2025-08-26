d0b26f8bb150b14cc0f09d76fb0e2b285666a9b6
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2025-08-27 06:14:13 +0900

    [Gold I] Title: 횡단보도, Time: 420 ms, Memory: 51008 KB -BaekjoonHub

---
+++ 백준/Gold/24042. 횡단보도/횡단보도.cc
@@ -0,0 +1,56 @@
+/*
+1.접근방식:
+
+2.시간복잡도:
+
+*/
+#include <bits/stdc++.h>
+#define ll long long
+#define pll std::pair<ll, ll>
+ll N, M, costs[100'005];
+const ll INF = 1e18;
+std::vector<pll> graph[100'005];
+std::priority_queue<pll, std::vector<pll>, std::greater<pll>> pq;
+int main()
+{
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(0);
+    std::cout.tie(0);
+    std::fill_n(costs, 100'005, INF);
+    std::cin >> N >> M;
+    for(int m = 1; m <= M; m++)
+    {
+        int u, v;
+        std::cin >> u >> v;
+        graph[u].push_back({v, m});
+        graph[v].push_back({u, m});
+    }
+    costs[1] = 0;
+    pq.push({costs[1], 1});
+    while(pq.size())
+    {
+        auto cur = pq.top();
+        pq.pop();
+        ll u = cur.second;
+        ll cc = cur.first;
+        //std::cout << u << ' ' << cc << '\n';
+        if(cc > costs[u]) continue;
+        for(const auto& iter : graph[u])
+        {
+            ll v = iter.first;
+            ll w = iter.second;
+            if(cc > w)
+            {
+                ll cnt = cc/M;
+                if(cnt*M + w < cc)
+                    cnt++;
+                w = cnt * M + w;
+            }
+            if(w > costs[v]) continue;
+            costs[v] = w;
+            pq.push({costs[v], v});
+        }
+    }
+    std::cout << costs[N];
+    return 0;
+}
