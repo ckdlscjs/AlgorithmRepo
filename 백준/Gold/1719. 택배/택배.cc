93745a8b4057a2be6d39974b2244e39645a87e3e
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2025-09-19 20:37:46 +0900

    [Gold III] Title: 택배, Time: 16 ms, Memory: 2340 KB -BaekjoonHub

--- 백준/Gold/1719. 택배/택배.cc
+++ 백준/Gold/1719. 택배/택배.cc
@@ -1,57 +1,57 @@
+/*
+1.접근방식:
+
+2.시간복잡도:
+
+*/
 #include <bits/stdc++.h>
-const int INF = 987654321;
-int N, M, dists[205][205], nxt[205][205], u, v, w;
+const int INF = 203*203*1000;
+int N, M, costs[202][202], prv[202][202];
 int main()
 {
-  std::ios::sync_with_stdio(false);
-  std::cin.tie(0);
-  std::cout.tie(0);
-  std::cin >> N >> M;
-  std::fill_n(&dists[0][0], 205*205, INF);
-  std::fill_n(&nxt[0][0], 205*205, INF);
-  for(int i = 1; i <= N; i++)
-  {
-    dists[i][i] = 0;
-    nxt[i][i] = 0;
-  }
-  for(int m = 0; m < M; m++)
-  {
-    std::cin >> u >> v >> w;
-    if(w < dists[u][v])
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(0);
+    std::cout.tie(0);
+    std::cin >> N >> M;
+    std::fill_n(&costs[0][0], 202*202, INF);
+    for(int i = 1; i <= N; i++) costs[i][i] = 0;
+    for(int m = 0; m < M; m++)
     {
-      dists[u][v] = w;
-      nxt[u][v] = v;
+        int u, v, c;
+        std::cin >> u >> v >> c;
+        if(c < costs[u][v])
+        {
+            costs[u][v] = c;
+            prv[u][v] = v;
+        }
+        if(c < costs[v][u])
+        {
+            costs[v][u] = c;
+            prv[v][u] = u;
+        }
     }
-    if(w < dists[v][u])
+    for(int k = 1; k <= N; k++)
     {
-      dists[v][u] = w;
-      nxt[v][u] = u;
+        for(int i = 1; i <= N; i++)
+        {
+           for(int j = 1; j <= N; j++)
+            {
+                if(costs[i][k] + costs[k][j] < costs[i][j])
+                {
+                    costs[i][j] = costs[i][k] + costs[k][j];
+                    prv[i][j] = prv[i][k];
+                }
+            }
+        }
     }
-  }
-  for(int k = 1; k <= N; k++)
-  {
     for(int i = 1; i <= N; i++)
     {
-      for(int j = 1; j <= N; j++)
-      {
-        if(dists[i][k] != INF && dists[k][j] != INF && dists[i][k] + dists[k][j] < dists[i][j])
+        for(int j = 1; j <= N; j++)
         {
-          dists[i][j] = dists[i][k] + dists[k][j];
-          nxt[i][j] = nxt[i][k];
+            if(i == j) std::cout << '-' << ' ';
+            else std::cout << prv[i][j] << ' ';
         }
-      }
+        std::cout << '\n';
     }
-  }
-  for(int i = 1; i <= N; i++)
-  {
-    for(int j = 1; j <= N; j++)
-    {
-      if(nxt[i][j])
-        std::cout << nxt[i][j] << ' ';
-      else
-        std::cout << '-' << ' ';
-    }
-    std::cout << '\n';
-  }
-  return 0;
+    return 0;
 }
