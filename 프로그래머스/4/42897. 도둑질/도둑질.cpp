2e1a44ec5a1a7333664aa054296bdc313d3b56be
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2025-09-29 06:32:22 +0900

    [level 4] Title: 도둑질, Time: 11.43 ms, Memory: 46.6 MB -BaekjoonHub

--- 프로그래머스/4/42897. 도둑질/도둑질.cpp
+++ 프로그래머스/4/42897. 도둑질/도둑질.cpp
@@ -1,24 +1,18 @@
 #include <bits/stdc++.h>
+
 using namespace std;
-int dp[1000005][2][2];
+int dp[2][1'000'002][2]; //0번째선택, i번째, 선택여부(0, 1)
 int solution(vector<int> money) 
 {
-    int n = money.size();
-    dp[0][0][0] = 0;
-    dp[0][0][0] = 0;
-    dp[0][1][0] = 0;
-    dp[0][1][1] = money[0];
-    
-    for(int i = 1; i < n; i++)
+    dp[0][0][1] = money[0];
+    dp[0][1][1] = std::max(money[1], money[0]);
+    dp[1][1][1] = money[1];
+    for(int i = 2; i < money.size(); i++)
     {
-        dp[i][0][0] = std::max(dp[i-1][0][0], dp[i-1][1][0]);
-        dp[i][0][1] = std::max(dp[i-1][0][1], dp[i-1][1][1]);
-        dp[i][1][0] = dp[i-1][0][0] + money[i];
-        if(i < n-1)
-            dp[i][1][1] = dp[i-1][0][1] + money[i];
-        else
-            dp[i][1][1] = std::max(dp[i-1][0][1], dp[i-1][1][1]);
+        dp[0][i][0] = std::max(dp[0][i-1][0], dp[0][i-1][1]);
+        dp[0][i][1] = std::max(dp[0][i-1][0], dp[0][i-2][1]) + money[i];
+        dp[1][i][0] = std::max(dp[1][i-1][0], dp[1][i-1][1]);
+        dp[1][i][1] = std::max(dp[1][i-1][0], dp[1][i-2][1]) + money[i];
     }
-    
-    return std::max({dp[n-1][0][0], dp[n-1][0][1], dp[n-1][1][0], dp[n-1][1][1]});
+    return std::max(dp[0][money.size()-1][0], dp[1][money.size()-1][1]);
 }
