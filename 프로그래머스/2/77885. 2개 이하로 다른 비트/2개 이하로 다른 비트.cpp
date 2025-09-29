d5e5cb4fc4c336be877c90477a58fd4df72f4c6e
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2025-09-30 08:10:25 +0900

    [level 2] Title: 2개 이하로 다른 비트, Time: 266.85 ms, Memory: 25.8 MB -BaekjoonHub

--- 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.cpp
+++ 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.cpp
@@ -1,13 +1,28 @@
 #include <bits/stdc++.h>
+#define ll long long
 using namespace std;
-vector<long long> solution(vector<long long> numbers) 
+
+vector<ll> solution(vector<long long> numbers) 
 {
-    vector<long long> answer;
+    vector<ll> answer;
     for(const auto& iter : numbers)
     {
-        long long int bit = 1;
-        while((bit & iter)) bit <<= 1;
-        answer.push_back(iter + bit - (bit>>1));
+        ll cur = (ll)1e15 + 1LL;
+        
+        for(int i = 63; i >= 0; i--)
+        {
+            ll temp = (1LL << i) ^ iter;
+            if(temp > iter)
+                cur = std::min(cur, temp);
+            for(int j = i-1; j >= 0; j--)
+            {
+                ll temp2 = (1LL << j) ^ temp;
+                if(temp2 > iter)
+                    cur = std::min(cur, temp2);
+            }
+        }
+        answer.push_back(cur);
     }
+    
     return answer;
 }
