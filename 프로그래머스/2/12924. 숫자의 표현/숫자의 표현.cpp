ef44bfc5bafe71b2ff9b25e44bca10c1d3fdd0af
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2025-10-02 16:40:25 +0900

    [level 2] Title: 숫자의 표현, Time: 0.03 ms, Memory: 3.8 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.cpp
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.cpp
@@ -1,28 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
-const int MaxN = 10005;
-int arr[MaxN];
+
 int solution(int n) 
 {
     int answer = 0;
-    int sum = 0;
-    for(int i = 1; i <= n; i++)
+    int l = 1, r = 1, sum = 1;
+    while(l <= r && r <= n)
     {
-        sum += i;
-        arr[i] = sum;
+        if(sum == n)
+        {
+            //std::cout << l << ' ' << r << '\n';
+            answer++;
+            r++;
+            sum += r;
+        }
+        else if(sum > n)
+        {
+            sum -= l;
+            l++;
+        }
+        else
+        {
+            r++;
+            sum += r;
+        }
     }
-    for(int i = n; i >= 1; i--)
-    {
-       for(int j = i-1; arr[i] - arr[j] <= n && j >= 0; j--)
-       {
-           if(arr[i] - arr[j] == n)
-           {
-               //std::cout << i << " : " << j << "->" << arr[i] - arr[j] <<'\n';
-               answer++;
-           }
-               
-       }
-    }
-    
     return answer;
 }
