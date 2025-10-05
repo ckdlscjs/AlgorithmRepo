c5c4553c6ec8e5d32050ab8c8ab7e9da37d9dfab
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2025-10-06 06:11:55 +0900

    [level 2] Title: 귤 고르기, Time: 24.24 ms, Memory: 12.3 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.cpp
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.cpp
@@ -1,13 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
-std::unordered_map<int, int> tanger;
-std::priority_queue<std::pair<int, int> ,std::vector<std::pair<int, int>>, std::less<std::pair<int, int>>> pq;
-int solution(int k, vector<int> tangerine) 
+std::unordered_map<int, int> cnts;
+std::priority_queue<std::pair<int, int>, std::vector<std::pair<int, int>>, std::less<std::pair<int, int>>> pq;
+int solution(int k, vector<int> arr) 
 {
-    for(const auto& iter : tangerine)
-        tanger[iter]++;
-    for(const auto& iter : tanger)
+    for(const auto& iter : arr)
+        cnts[iter]++;
+    for(const auto& iter : cnts)
+    {
+        //std::cout << iter.first << ' ' << iter.second << '\n';
         pq.push({iter.second, iter.first});
+    }
+        
     int answer = 0;
     while(k > 0)
     {
@@ -15,6 +19,5 @@ int solution(int k, vector<int> tangerine)
         pq.pop();
         answer++;
     }
-   
     return answer;
 }
