ad302fa34880070c63a76c2679f850d71916edb2
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2025-10-07 05:00:41 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.01 ms, Memory: 3.63 MB -BaekjoonHub

--- 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.cpp
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.cpp
@@ -1,20 +1,9 @@
-#include <bits/stdc++.h>
+#include <string>
+#include <vector>
+
 using namespace std;
-int gcd(int large, int small)
-{
-    if(large % small == 0) return small;
-    return gcd(small, large % small);
-}
-int solution(vector<int> arr) 
-{
-    std::sort(arr.begin(), arr.end());
-    int _lcm = 1;
-    for(const auto& iter : arr)
-    {
-        int large = std::max(iter, _lcm);
-        int small = std::min(iter, _lcm);
-        int _gcd = gcd(large, small);
-        _lcm = iter * _lcm / _gcd;
-    }
-    return _lcm;
+
+int solution(vector<int> arr) {
+    int answer = 0;
+    return answer;
 }
