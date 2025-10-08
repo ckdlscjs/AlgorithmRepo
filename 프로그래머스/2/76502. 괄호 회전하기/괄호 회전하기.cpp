c6fa98eb475a34d3994e53afd8eeb48a08c62ba0
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2025-10-08 21:38:09 +0900

    [level 2] Title: 괄호 회전하기, Time: 5.29 ms, Memory: 4.23 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.cpp
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.cpp
@@ -1,40 +1,32 @@
 #include <bits/stdc++.h>
-#include <unordered_map>
+
+
 using namespace std;
+
 int solution(string s) 
 {
-    std::unordered_map<char, char> oper;
-    oper['('] = ')';
-    oper['{'] = '}';
-    oper['['] = ']';
     int answer = 0;
     for(int i = 0; i < s.size()-1; i++)
     {
         std::stack<char> st;
-        int cnt = 0;
-        for(int j = 0 + i; j - i < s.size(); j++)
+        for(int j = 0; j < s.size(); j++)
         {
-            int idx = j % s.size();
-            if(oper.find(s[idx]) != oper.end())
-            {
-                st.push(s[idx]);
-            }
+            int idx = (j + i) % s.size();
+            if(s[idx] == '(' || s[idx] == '{' || s[idx] == '[') st.push(s[idx]);
             else
             {
                 if(st.empty())
-                    break;
-                if(oper[st.top()] == s[idx])
                 {
-                    cnt++;
-                    st.pop();
-                    continue;
+                    st.push(s[idx]);
+                    break;
                 }
-                break;
+                else if(st.top() == '(' && s[idx] != ')') break;
+                else if(st.top() == '{' && s[idx] != '}') break;
+                else if(st.top() == '[' && s[idx] != ']') break;
+                st.pop();
             }
         }
-        if(st.size() || cnt != s.size()/2)
-            continue;
-        answer++;
+        if(st.empty()) answer++;
     }
     return answer;
 }
