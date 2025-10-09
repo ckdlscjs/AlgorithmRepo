056b3d9804b05b216cd5a1a3f0d9d6c7c6d426ba
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2025-10-09 16:20:51 +0900

    [level 2] Title: 행렬의 곱셈, Time: 2.74 ms, Memory: 5.24 MB -BaekjoonHub

--- 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.cpp
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.cpp
@@ -4,16 +4,17 @@ using namespace std;
 
 vector<vector<int>> solution(vector<vector<int>> arr1, vector<vector<int>> arr2) 
 {
-    vector<vector<int>> answer(arr1.size(), std::vector<int> (arr2[0].size(), 0));
-    for(int i = 0; i < arr1.size(); i++)
+    vector<vector<int>> answer(arr1.size(), std::vector<int>(arr2[0].size(), 0));
+    for(int i = 0; i < answer.size(); i++)
     {
-        for(int j = 0; j < arr2[0].size(); j++)
+        for(int j = 0; j < answer[0].size(); j++)
         {
-            for(int idx = 0; idx < arr1[0].size(); idx++)
+            for(int k = 0; k < arr1[0].size(); k++)
             {
-                answer[i][j] += (arr1[i][idx] * arr2[idx][j]);
+                answer[i][j] += arr1[i][k] * arr2[k][j];
             }
         }
     }
+    
     return answer;
 }
