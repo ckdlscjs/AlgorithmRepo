8ffbff31b3e37be1e6565f26477d99e6c00a4ee4
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2025-10-12 20:54:23 +0900

    [level 2] Title: 피로도, Time: 16.45 ms, Memory: 3.66 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.cpp
+++ 프로그래머스/2/87946. 피로도/피로도.cpp
@@ -1,26 +1,9 @@
-#include <bits/stdc++.h>
+#include <string>
+#include <vector>
+
 using namespace std;
-int Check(const std::vector<std::vector<int>> dungeons, int k, int cnt, int mask)
-{
-    if(cnt >= dungeons.size())
-    {
-        return dungeons.size();
-    }
-    else
-    {
-        int ret = cnt;
-        for(int i = 0; i < dungeons.size(); i++)
-        {
-            if(mask & (1 << i)) continue;
-            int need = dungeons[i][0];
-            int used = dungeons[i][1];
-            if(need > k) continue;
-            ret = std::max(ret, Check(dungeons, k-used, cnt+1, mask | (1 << i)));
-        }
-        return ret;
-    }
-}
-int solution(int k, vector<vector<int>> dungeons) 
-{
-    return Check(dungeons, k, 0, 0);
+
+int solution(int k, vector<vector<int>> dungeons) {
+    int answer = -1;
+    return answer;
 }
