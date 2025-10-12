abdc01a879908a28787724cdbd07fe20e57b0185
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2025-10-12 23:51:43 +0900

    [level 2] Title: [1차] 캐시, Time: 21.64 ms, Memory: 12.4 MB -BaekjoonHub

--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.cpp
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.cpp
@@ -1,30 +1,30 @@
 #include <bits/stdc++.h>
+
 using namespace std;
-const int cacheHit = 1, cacheMiss = 5;
-std::unordered_map<string, std::list<string>::iterator> chks; 
-std::list<string> cache;
+std::list<std::string> cache;
+std::unordered_map<std::string, std::list<std::string>::iterator> chks; 
 int solution(int cacheSize, vector<string> cities) 
 {
-    if (cacheSize == 0) return cities.size() * cacheMiss;
+    if(cacheSize <= 0) return cities.size() * 5;
     int answer = 0;
-    for (auto& iter : cities) 
+    for(auto& iter : cities)
     {
         std::transform(iter.begin(), iter.end(), iter.begin(), ::tolower);
-        if(chks.find(iter) == chks.end())
+        if(chks.size() && chks.find(iter) != chks.end())
         {
-            answer += cacheMiss;
+            answer += 1;
+            cache.erase(chks[iter]);
         }
         else
         {
-            answer += cacheHit;
-            cache.erase(chks[iter]);
+            answer += 5;
         }
-        cache.push_back(iter);
-        chks[iter] = --cache.end();
+        cache.push_front(iter);
+        chks[iter] = cache.begin();
         if(cache.size() > cacheSize)
         {
-            chks.erase(cache.front());
-            cache.pop_front();
+            chks.erase(cache.back());
+            cache.pop_back();
         }
     }
     return answer;
