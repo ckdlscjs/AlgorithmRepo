76fe465f594e72de1af221a4f30762afff4947e8
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2025-10-14 04:30:18 +0900

    [level 2] Title: 튜플, Time: 9.68 ms, Memory: 5.11 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.cpp
+++ 프로그래머스/2/64065. 튜플/튜플.cpp
@@ -1,28 +1,27 @@
-//https://school.programmers.co.kr/learn/courses/30/lessons/64065
 #include <bits/stdc++.h>
 using namespace std;
-std::unordered_map<int, int> cnts;
-std::priority_queue<std::pair<int, int>, std::vector<std::pair<int, int>>, std::less<std::pair<int, int>>> pq;
+
 vector<int> solution(string s) 
 {
+    std::unordered_map<int, int> cnts;
     std::string num;
-    for(const auto& iter : s)
+    for(int i = 1; i < s.size()-1; i++)
     {
-        if(num.size() && !std::isdigit(iter))
+        if(s[i] == '{') continue;
+        else if(s[i] == ',' || s[i] == '}')
         {
+            if(num.empty()) continue;
             cnts[std::stoi(num)]++;
             num.clear();
         }
-        else if(std::isdigit(iter))
-        {
-            num += iter;
-        }
+        else
+            num += s[i];
     }
-    
-    for(const auto& iter : cnts)
-        pq.push({iter.second, iter.first});
     vector<int> answer;
-    while(pq.size())
-        answer.push_back(pq.top().second), pq.pop();
+    for(const auto& iter : cnts)
+        answer.push_back(iter.first);
+    std::sort(answer.begin(), answer.end(), [&](const int& a, const int& b)->bool{
+       return cnts[a] > cnts[b];
+    });
     return answer;
 }
