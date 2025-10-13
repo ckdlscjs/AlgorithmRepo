49f1525980cddd2fd74f4b8a9199ace6d6d91fed
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2025-10-14 05:12:58 +0900

    [level 2] Title: 전화번호 목록, Time: 318.26 ms, Memory: 242 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.cpp
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.cpp
@@ -1,23 +1,50 @@
 #include <bits/stdc++.h>
 using namespace std;
+struct Trie
+{
+    Trie* nodes[10];
+    bool finish = false;
+    Trie()
+    {
+        finish = false;
+        std::fill_n(nodes, 10, nullptr);
+    }
+    ~Trie()
+    {
+        for(int i = 0; i < 10; i++)
+            if(nodes[i]) 
+                delete nodes[i];
+    }
+    void Insert(const std::string& str, int idx)
+    {
+        if(idx >= str.size())
+        {
+            finish = true;
+            return;
+        }
+        int cur = str[idx] - '0';
+        if(nodes[cur] == nullptr) nodes[cur] = new Trie();
+        nodes[cur]->Insert(str, idx+1);
+    }
+    bool Find(const std::string& str, int idx)
+    {
+        if(idx >= str.size()) return true;
+        int cur = str[idx] - '0';
+        if(nodes[cur] == nullptr) return false;
+        return nodes[cur]->Find(str, idx+1);
+    }
+};
 bool solution(vector<string> phone_book) 
 {
-    std::sort(phone_book.begin(), phone_book.end(), [](const std::string& a, const std::string& b)
-              {
-                  return a.size() < b.size();
-              });
-    std::unordered_set<std::string> chks;
-    std::string temp = "";
+    Trie trie;
+    std::sort(phone_book.begin(), phone_book.end(), [](std::string& a, std::string& b)->bool{
+        return a.size() > b.size();
+    });
     for(const auto& iter : phone_book)
     {
-        for(int i = 0; i < iter.size(); i++)
-        {
-            temp += iter[i];
-            if(chks.find(temp) != chks.end())
-                return false;
-        }
-        chks.insert(iter);
-        temp.clear();
+        if(trie.Find(iter, 0)) return false;
+        trie.Insert(iter, 0);
     }
+    
     return true;
 }
