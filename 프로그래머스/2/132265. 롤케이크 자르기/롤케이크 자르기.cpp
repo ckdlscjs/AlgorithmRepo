f2389cd9f08897f757dd59bc935ccb25667ff40c
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2025-10-15 01:19:46 +0900

    [level 2] Title: 롤케이크 자르기, Time: 68.64 ms, Memory: 36.8 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.cpp
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.cpp
@@ -1,21 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
-std::map<int, int> lasts;
-std::map<int, int> cnts;
+std::unordered_map<int, int> l, r;
 int solution(vector<int> topping) 
 {
     int answer = 0;
     for(const auto& iter : topping)
-        lasts[iter]++;
-    int zero_cnt = 0;
-    for(int i = 0;i < topping.size()-1; i++)
+        r[iter]++;
+    for(const auto& iter : topping)
     {
-        cnts[topping[i]]++;
-        lasts[topping[i]]--;
-        if(lasts[topping[i]] <= 0)
-            zero_cnt++;
-        if(cnts.size() == lasts.size() - zero_cnt)
-            answer++;
+        l[iter]++;
+        r[iter]--;
+        if(r[iter] <= 0)
+            r.erase(iter);
+        if(l.size() == r.size()) answer++;
     }
     return answer;
 }
