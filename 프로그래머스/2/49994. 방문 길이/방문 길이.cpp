ce745fe12d9bcb9e6570462fe6e09734d2174044
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2025-10-16 06:19:54 +0900

    [level 2] Title: 방문 길이, Time: 0.16 ms, Memory: 4.16 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.cpp
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.cpp
@@ -1,27 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
-std::unordered_map<char, std::vector<int>> D;
-std::set<std::pair<std::pair<int, int>, std::pair<int, int>>> chks;
+const int dy[] = {-1, 1, 0, 0};
+const int dx[] = {0, 0, 1, -1};
+std::set<std::pair<std::pair<int, int>, std::pair<int, int>>> checks; 
+std::unordered_map<char, int> dir;
+int y, x, answer;
 int solution(string dirs) 
 {
-    D['U'] = {1, 0};
-    D['D'] = {-1, 0};
-    D['R'] = {0, 1};
-    D['L'] = {0, -1};
-    int answer = 0;
-    int y = 0, x = 0;
-    for(const auto& dir : dirs)
+    dir['U'] = 0;dir['D'] = 1;dir['R'] = 2;dir['L'] = 3;
+    for(const auto& iter : dirs)
     {
-        int sy = y;
-        int sx = x;
-        int ny = y + D[dir][0];
-        int nx = x + D[dir][1];
-        if(ny < -5 || nx < -5 || ny > 5 || nx > 5) continue;
-        chks.insert({{sy, sx}, {ny, nx}});
-        chks.insert({{ny, nx}, {sy, sx}});
+        int ny = y + dy[dir[iter]];
+        int nx = x + dx[dir[iter]];
+        if(ny < -5 || nx < -5 || ny > 5 || nx > 5)
+            continue;
+        if(checks.find({{y, x}, {ny, nx}}) == checks.end())
+            answer++;
+        checks.insert({{y, x}, {ny, nx}});
+        checks.insert({{ny, nx}, {y, x}});
         y = ny;
         x = nx;
     }
-    
-    return chks.size() / 2;
+    return answer;
 }
