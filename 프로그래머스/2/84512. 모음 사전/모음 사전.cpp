ece7c3e612f740d55bb99c92eb14c78139389a54
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2025-10-16 14:00:16 +0900

    [level 2] Title: 모음 사전, Time: 0.91 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.cpp
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.cpp
@@ -1,28 +1,9 @@
-#include <bits/stdc++.h>
+#include <string>
+#include <vector>
 
 using namespace std;
-std::unordered_map<int, char> ch;
-std::unordered_map<std::string, int> words;
-int idx = 0;
-void Check(std::string str)
-{
-    //std::cout << str << '\n';
-    words[str] = idx++;
-    if(str.size() >= 5) return;
-    for(int i = 0; i < 5; i++)
-    {
-        str += ch[i];
-        Check(str);
-        str.pop_back();
-    }
-}
-int solution(string word) 
-{
-    ch[0] = 'A';
-    ch[1] = 'E';
-    ch[2] = 'I';
-    ch[3] = 'O';
-    ch[4] = 'U';
-    Check("");
-    return words[word];
+
+int solution(string word) {
+    int answer = 0;
+    return answer;
 }
