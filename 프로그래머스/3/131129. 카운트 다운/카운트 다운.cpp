a6e644f38b20b7b710886505c816730edc775af8
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2025-10-23 07:58:32 +0900

    [level 3] Title: 카운트 다운, Time: 6.64 ms, Memory: 4.34 MB -BaekjoonHub

--- 프로그래머스/3/131129. 카운트 다운/카운트 다운.cpp
+++ 프로그래머스/3/131129. 카운트 다운/카운트 다운.cpp
@@ -5,7 +5,8 @@ int dp[100'002], cnts[100'002];
 std::vector<int> sb = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
 vector<int> solution(int target) 
 {
-    std::fill_n(&dp[1], 100'002, 100'000);
+    std::fill_n(&dp[0], 100'002, 100'000);
+    dp[0] = 0;
     for(int i = 0; i < sb.size(); i++)
     {
         dp[sb[i]] = 1;
@@ -30,12 +31,12 @@ vector<int> solution(int target)
             if(i - iter * 2 >= 0 && dp[i-iter*2] + 1 < dp[i])
             {
                 dp[i] = dp[i-iter*2] + 1;
-                cnts[i] = 0;
+                cnts[i] = cnts[i-iter*2];
             }
             if(i - iter * 3 >= 0 && dp[i-iter*3] + 1 < dp[i])
             {
                 dp[i] = dp[i-iter*3] + 1;
-                cnts[i] = 0;
+                cnts[i] = cnts[i-iter*3];
             }
         }
     }
