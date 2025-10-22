afe83184996213a6d710181cab8f083d668e9059
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2025-10-23 02:23:49 +0900

    [level 2] Title: 숫자 변환하기, Time: 7.76 ms, Memory: 7.44 MB -BaekjoonHub

--- 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.cpp
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.cpp
@@ -1,22 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
-int dp[1'000'005];
+std::queue<int> q;
+int dist[1'000'002];
 int solution(int x, int y, int n) 
 {
-    std::queue<int> q;
-    q.push(x);
-    dp[x] = 1;
+    int answer = 0;
+    dist[y] = 1;
+    q.push(y);
     while(q.size())
     {
-        int cur = q.front();
+        auto cur = q.front();
         q.pop();
-        for(const auto& iter : {cur + n, cur * 2, cur * 3})
+        if(cur % 2 == 0 && dist[cur/2] == 0)
         {
-            if(1'000'000 < iter) continue;
-            if(dp[iter]) continue;
-            dp[iter] = dp[cur] + 1;
-            q.push(iter);
+            dist[cur/2] = dist[cur] + 1;
+            q.push(cur/2);
+        }
+        if(cur % 3 == 0 && dist[cur/3] == 0)
+        {
+            dist[cur/3] = dist[cur] + 1;
+            q.push(cur/3);
+        }
+        if(cur - n >= 1 && dist[cur-n] == 0)
+        {
+            dist[cur-n] = dist[cur]+1;
+            q.push(cur-n);
         }
     }
-    return (dp[y] ? dp[y]-1 : -1);
+    return dist[x] - 1;
 }
