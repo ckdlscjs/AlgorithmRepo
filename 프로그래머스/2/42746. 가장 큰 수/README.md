2bc4a8ba2396674fe2783354d02ac7e20cbc77b6
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2025-11-04 08:41:18 +0900

    [level 2] Title: 가장 큰 수, Time: 59.04 ms, Memory: 10.6 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/README.md
+++ 프로그래머스/2/42746. 가장 큰 수/README.md
@@ -1,10 +1,10 @@
 # [level 2] 가장 큰 수 - 42746 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42746) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42746#) 
 
 ### 성능 요약
 
-메모리: 7.52 MB, 시간: 312.03 ms
+메모리: 10.6 MB, 시간: 59.04 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 27일 05:14:46
+2025년 11월 04일 08:41:16
 
 ### 문제 설명
 
--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.cpp
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.cpp
@@ -1,17 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool compare(const int& a, const int& b)
-{
-    std::string st1 = std::to_string(a);
-    std::string st2 = std::to_string(b);
-    return std::stoi(st1+st2) > std::stoi(st2+st1);
-}
 string solution(vector<int> numbers) 
 {
-    std::sort(numbers.begin(), numbers.end(), compare);
-    string answer = "";
+    std::vector<std::string> nums;
     for(const auto& iter : numbers)
-        answer += std::to_string(iter);
-    return answer[0] == '0' ? "0" : answer;
+        nums.push_back(std::to_string(iter));
+    
+    std::sort(nums.begin(), nums.end(), [](const std::string& a, const std::string& b){
+        return (a + b) > (b + a);
+    });
+    string answer = "";
+    for(const auto& iter : nums)
+        answer += iter;
+    return (answer[0] == '0') ? "0" : answer;
 }
