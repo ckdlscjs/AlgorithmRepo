c0dc060cd34f2a96002c486319ff11b49df85eec
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2025-11-08 05:34:01 +0900

    [level 2] Title: 삼각 달팽이, Time: 158.14 ms, Memory: 107 MB -BaekjoonHub

---
+++ 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int dy[] = {1, 0, -1};
+const int dx[] = {0, 1, -1};
+int dir;
+vector<int> solution(int n) 
+{
+    std::vector<int> answer;
+    answer.resize(n * (n+1) / 2, 0);
+    int y = 0, x = 0, num = 1;
+    
+    for(int i = n; i >= 1; i--)
+    {
+        for(int j = 0; j < i; j++)
+        {
+            int idx = y * n + x;
+            for(int diff = 0; diff < y; diff++)
+            {
+                idx -= (n-(diff+1));
+            }
+            answer[idx] = num++;
+            y += dy[dir];
+            x += dx[dir];
+        }
+        y -= dy[dir];
+        x -= dx[dir];
+        dir++;
+        dir%=3;
+        y += dy[dir];
+        x += dx[dir];
+    }
+    
+    return answer;
+}
