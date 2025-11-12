793be4c7e3afd909741c48dc7445415340acf510
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2025-11-13 03:05:42 +0900

    [level 2] Title: 서버 증설 횟수, Time: 0.01 ms, Memory: 4.15 MB -BaekjoonHub

--- 프로그래머스/2/389479. 서버 증설 횟수/README.md
+++ 프로그래머스/2/389479. 서버 증설 횟수/README.md
@@ -1,10 +1,10 @@
 # [level 2] 서버 증설 횟수 - 389479 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/389479?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/389479) 
 
 ### 성능 요약
 
-메모리: 4.12 MB, 시간: 0.04 ms
+메모리: 4.15 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 02월 18일 03:09:16
+2025년 11월 13일 03:05:39
 
 ### 문제 설명
 
--- 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.cpp
+++ 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.cpp
@@ -3,18 +3,23 @@
 using namespace std;
 
 int solution(vector<int> players, int m, int k) 
-{  
+{
     int answer = 0;
-    std::priority_queue<int, std::vector<int>, std::greater<int>> servers;
+    std::queue<int> servers;
     for(int i = 0; i < players.size(); i++)
     {
-        while(servers.size() && servers.top() < i) servers.pop();
-        if(players[i] / m > servers.size())
+        while(servers.size() && i >= servers.front())
         {
-            int cnt = (players[i] / m) - servers.size();
-            answer += cnt;
-            for(int j = 0; j < cnt; j++) servers.push(i + k - 1);
+            servers.pop();
+        }
+        if(players[i] >= (servers.size() + 1) * m)
+        {
+            int need = players[i] / m - servers.size();
+            answer += need;
+            for(int j = 0; j < need; j++)
+                servers.push(i + k);
         }
     }
+    
     return answer;
 }
