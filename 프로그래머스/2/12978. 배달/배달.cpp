193656b510e042d486d715a449a22952326f6f87
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2025-11-15 02:35:52 +0900

    [level 2] Title: 배달, Time: 0.23 ms, Memory: 4.15 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.cpp
+++ 프로그래머스/2/12978. 배달/배달.cpp
@@ -1,44 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
-const int MaxN = 55;
-const int INF = 987654321;
-std::vector<std::pair<int, int>> graph[MaxN];
-int solution(int N, vector<vector<int>> road, int K) {
-    std::ios::sync_with_stdio(false);
-    std::cin.tie(0);
-    std::cout.tie(0);
+const int INF = 10'000 * 10'000 + 5;
+std::vector<std::pair<int, int>> graph[52];
+std::priority_queue<std::pair<int, int>, std::vector<std::pair<int, int>>, std::greater<std::pair<int, int>>> pq;
+int dist[52];
+int solution(int N, vector<vector<int>> road, int K) 
+{
+    std::fill_n(&dist[0], 52, INF);
     for(const auto& iter : road)
     {
         graph[iter[0]].push_back({iter[1], iter[2]});
         graph[iter[1]].push_back({iter[0], iter[2]});
     }
-    int costs[MaxN];
-    std::fill(costs, costs+MaxN, INF);
-    
-    std::priority_queue<std::pair<int, int>, std::vector<std::pair<int,int>>, std::greater<std::pair<int,int>>> pq;
-    costs[1] = 0;
+    dist[1] = 0;
     pq.push({0, 1});
     while(pq.size())
     {
-        int sum_cost = pq.top().first;
-        int idx = pq.top().second;
+        int cur = pq.top().second;
+        int cost = pq.top().first;
         pq.pop();
-        if(sum_cost > costs[idx])
-            continue;
-        for(int i = 0; i < graph[idx].size(); i++)
+        if(dist[cur] < cost) continue;
+        for(const auto& iter : graph[cur])
         {
-            int cost = graph[idx][i].second;
-            int j = graph[idx][i].first;
-            if(cost+sum_cost < costs[j])
+            if(dist[cur] + iter.second < dist[iter.first])
             {
-                costs[j] = cost + sum_cost;
-                pq.push({costs[j], j});
+                dist[iter.first] = dist[cur] + iter.second;
+                pq.push({dist[iter.first], iter.first});
             }
         }
     }
     int answer = 0;
     for(int i = 1; i <= N; i++)
-        if(costs[i] <= K)
+        if(dist[i] <= K)
             answer++;
     return answer;
 }
