8cc2bfa463cd127d91299d47299bac6eb8cbd96f
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2025-11-19 13:19:44 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 1.57 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.cpp
+++ 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.cpp
@@ -1,43 +1,53 @@
-//https://school.programmers.co.kr/learn/courses/30/lessons/72411
 #include <bits/stdc++.h>
+
 using namespace std;
-std::vector<std::pair<std::string, int>> results[15];
-std::map<std::string, int> menus;
-void Check(const std::string& order, int length, std::string menu, int idx)
+std::map<int, int> cnts;
+void Check(const std::string& str, const int length, int cur, int idx, int mask)
 {
-    if(menu.size() >= length)
+    if(cur >= length)
     {
-        menus[menu]++;
+        cnts[mask]++;
         return;
     }
-    for(int i = idx; i < order.size(); i++)
-        Check(order, length, menu + order[i], i+1);
+    for(int i = idx; i < str.size(); i++)
+    {
+        Check(str, length, cur+1, i+1, mask | 1 << (str[i] - 'A'));
+    }
 }
 vector<string> solution(vector<string> orders, vector<int> course) 
 {
-    for(auto& iter : orders)
-    {
-        std::sort(iter.begin(), iter.end(), std::less<>());
-        for(const auto& cnt : course)
-            Check(iter, cnt, {}, 0);
-    }
-        
-    for(const auto& iter : menus)
-    {
-        if(iter.second <= 1)
-            continue;
-        results[iter.first.size()].push_back(iter);
-    }
     vector<string> answer;
-    for(const auto& iter : course)
+    for(const auto& iter : orders)
     {
-        std::sort(results[iter].begin(), results[iter].end(), [](const std::pair<std::string, int>& a, const std::pair<std::string, int>& b)
-                  {
-                      return a.second == b.second ? a.first < b.first : a.second > b.second;
-                  });
-        for(int i = 0; i < results[iter].size() && results[iter][i].second == results[iter][0].second; i++)
-            answer.push_back(results[iter][i].first);
+        for(const auto& cnt : course)
+        {
+            Check(iter, cnt, 0, 0, 0);
+        }
     }
-    std::sort(answer.begin(), answer.end(), std::less<>());
+    std::map<int, std::vector<std::pair<int, std::string>>> results;
+    for(const auto& iter : cnts)
+    {
+        std::string str;
+        for(int i = 0; i < 26; i++)
+        {
+            if(iter.first & (1 << i))
+            str += std::string(1, (char)i + 'A');
+        }
+        results[str.size()].push_back({iter.second, str});
+    }
+    
+    
+    for(auto& iter : results)
+    {
+        std::sort(iter.second.begin(), iter.second.end(), [](const std::pair<int, std::string>& a, const std::pair<int, std::string>& b) {
+           return a.first > b.first; 
+        });
+        
+        for(int i = 0; i < iter.second.size() && iter.second[i].first >= 2 && iter.second[i].first == iter.second[0].first; i++)
+        {
+            answer.push_back(iter.second[i].second);
+        }
+    }
+    std::sort(answer.begin(), answer.end());
     return answer;
 }
