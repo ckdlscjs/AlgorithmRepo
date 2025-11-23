58695c42d49986e1bb98c2f3628818259d451560
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2025-11-23 21:50:23 +0900

    [level 3] Title: 섬 연결하기, Time: 0.02 ms, Memory: 4.18 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.cpp
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.cpp
@@ -1,46 +1,34 @@
-//https://school.programmers.co.kr/learn/courses/30/lessons/42861
 #include <bits/stdc++.h>
+
 using namespace std;
-struct DisjointSet
+std::vector<int> parents;
+std::vector<int> ranks;
+int Find(int node)
 {
-    std::vector<int> parents;
-    std::vector<int> ranks;
-    DisjointSet(int _size) : parents(_size, -1), ranks(_size, 0) {}
-    int Find(int node)
-    {
-        if(parents[node] == -1)
-            return node;
-        return parents[node] = Find(parents[node]);
-    }
-    void Union(int node1, int node2)
-    {
-        int root1 = Find(node1);
-        int root2 = Find(node2);
-        if(root1 == root2)
-            return;
-        if(ranks[root1] == ranks[root2])
-        {
-            ranks[root1] ++;
-            parents[root2] = root1;
-        }
-        if(ranks[root1] > ranks[root2])
-            parents[root2] = root1;
-        else
-            parents[root1] = root2;
-    }
-};
+    if(parents[node] == node || parents[node] == -1) return parents[node] = node;
+    return parents[node] = Find(parents[node]);
+}
+void Union(int node1, int node2)
+{
+    int root1 = Find(node1);
+    int root2 = Find(node2);
+    if(root1 == root2) return;
+    if(ranks[root1] == ranks[root2]) ranks[root1]++;
+    if(ranks[root1] > ranks[root2]) parents[root2] = root1; 
+    else parents[root1] = root2;
+}
 int solution(int n, vector<vector<int>> costs) 
 {
-    DisjointSet ds(n);
-    int answer = 0;
-    std::sort(costs.begin(), costs.end(), [](const std::vector<int>& a, const std::vector<int>& b){
+    parents.resize(n+1, -1);
+    ranks.resize(n+1, 0);
+    std::sort(costs.begin(), costs.end(), [](const vector<int>& a, const vector<int>& b){
         return a[2] < b[2];
     });
+    int answer = 0;
     for(const auto& iter : costs)
     {
-        if(ds.Find(iter[0]) == ds.Find(iter[1]))
-            continue;
-        ds.Union(iter[0], iter[1]);
+        if(Find(iter[0]) == Find(iter[1])) continue;
+        Union(iter[0], iter[1]);
         answer += iter[2];
     }
     return answer;
