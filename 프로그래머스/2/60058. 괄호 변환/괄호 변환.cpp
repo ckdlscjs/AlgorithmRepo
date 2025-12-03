8e8a007b01d04c66e99a53201ba7f2781336a1bb
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2025-12-03 13:07:30 +0900

    [level 2] Title: 괄호 변환, Time: 0.11 ms, Memory: 4.23 MB -BaekjoonHub

--- 프로그래머스/2/60058. 괄호 변환/괄호 변환.cpp
+++ 프로그래머스/2/60058. 괄호 변환/괄호 변환.cpp
@@ -1,31 +1,31 @@
-//https://school.programmers.co.kr/learn/courses/30/lessons/60058
 #include <bits/stdc++.h>
+
 using namespace std;
-string solution(string p) 
+
+string solution(string w) 
 {
-    if(p.empty())
-        return "";
-    bool chk = (p[0] == '(' ? true : false);
-    int cnt_l = 0, cnt_r = 0;
-    std::string u = "", v = "";
-    for(const auto& ch : p)
+    if(w.empty()) return w;
+    int cl = 0, cr = 0;
+    bool chk = w[0] == '(';
+    std::string u, v;
+    for(const auto& ch : w)
     {
         u += ch;
-        if(ch == '(') cnt_l++;
-        else cnt_r++;
-        if(cnt_l == cnt_r)
-            break;
+        cl += ch == '(';
+        cr += ch == ')';
+        if(cl == cr) break;
     }
-    v = p.substr(cnt_l + cnt_r);
-    v = solution(v);
-    if(chk) 
-        return u+v;
-    std::string temp = "";
-    temp += '(';
-    temp += v;
-    temp += ')';
-    for(int i = 1; i < u.size() - 1; i++)
-        u[i] = (u[i] == '(' ? ')' : '(');
-    temp += u.substr(1, u.size()-2);
-    return temp;
+    v = w.substr(u.size());
+    std::string recursive = solution(v);
+    if(chk)
+    {
+        return u + recursive;
+    }
+    else
+    {
+        u = std::string(u.begin()+1, u.end()-1);
+        for(auto& ch : u)
+            ch = ch == '(' ? ')' : '(';
+        return "(" + recursive + ")" + u;
+    }
 }
