837b23b16abb22c61c5f9c91f8e3faa731a09717
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2025-12-04 10:58:30 +0900

    [level 2] Title: 가장 큰 정사각형 찾기, Time: 6.91 ms, Memory: 42.6 MB -BaekjoonHub

--- 프로그래머스/2/12905. 가장 큰 정사각형 찾기/가장 큰 정사각형 찾기.cpp
+++ 프로그래머스/2/12905. 가장 큰 정사각형 찾기/가장 큰 정사각형 찾기.cpp
@@ -1,23 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
+std::vector<std::vector<int>> dp;
 int solution(vector<vector<int>> board)
 {
+    dp.resize(board.size(), std::vector<int>(board[0].size(), 0));
     int answer = 0;
-    for(int i = 1; i < board.size(); i++)
-    {
-        for(int j = 1; j < board[0].size(); j++)
-        {
-            if(!board[i][j])
-                continue;
-            board[i][j] = std::min({board[i-1][j-1], board[i-1][j], board[i][j-1]}) + 1;
-        }
-    }
     for(int i = 0; i < board.size(); i++)
     {
-        for(int j = 0; j < board[0].size(); j++)
+        for(int j = 0; j < board[i].size(); j++)
         {
-            answer = std::max(board[i][j], answer);
+            if(!board[i][j]) continue;
+            if(i >= 1 && j >= 1) dp[i][j] = std::min({dp[i][j-1], dp[i-1][j], dp[i-1][j-1]}) + 1;
+            else dp[i][j] = board[i][j];
+                
+            answer = std::max(dp[i][j], answer);
+            //std::cout << dp[i][j] << ' ';
         }
+        //std::cout << '\n';
     }
     return answer*answer;
 }
