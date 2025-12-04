6756774d3f4bf5465016ddf6a87831a0cc077a80
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2025-12-04 13:47:35 +0900

    [level 3] Title: 입국심사, Time: 59.42 ms, Memory: 6.87 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.cpp
+++ 프로그래머스/3/43238. 입국심사/입국심사.cpp
@@ -1,20 +1,17 @@
-//https://school.programmers.co.kr/learn/courses/30/lessons/43238
 #include <bits/stdc++.h>
+#define ll long long
 using namespace std;
-bool Check(int n, const vector<int>& times, long long int t)
+
+ll solution(int n, vector<int> times) 
 {
-    long long int cnt = 0;
-    for(const auto& iter : times)
-        cnt += t / (long long int)iter;
-    return cnt >= n;
-}
-long long solution(int n, vector<int> times) 
-{
-    long long int lo = 0, hi = 1'000'000'000'000'005;
+    ll lo = 0, hi = 1e9 * 1e9 + 5LL;
     while(lo + 1 < hi)
     {
-        long long int mid = (lo + hi) / 2;
-        if(Check(n, times, mid)) hi = mid;
+        ll mid = (lo + hi) / 2;
+        ll c = 0;
+        for(const auto& t : times)
+            c += mid / (long long)t;
+        if(n <= c) hi = mid;
         else lo = mid;
     }
     return hi;
