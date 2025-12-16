b80a9070bd6002ad4f9d593d0e99ffeeaae8a8fc
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2025-12-16 22:55:36 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 퍼즐 게임 챌린지, Time: 21.53 ms, Memory: 23.6 MB -BaekjoonHub

--- 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.cpp
+++ 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.cpp
@@ -2,23 +2,23 @@
 
 using namespace std;
 
-long long int solution(vector<int> diffs, vector<int> times, long long limit) 
+int solution(vector<int> diffs, vector<int> times, long long limit) 
 {
-    long long int lo = 0, hi = *std::max_element(diffs.begin(), diffs.end()) + 1;
+    int lo = 0, hi = 100'000;
     while(lo + 1 < hi)
     {
-        long long int mid = (lo + hi) / 2;
-        long long int cur = 0;
+        int level = (lo + hi) / 2;
+        long long cur = 0;
+        int prev = 0;
         for(int i = 0; i < diffs.size(); i++)
         {
-            if(diffs[i] <= mid)
-                cur += times[i];
-            else
-                cur += (diffs[i]-mid)*(times[i] + (i > 0 ? times[i-1] : 0)) + times[i];
+            if(level < diffs[i])
+                cur += (diffs[i] - level) * (times[i] + prev);
+            cur += times[i];
+            prev = times[i];
         }
-        if(cur <= limit) hi = mid;
-        else lo = mid;
+        if(cur <= limit) hi = level;
+        else lo = level;
     }
-    
     return hi;
 }
