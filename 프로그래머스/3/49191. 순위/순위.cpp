fd8ca649abf0fc065d016f892dbb9cf18b149095
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2025-12-18 16:27:31 +0900

    [level 3] Title: 순위, Time: 2.77 ms, Memory: 4.13 MB -BaekjoonHub

--- 프로그래머스/3/49191. 순위/순위.cpp
+++ 프로그래머스/3/49191. 순위/순위.cpp
@@ -1,44 +1,44 @@
-//https://school.programmers.co.kr/learn/courses/30/lessons/49191
 #include <bits/stdc++.h>
+
 using namespace std;
-int costs[105][105];
+const int INF = 100 * 105;
+int dp[102][102];
 int solution(int n, vector<vector<int>> results) 
 {
+    std::fill_n(&dp[0][0], 102*102, INF);
+    for(int i = 1; i <= n; i++) dp[i][i] = 0;
     for(const auto& iter : results)
     {
-        costs[iter[1]][iter[0]] = 1;
-        costs[iter[0]][iter[1]] = -1;
+        dp[iter[0]][iter[1]] = 1;
+        dp[iter[1]][iter[0]] = -1;
     }
-    for(int m = 1; m <= n; m++)
+    for(int k = 1; k <= n; k++)
     {
         for(int i = 1; i <= n; i++)
         {
             for(int j = 1; j <= n; j++)
             {
-                if(!costs[i][m]) continue;
-                if(costs[i][m] == 1 && costs[m][j] == 1)
+                if(dp[i][k] >= INF || dp[k][j] >= INF) continue;
+                if(dp[i][k] == 1 && dp[k][j] == 1)
                 {
-                    costs[i][j] = 1;
-                    costs[j][i] = -1;
+                    dp[i][j] = 1;
+                    dp[j][i] = -1;
                 }
-                else if(costs[i][m] == -1 && costs[m][j] == -1)
+                else if(dp[i][k] == -1 && dp[k][j] == -1)
                 {
-                    costs[i][j] = -1;
-                    costs[j][i] = 1;
+                    dp[i][j] = -1;
+                    dp[j][i] = 1;
                 }
             }
         }
-    }     
+    }
     int answer = 0;
     for(int i = 1; i <= n; i++)
     {
-        bool chk = true;
-        for(int j = 1; j <= n; j++)
-        {
-            if(i == j) continue;
-            if(!costs[i][j]) chk = false;
-        }
-        if(chk) answer++;
+        int j = 1;
+        while(j <= n && dp[i][j] < INF) j++;
+        answer += j > n ? 1 : 0;
     }
+    
     return answer;
 }
