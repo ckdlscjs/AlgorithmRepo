f9de39410ed017c9644ddc4b0683d1a2bd21a91f
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2025-12-29 23:22:05 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 석유 시추, Time: 12.96 ms, Memory: 14.4 MB -BaekjoonHub

--- 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.cpp
+++ 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.cpp
@@ -1,50 +1,47 @@
-//https://school.programmers.co.kr/learn/courses/30/lessons/250136
 #include <bits/stdc++.h>
 using namespace std;
-const int dy[] = {0, 0, 1, -1};
-const int dx[] = {1, -1, 0, 0};
-int amount[62'505];
-int visited[505][505];
-int dfs(const std::vector<std::vector<int>>& land, const int mask, int y, int x)
+const int dy[] = {-1, 1, 0, 0};
+const int dx[] = {0, 0, -1, 1};
+std::vector<int> amounts;
+std::vector<std::vector<int>> arr;
+int DFS(const std::vector<std::vector<int>>& land, const int area, int i, int j)
 {
     int ret = 1;
-    visited[y][x] = mask;
+    arr[i][j] = area;
     for(int dir = 0; dir < 4; dir++)
     {
-        int ny = y + dy[dir];
-        int nx = x + dx[dir];
-        if(ny < 0 || nx < 0 || ny >= land.size() || nx >= land[0].size() || visited[ny][nx] || !land[ny][nx])
-            continue;
-        ret += dfs(land, mask, ny, nx);
+        int ny = i + dy[dir];
+        int nx = j + dx[dir];
+        if(ny < 0 || nx < 0 || ny >= land.size() || nx >= land[0].size()) continue;
+        if(land[ny][nx] == 0 || arr[ny][nx] != -1) continue;
+        ret += DFS(land, area, ny, nx);
     }
     return ret;
 }
 int solution(vector<vector<int>> land) 
 {
-    int mask = 1;
+    arr.resize(land.size(), std::vector<int>(land[0].size(), -1));
     for(int i = 0; i < land.size(); i++)
     {
         for(int j = 0; j < land[i].size(); j++)
         {
-            if(visited[i][j] || !land[i][j])
-                continue;
-            int cnt = dfs(land, mask, i, j);
-            amount[mask++] = cnt;
+            if(land[i][j] == 0) continue;
+            if(arr[i][j] != -1) continue;
+            amounts.push_back(DFS(land, amounts.size(), i, j));
         }
     }
     int answer = 0;
     for(int j = 0; j < land[0].size(); j++)
     {
+        std::unordered_set<int> idxs;
         int sum = 0;
-        std::unordered_set<int> chks;
         for(int i = 0; i < land.size(); i++)
         {
-            if(!visited[i][j])
-                continue;
-            chks.insert(visited[i][j]);
+            if(arr[i][j] == -1) continue;
+            idxs.insert(arr[i][j]);
         }
-        for(const auto& iter : chks)
-            sum += amount[iter];
+        for(const auto& iter : idxs)
+            sum += amounts[iter]; 
         answer = std::max(answer, sum);
     }
     return answer;
