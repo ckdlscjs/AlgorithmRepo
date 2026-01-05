d0e5ba22668abf0744ab7289bf7a7d5490f9cb5d
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2026-01-05 14:34:00 +0900

    [level 2] Title: 요격 시스템, Time: 128.74 ms, Memory: 94.7 MB -BaekjoonHub

--- 프로그래머스/2/181188. 요격 시스템/요격 시스템.cpp
+++ 프로그래머스/2/181188. 요격 시스템/요격 시스템.cpp
@@ -1,18 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
+
 int solution(vector<vector<int>> targets) 
 {
     std::sort(targets.begin(), targets.end(), [](const std::vector<int>& a, const std::vector<int>& b)
               {
-                 return a[1] < b[1]; 
+                  return a[1] == b[1] ? a[0] < b[0] : a[1] < b[1];
               });
-    int right = targets[0][1] - 1;
-    int answer = 1;
+    int answer = 0, cr = 0;
     for(const auto& iter : targets)
     {
-        if(iter[0] > right)
+        //std::cout << iter[0] << ' ' << iter[1] << '\n';
+        if(iter[0] >= cr)
         {
-            right = iter[1] - 1;
+            cr = iter[1];
             answer++;
         }
     }
