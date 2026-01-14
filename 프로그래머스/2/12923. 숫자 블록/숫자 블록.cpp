802d6973b53e04b005124a4e9db4d403066cf8ea
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2026-01-15 00:17:43 +0900

    [level 2] Title: 숫자 블록, Time: 279.57 ms, Memory: 4.71 MB -BaekjoonHub

---
+++ 프로그래머스/2/12923. 숫자 블록/숫자 블록.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<int> solution(long long begin, long long end) 
+{
+    vector<int> answer;
+    for(begin; begin <= end; begin++)
+    {
+        if(begin == 1)
+        {
+            answer.push_back(0);
+            continue;
+        }
+        int res = 1;
+        for(int i = 2; i <= std::sqrt(begin); i++)
+        {
+            if(begin % i == 0)
+            {
+                res = std::max(res, i);
+                if(begin / i <= 10'000'000)
+                {
+                    res = begin / i;
+                    break;
+                }  
+            }
+        }
+        answer.push_back(res);
+    }
+    return answer;
+}
