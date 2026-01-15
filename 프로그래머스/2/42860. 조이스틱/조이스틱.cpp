802bd99c71715f3664c0eb980e3aab46bcb372a3
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2026-01-15 22:15:52 +0900

    [level 2] Title: 조이스틱, Time: 0.01 ms, Memory: 3.66 MB -BaekjoonHub

--- 프로그래머스/2/42860. 조이스틱/조이스틱.cpp
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.cpp
@@ -1,25 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-int solution(string name) {
-    std::unordered_map<char, int> alps;
-    for(int i = 0; i < 26; i++)
-    {
-        if(i <= 13)
-            alps[i + 'A'] = i;
-        else
-            alps[i+'A'] = 26 - i;
-    }
+int solution(string name) 
+{
     int answer = name.size()-1;
     for(int i = 0; i < name.size(); i++)
     {
         int cursor = i+1;
         while(cursor < name.size() && name[cursor] == 'A') cursor++;
-        answer = std::min(answer, (int)(i*2 + name.size() - cursor));
-        answer = std::min(answer, (int)(i + (name.size() - cursor) * 2));
+        //...i AAAA cursor... 
+        answer = std::min(answer, (int)(i*2 + name.size() - cursor)); //left*2, left
+        answer = std::min(answer, (int)(i + (name.size() - cursor) * 2)); //left, right*2
     }
     
     for(const auto& iter : name)
-        answer += alps[iter];
+    {
+        int diff = iter - 'A';
+        answer += std::min(diff, 26-diff);
+    }
     return answer;
 }
