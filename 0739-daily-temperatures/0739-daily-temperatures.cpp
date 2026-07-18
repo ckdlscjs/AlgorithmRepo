19cfc70e7b903220492c904a9f5b29713b072c08
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2026-07-18 13:27:43 +0900

    Time: 9 ms (96.27%), Space: 102.8 MB (91.46%) - LeetHub

---
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -0,0 +1,25 @@
+auto fastio = []()
+{
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(0);
+    std::cout.tie(0);
+    return 0;
+}();
+
+class Solution 
+{
+public:
+    vector<int> dailyTemperatures(vector<int>& temp) 
+    {
+        std::vector<int> rets(temp.size(), 0);
+        std::stack<int> st;
+        for(int i = temp.size()-1; i >= 0; i--)
+        {
+            while(st.size() && temp[st.top()] <= temp[i])
+                st.pop();
+            rets[i] = st.empty() ? 0 : st.top() - i;
+            st.push(i);
+        }
+        return rets;
+    }
+};
