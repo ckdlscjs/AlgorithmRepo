412a6728f8136fc4099995af09feda2e9de19c79
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2026-07-24 14:37:11 +0900

    Time: 59 ms (65.91%), Space: 173.8 MB (15.04%) - LeetHub

---
+++ 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.cpp
@@ -0,0 +1,36 @@
+auto fastio = [](){
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(0);
+    std::cout.tie(0);
+    return 0;
+};
+#define tiii std::tuple<int, int, int>
+class Solution 
+{
+public:
+    vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) 
+    {
+        std::priority_queue<tiii, std::vector<tiii>, std::greater<tiii>> pq;
+        int smallest = std::min(nums1.size(), nums2.size());
+        for(int i = 0; i < nums1.size(); i++)
+        {
+            pq.push({nums1[i] + nums2[0], i, 0});
+        }
+
+        std::vector<std::vector<int>> ret;
+        int i = 1;
+        while(k)
+        {
+            k--;
+            auto iter = pq.top(); pq.pop();
+            int idx1 = std::get<1>(iter);
+            int idx2 = std::get<2>(iter);
+            ret.push_back({nums1[idx1], nums2[idx2]});
+            if(idx2+1 < nums2.size())
+            {
+                pq.push({nums1[idx1] + nums2[idx2+1], idx1, idx2+1});
+            }
+        }
+        return ret;
+    }
+};
