a91d1e91d8ff392bbac7c22a5efbe651f19faf6c
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2026-07-24 14:49:14 +0900

    Time: 39 ms (92.56%), Space: 157.6 MB (66.88%) - LeetHub

--- 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.cpp
+++ 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.cpp
@@ -3,41 +3,28 @@ auto fastio = [](){
     std::cin.tie(0);
     std::cout.tie(0);
     return 0;
-};
+}();
 #define tiii std::tuple<int, int, int>
-struct Entity
-{
-    int sum;
-    int idx1;
-    int idx2;
-};
-struct comp
-{
-    bool operator()(const Entity& e1, const Entity& e2)
-    {
-        return e1.sum > e2.sum;
-    }
-};
 class Solution 
 {
 public:
     vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) 
     {
-        std::priority_queue<Entity, std::vector<Entity>, comp> pq;
-        int smallest = std::min(nums1.size(), nums2.size());
-        for(int i = 0; i < nums1.size(); i++)
+        std::priority_queue<tiii, std::vector<tiii>, std::greater<tiii>> pq;
+        
+        for(int i = 0; i < std::min(k, (int)nums1.size()); i++)
         {
             pq.push({nums1[i] + nums2[0], i, 0});
         }
 
         std::vector<std::vector<int>> ret;
         int i = 1;
-        while(k && !pq.empty())
+        while(k)
         {
             k--;
             auto iter = pq.top(); pq.pop();
-            int idx1 = iter.idx1;
-            int idx2 = iter.idx2;
+            int idx1 = std::get<1>(iter);
+            int idx2 = std::get<2>(iter);
             ret.push_back({nums1[idx1], nums2[idx2]});
             if(idx2+1 < nums2.size())
             {
