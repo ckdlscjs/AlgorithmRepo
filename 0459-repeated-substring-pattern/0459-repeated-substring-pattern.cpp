38e42a1eb1a10bf3d01f51d617914b7cebbc316d
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2026-07-30 21:17:57 +0900

    Time: 7 ms (61.95%), Space: 15.2 MB (72.47%) - LeetHub

--- 0459-repeated-substring-pattern/0459-repeated-substring-pattern.cpp
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.cpp
@@ -2,14 +2,7 @@ class Solution {
 public:
     bool repeatedSubstringPattern(string s) 
     {
-        for(int i = 1; i <= s.size() / 2; i++)
-        {
-            auto substr = s.substr(0, i);
-            std::string temp;
-            for(int j = 0; j < s.size() / i; j++)
-                temp += substr;
-            if(temp == s) return true;
-        }
-        return false;
+        auto str = (s + s).substr(1, s.size() * 2 - 2);
+        return str.find(s) != std::string::npos;
     }
 };
