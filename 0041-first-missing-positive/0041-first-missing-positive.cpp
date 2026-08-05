29e913c95ef592441b1402dcdd6e390d967a9a22
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2026-08-06 00:07:30 +0900

    Time: 0 ms (100%), Space: 55.1 MB (63.32%) - LeetHub

---
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) 
+    {
+        for(int i = 0; i < nums.size(); i++)
+        {
+            while(0 < nums[i] && nums[i] <= nums.size() && nums[i] != nums[nums[i]-1])
+                std::swap(nums[i], nums[nums[i]-1]);
+        }
+        for(int i = 0; i < nums.size(); i++)
+        {
+            if(nums[i] != i+1)
+                return i+1;
+        }
+        return nums.back() + 1;
+    }
+};
