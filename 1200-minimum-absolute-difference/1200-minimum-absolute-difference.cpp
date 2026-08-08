b067b6ab44760b82b455db8b67f5006ca5b22b2b
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2026-08-08 09:08:54 +0900

    Time: 19 ms (24.24%), Space: 36.7 MB (46.61%) - LeetHub

--- 1200-minimum-absolute-difference/1200-minimum-absolute-difference.cpp
+++ 1200-minimum-absolute-difference/1200-minimum-absolute-difference.cpp
@@ -2,10 +2,14 @@ class Solution {
 public:
     vector<vector<int>> minimumAbsDifference(vector<int>& arr) 
     {
+        vector<vector<int>> ret;
         std::sort(arr.begin(), arr.end());
-        std::map<int, vector<vector<int>>> mp;
+        int diff_min = 1e7;
         for(int i = 0; i < arr.size()-1; i++)
-            mp[arr[i+1] - arr[i]].push_back({arr[i], arr[i+1]});
-        return mp.begin()->second;
+            diff_min = std::min(diff_min, arr[i+1] - arr[i]);
+        for(int i = 0; i < arr.size()-1; i++)
+            if(diff_min == arr[i+1] - arr[i])
+                ret.push_back({arr[i], arr[i+1]});
+        return ret;
     }
 };
