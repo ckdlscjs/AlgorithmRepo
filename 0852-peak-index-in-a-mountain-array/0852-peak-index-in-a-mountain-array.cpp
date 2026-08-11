a1d662933d05c3cf9e1c6676166a6c63e52f313e
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2026-08-11 15:59:11 +0900

    Time: 0 ms (100%), Space: 63.7 MB (11%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int peakIndexInMountainArray(vector<int>& arr) 
     {
-        int lo = -1, hi = arr.size();
+        int lo = 0, hi = arr.size()-1;
         while(lo + 1 < hi)
         {
             int mid = (lo + hi) / 2;
