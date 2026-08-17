1b4e79eab64742aa13a62ae576d4b905a066a22d
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2026-08-17 13:57:00 +0900

    Time: 63 ms (68.27%), Space: 73 MB (69.25%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.cpp
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -1,25 +1,25 @@
 class Solution {
-    void MergeSort(std::vector<int>& arr, int s, int e)
+    void MergeSort(std::vector<int>& arr, std::vector<int>& temp, int s, int e)
     {
         if(s >= e) return;
         int mid = (s + e) / 2;
-        MergeSort(arr, s, mid);
-        MergeSort(arr, mid+1, e);
-        std::vector<int> temp;
-        int sl = s, sr = mid+1;
+        MergeSort(arr, temp, s, mid);
+        MergeSort(arr, temp, mid+1, e);
+        int sl = s, sr = mid+1, idx = s;
         while(sl <= mid && sr <= e)
-            temp.push_back(arr[sl] <= arr[sr] ? arr[sl++] : arr[sr++]);
+            temp[idx++] = arr[sl] <= arr[sr] ? arr[sl++] : arr[sr++];
         while(sl <= mid)
-            temp.push_back(arr[sl++]);
+            temp[idx++] = arr[sl++];
         while(sr <= e)
-            temp.push_back(arr[sr++]);
-        for(const auto& iter : temp)
-            arr[s++] = iter;
+            temp[idx++] = arr[sr++];
+        for(s; s <= e; s++)
+            arr[s] = temp[s];
     }
 public:
     vector<int> sortArray(vector<int>& nums) 
     {
-        MergeSort(nums, 0, nums.size()-1);
+        std::vector<int> temp(nums.size());
+        MergeSort(nums, temp, 0, nums.size()-1);
         return nums;
     }
 };
